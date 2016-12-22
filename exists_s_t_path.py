8a7f97e9a4503308647f51aa51a70418623e37ee
Perry Eng <eng.perry@gmail.com>
2016-12-22 10:04:39 -0600

    Included function to generate directed graphs

--- exists_s_t_path.py
+++ exists_s_t_path.py
@@ -3,9 +3,9 @@
 # Graph is in form of adjacency matrix
 
 import Queue
+import random
 
 def exists_s_t_path_breadth(G,s,t):
-
 	if s==t: return True
 	visited = [False for _ in xrange(len(G))]
 	visited[s] = True
@@ -36,6 +36,18 @@ def exists_s_t_path_depth(G,s,t):
 				stk.append(neighbor)
 	return False
 
+def random_graph(vertices_count):
+	G = []
+	nodes = range(vertices_count)
+	for v in xrange(vertices_count):
+		G.append([0]*vertices_count)
+		random.shuffle(nodes)
+		G[v][v] = 1
+		for neighbor in nodes[:random.randint(0,vertices_count)]:
+			G[v][neighbor] = 1
+	return G
+
+
 G = [\
 	[1,1,1,0,0],\
 	[1,1,0,1,0],\
@@ -65,9 +77,20 @@ I = [\
 	[0,1,0,1,0,0,0],\
 	[0,0,1,0,1,0,0],\
 	[0,0,0,0,0,1,1],\
-	[0,0,0,0,0,1,1],\
+	[0,0,0,0,0,1,1]\
 ] # last two vertices are in seperate component
 
 print exists_s_t_path_breadth(I,1,5)
 print exists_s_t_path_breadth(I,6,5)
 print exists_s_t_path_breadth(I,6,4)
+
+J = [\
+	[1,1,0,1,0],\
+	[0,1,1,0,0],\
+	[0,0,1,0,0],\
+	[0,0,0,1,0],\
+	[1,0,0,0,1]\
+] # directed graph; G[x] is list of x's neighbors
+
+print exists_s_t_path_depth(J,0,2)
+print exists_s_t_path_depth(J,2,0)
