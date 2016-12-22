46d6b6d9e92d3bca1a1b3fd4ce39c1f21d30da89
Perry Eng <eng.perry@gmail.com>
2016-12-22 09:22:40 -0600

    Included depth search

--- exists_s_t_path.py
+++ exists_s_t_path.py
@@ -4,7 +4,8 @@
 
 import Queue
 
-def exists_s_t_path(G,s,t):
+def exists_s_t_path_breadth(G,s,t):
+
 	if s==t: return True
 	visited = [False for _ in xrange(len(G))]
 	visited[s] = True
@@ -21,6 +22,20 @@ def exists_s_t_path(G,s,t):
 				q.put(neighbor)
 	return False
 
+def exists_s_t_path_depth(G,s,t):
+	if s==t: return True
+	visited = [False for _ in xrange(len(G))]
+	visited[s] = True
+	stk = [neighbor for neighbor,is_neighbor in enumerate(G[s]) if (neighbor!=s and is_neighbor==1)]
+	while len(stk)!=0:
+		n = stk.pop()
+		if n==t: return True
+		visited[n] = True
+		for neighbor,is_neighbor in enumerate(G[n]):
+			if neighbor!=n and is_neighbor==1 and not visited[neighbor]:
+				stk.append(neighbor)
+	return False
+
 G = [\
 	[1,1,1,0,0],\
 	[1,1,0,1,0],\
@@ -29,7 +44,7 @@ G = [\
 	[0,0,1,0,1],\
 ] # graph is connected
 
-print exists_s_t_path(G,2,3)
+print exists_s_t_path_breadth(G,2,3)
 
 H = [\
 	[1,1,1,0,0,0],\
@@ -40,8 +55,8 @@ H = [\
 	[0,0,0,0,0,1]
 ] # last vertex is disconnected
 
-print exists_s_t_path(H,2,3)
-print exists_s_t_path(H,2,5)
+print exists_s_t_path_breadth(H,2,3)
+print exists_s_t_path_breadth(H,2,5)
 
 I = [\
 	[1,1,1,0,0,0,0],\
@@ -53,6 +68,6 @@ I = [\
 	[0,0,0,0,0,1,1],\
 ] # last two vertices are in seperate component
 
-print exists_s_t_path(I,1,5)
-print exists_s_t_path(I,6,5)
-print exists_s_t_path(I,6,4)
+print exists_s_t_path_breadth(I,1,5)
+print exists_s_t_path_breadth(I,6,5)
+print exists_s_t_path_breadth(I,6,4)
