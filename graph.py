9e7ad62775b39c6fc8798d0525dacfd5650dfe3c
Perry Eng <eng.perry@gmail.com>
2016-12-23 14:23:49 -0600

    Putting graph related code in graph.py

---
+++ graph.py
@@ -0,0 +1,54 @@
+#
+#
+import random
+
+def random_graph(vertices_count):
+	G = [[False]*vertices_count for _ in xrange(vertices_count)]
+	nodes = range(vertices_count)
+	for v in xrange(vertices_count):
+		random.shuffle(nodes)
+		for neighbor in nodes[:random.randint(0,vertices_count)]:
+			G[v][neighbor] = True
+		G[v][v] = False
+	return G
+
+def simplize(adjm):
+	t = [[v==1 for v in a] for a in adjm]
+	return SimpleGraph(t)
+
+
+class SimpleGraph:
+	def __init__(self,adjm):
+		#self.adjm = [[v==1 for v in a] for a in adjm]
+		self.adjm = adjm
+		self.mag = len(adjm)
+		self.visited = [False]*self.mag
+		for v in xrange(self.mag):
+			self.adjm[v][v] = False
+
+	def has_edge(self,v,u):
+		"vu is not the same as uv"
+		return not v==u and self.adjm[v][u]
+
+	def are_neighbors(self,v,u):
+		"v is a neighbor of u if vu and uv are edges"
+		return not v==u and self.adjm[v][u] and self.adjm[u][v]
+
+	def outdegree(self,v):
+		"returns # of distinct vu edges"
+		#"accounts for diagonal if present"
+		#d = sum(self.adjm[v])
+		#return d if not self.adjm[v][v] else d-1
+		return sum(self.adjm[v])
+
+	def indegree(self,v):
+		"returns # of distinct uv edges"
+		#d = sum((1 if u[v] else 0 for u in self.adjm))
+		#d = sum((1 for u in self.adjm if u[v]))
+		#d = 0
+		#for u in self.adjm:
+		#	if u[v]>0:
+		#		d += 1
+		#return d if self.adjm[v][v] else d-1
+		return sum(1 for u in self.adjm if u[v])
+
