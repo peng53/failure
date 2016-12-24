476782e713724e06b74af86006398ae22d839a35
Perry Eng <eng.perry@gmail.com>
2016-12-24 11:05:40 -0600

    Moved draw_graph use class; improved positioning of points

--- draw_graph.py
+++ draw_graph.py
@@ -6,19 +6,49 @@ import math
 import itertools
 import graph
 
+class App:
+	def __init__(self,master,sG):
+		self.G = sG.adjm
+		self.D = 800
+		self.pointSize = 10
+		self.R = self.D//2
+		self.dA = 360/(len(self.G)-1)
+
+		Tk.Button(root, text="Close", command=quit).pack()
+		self.w = Tk.Canvas(root, width=self.D+20, height=self.D+20)
+		self.w.pack()
+		self.w.create_line(1,0,1,self.D+20)
+		self.w.create_line(self.D+20,0,self.D+20,self.D+20)
+		self.XY = [(self.R * math.cos(v*self.dA)+self.R+5,self.R * math.sin(v*self.dA)+self.R+5) for v in xrange(len(self.G))]
+
+		for v,u in itertools.combinations(xrange(len(self.G)),2):
+			vu,uv = self.G[v][u] == 1,self.G[u][v] == 1
+			if not vu and not uv: continue
+			x1,y1 = self.XY[v]
+			x2,y2 = self.XY[u]
+			EP = "both" if vu and uv else "last" if vu else "first"
+			LC = "black" if vu and uv else "red" if vu else "blue"
+			self.w.create_line(x1,y1,x2,y2,fill=LC,arrow=EP)
+		for v,(x,y) in enumerate(self.XY):
+			self.w.create_oval(x-self.pointSize,y-self.pointSize,x+self.pointSize,y+self.pointSize)
+			self.w.create_text(x,y+2*self.pointSize,text=str(v))
+
+def draw_simple_graph(sG):
+	root = Tk.Tk()
+	app = App(root,sG)
+	root.mainloop()
+
 def draw_simple_graph(sG):
 	G = sG.adjm
 	node_coords = []
 	root = Tk.Tk()
 
 	intersize = 800
-	dia=10
+	dia=4
 	hk = intersize//2
-	print "hk",hk
-	r = intersize//2
-	angle_int = 360//len(G)
-	print "angle_int",angle_int
 
+	r = intersize//2
+	angle_int = 360//(len(G)-1)
 	w = Tk.Canvas(root, width=intersize+50, height=intersize+50)
 	b = Tk.Button(root, text="Close", command=quit)
 	b.pack()
@@ -47,4 +77,16 @@ def draw_simple_graph(sG):
 	root.mainloop()
 
 if __name__=="__main__":
-	draw_simple_graph(graph.biclique(5,3))
+	root = Tk.Tk()
+	#app = App(root,graph.biclique(3,4))
+	G = [\
+	[0,0,0,1,0,0],\
+	[0,0,0,0,1,0],\
+	[0,0,0,0,0,1],\
+	[0,0,1,0,0,0],\
+	[1,0,0,0,0,0],\
+	[0,1,0,0,0,0],\
+	]
+	app = App(root,graph.SimpleGraph(G))
+	root.mainloop()
+	#draw_simple_graph(graph.biclique(10,10))
