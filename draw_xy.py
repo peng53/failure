57e83f17eec9481c7384fa95eed852df11c68f6b
Perry Eng <eng.perry@gmail.com>
2017-01-21 22:27:15 -0600

    changed input from [pts] to [([pts],color)]; this allows point groupings have the same color

--- draw_xy.py
+++ draw_xy.py
@@ -9,27 +9,29 @@ Phase 3: connect points like a line graph
 import Tkinter as Tk
 
 class XYPlane:
-	def __init__(self,master,pts):
-		self.pts = pts
+	def __init__(self,master,G):
+		self.max_dim = 800
 		self.min_x, self.max_x = 0, 0
 		self.min_y, self.max_y = 0, 0
-		for (x,y) in pts:
-			self.min_x = min(self.min_x,x)
-			self.max_x = max(self.max_x,x)
-			self.min_y = min(self.min_y,y)
-			self.max_y = max(self.max_y,y)
+		for (pts,c) in G:
+			for (x,y) in pts:
+				self.min_x = min(self.min_x,x)
+				self.max_x = max(self.max_x,x)
+				self.min_y = min(self.min_y,y)
+				self.max_y = max(self.max_y,y)
 		print self.min_x,self.max_x
 		print self.min_y,self.max_y
 		Tk.Button(master, text="Close", command=quit).pack()
-		self.scale = 500
+		self.scale = 100
 		self.w = Tk.Canvas(master, width=self.scale*(abs(self.min_x)+self.max_x)+1, height=self.scale*(abs(self.min_y)+self.max_y)+1, background="white")
 		self.w.pack()
 		self.w.create_line(0,self.scale*self.max_y,self.scale*(abs(self.min_x)+self.max_x),self.scale*self.max_y,fill="grey")
 		self.w.create_line(self.scale*abs(self.min_x),0,self.scale*abs(self.min_x),self.scale*(abs(self.min_y)+self.max_y),fill="grey")
-		for (x,y) in pts:
-			cy = self.scale*(-y+self.max_y)
-			cx = self.scale*(x+abs(self.min_x))
-			self.w.create_oval(cx,cy,cx+1,cy+1,width=0.5)
+		for (pts,c) in G:
+			for (x,y) in pts:
+				cy = self.scale*(-y+self.max_y)
+				cx = self.scale*(x+abs(self.min_x))
+				self.w.create_oval(cx,cy,cx+1,cy+1,width=0.5,outline=c)
 
 def frange(a,b,d):
 	x = a
@@ -45,7 +47,6 @@ def draw_graph(pts):
 
 
 if __name__=="__main__":
-	pts = [(x,x*x) for x in frange(-1.0,1.0,0.1)]+[(x,x) for x in frange(-1.0,1.0,0.1)]
-	print pts
+	pts = [([(x,x*x) for x in frange(-2.0,2.0,0.1)],"grey"),([(x,x) for x in frange(-2.0,2.0,0.1)],"blue"),([(x,x*x*x) for x in frange(-1.0,1.0,0.1)],"red")]
 	draw_graph(pts)
 
