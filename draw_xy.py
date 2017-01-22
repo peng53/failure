b787587051ca5e3f05625a7bc4deaa6a161dce5d
Perry Eng <eng.perry@gmail.com>
2017-01-21 21:42:09 -0600

    able to do negative x&y, but limited to scale of 1:1, which makes graphing x^2 not as pretty

--- draw_xy.py
+++ draw_xy.py
@@ -14,12 +14,21 @@ class XYPlane:
 		self.min_x, self.max_x = 0, 0
 		self.min_y, self.max_y = 0, 0
 		for (x,y) in pts:
+			self.min_x = min(self.min_x,x)
 			self.max_x = max(self.max_x,x)
+			self.min_y = min(self.min_y,y)
 			self.max_y = max(self.max_y,y)
-		self.w = Tk.Canvas(master, width=(abs(self.min_x)+self.max_x)+10, height=(abs(self.min_y)+self.max_y)+10, background="white")
+		print self.min_x,self.max_x
+		print self.min_y,self.max_y
+		Tk.Button(master, text="Close", command=quit).pack()
+		self.w = Tk.Canvas(master, width=(abs(self.min_x)+self.max_x)+1, height=(abs(self.min_y)+self.max_y)+1, background="white")
 		self.w.pack()
+		self.w.create_line(0,self.max_y,abs(self.min_x)+self.max_x,self.max_y,fill="grey")
+		self.w.create_line(abs(self.min_x),0,abs(self.min_x),abs(self.min_y)+self.max_y,fill="grey")
 		for (x,y) in pts:
-			self.w.create_oval(x,y,x+1,y+1)
+			cy = -y+self.max_y
+			cx = x+abs(self.min_x)
+			self.w.create_oval(cx,cy,cx+1,cy+1,width=0.5)
 
 def draw_graph(pts):
 	root = Tk.Tk()
@@ -27,6 +36,6 @@ def draw_graph(pts):
 	root.mainloop()
 
 if __name__=="__main__":
-	pts = [(x,x**2) for x in xrange(50)]
+	pts = [(x,x**2) for x in xrange(-20,20,1)]+[(-2,-4),(10,-20)]+[(x,x) for x in xrange(-100,100,2)]
 	draw_graph(pts)
 
