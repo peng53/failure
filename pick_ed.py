1347234a26470417403df23c963d55b6175f13bd
Perry Eng <eng.perry@gmail.com>
2017-01-27 17:39:54 -0600

    work on interface for editor; puzzled at how I'll make it work with dicts AND lists/arrays and also how I'll optimally update the listboxes

--- pick_ed.py
+++ pick_ed.py
@@ -1,46 +1,75 @@
 import Tkinter as Tk
 
 class PickEd:
-	def __init__(self,master):
-		self.kframe = Tk.Frame(background="white")
-		self.vframe = Tk.Frame(bg="grey")
-		self.kframe.grid(column=0,row=0)
-		self.vframe.grid(column=1,row=0)
-		Tk.Label(self.kframe,text="Kframe").grid()
-		Tk.Label(self.vframe,text="Vframe").grid()
+	def __init__(self,master,V):
+		self.v = V
+		self.KV = not hasattr(V,'index')
+		self.fr1 = Tk.Frame()
+		SB = Tk.Scrollbar(master,command=self.yview, orient='vertical')
+		self.lb1 = Tk.Listbox(self.fr1,height=25,width=40 if self.KV else 80, yscrollcommand=SB.set)
+		for item in self.v:
+			self.lb1.insert('end', item)
 
-		self.dd =  {}
-		scrollbar = Tk.Scrollbar(master,command=self.yview, orient=Tk.VERTICAL)
-		scrollbar.grid(column=3,row=0,sticky="NS")
-		for e in xrange(50):
-			self.dd[e] = e+2
-		self.listbox = Tk.Listbox(self.kframe,height=25,width=40, yscrollcommand=scrollbar.set)
-		self.listbox.grid()
-		for item in self.dd:
-			self.listbox.insert(Tk.END, item)
-		self.listbox2 = Tk.Listbox(self.vframe,height=25,width=40, yscrollcommand=scrollbar.set)
-		self.listbox2.grid()
-		for item in self.dd:
-			self.listbox2.insert(Tk.END, str(self.dd[item]))
+		self.EN1 = Tk.Entry()
+		self.fr1.grid(column=0,row=0,columnspan=3 if self.KV else 6)
 
-		Tk.Button(master,text="Get", command=self.getb).grid()
+		if self.KV:
+			self.fr2 = Tk.Frame()
+			self.lb2 = Tk.Listbox(self.fr2,height=25,width=40, yscrollcommand=SB.set)
+			for item in self.v:
+				self.lb2.insert('end', str(self.v[item]))
+			self.EN2 = Tk.Entry()
 
-		#self.menubar = Tk.Menu(master)
-		#self.menubar.add_command(label="Quit", command=master.quit)
-		#master.config(menu=self.menubar)
-		#self.fname = Tk.Entry(master)
-		#self.fname.pack()
+			self.fr2.grid(column=3,row=0,columnspan=3)
+			Tk.Label(self.fr2,text="Values").grid()
+			Tk.Label(self.fr1,text="Keys").grid()
+			self.lb2.grid()
+			self.EN2.grid(row=1,column=3,columnspan=3,sticky='nsew')
+
+		else:
+			Tk.Label(self.fr1,text="Values").grid()
+		self.lb1.grid()
+		SB.grid(row=0,rowspan=3,column=6,sticky='ns')
+
+		self.EN1.grid(row=1,column=0,columnspan=3,sticky='nsew')
+
+
+		Tk.Button(master,text="Get", command=self.getb).grid(row=2,column=0, sticky='ew')
+		Tk.Button(master,text="Set", command=self.setb).grid(row=2,column=1, sticky='ew')
+		Tk.Button(master,text="Add", command=self.getb).grid(row=2,column=2, sticky='ew')
+		Tk.Button(master,text="Del", command=self.getb).grid(row=2,column=3, sticky='ew')
+		Tk.Button(master,text="Clr", command=self.getb).grid(row=2,column=4, sticky='ew')
+		Tk.Button(master,text="Exit", command=master.quit).grid(row=2,column=5, sticky='ew')
 
 	def yview(self, *args):
-		apply(self.listbox.yview, args)
-		apply(self.listbox2.yview, args)
+		apply(self.lb1.yview, args)
+		if self.KV:
+			apply(self.lb2.yview, args)
 
 	def getb(self):
-		print self.listbox.index(Tk.ACTIVE)
-		print self.dd[self.listbox.index(Tk.ACTIVE)]
+		I = self.lb1.index('active')
+		if self.KV:
+			self.lb2.see(I)
+			v,v2 = self.lb1.get(I), self.lb2.get(I)
+			self.EN2.delete(0,'end')
+			self.EN2.insert(0,v2)
+		else:
+			v = self.v[I]
+		self.EN1.delete(0,'end')
+		self.EN1.insert(0,v)
+
+	def setb(self):
+		I = self.lb1.index('active')
+		if self.KV:
+			pass
+
 def run_pick_ed():
 	root = Tk.Tk()
-	app = PickEd(root)
+	v = {"cookie":"cake","banana":"bread"}
+	for x in xrange(40):
+		v[x+4] = x*30
+	#v = range(10,50,2)
+	app = PickEd(root,v)
 	root.mainloop()
 
 run_pick_ed()
