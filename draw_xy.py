1090a2f93bd9c9d5673ed033d57e9914f9c17f2c
Perry Eng <eng.perry@gmail.com>
2017-01-29 14:06:38 -0600

    added grid-lines & tickmarks; it may look like I just moved around lot code and renamed variables but there has been a great recode on the drawing process; planning to use numpy next instead of scaling each and every pt

--- draw_xy.py
+++ draw_xy.py
@@ -57,7 +57,7 @@ class XYpts:
 		return (self.x[0],self.x[0]*W[0]+W[1],self.x[-1],self.x[-1]*W[0]+W[1])
 	def nslq(self,parts):
 		"naive segmented least squares"
-		if 2*parts<self.x.size: return None
+		if self.x.size>>1<parts: return []
 		W, cut = [], self.x.size//parts
 		for i in xrange(0,self.x.size,cut):
 			x, y = self.x[i:i+cut], self.y[i:i+cut]
@@ -130,7 +130,6 @@ class XYpts_poly(XYpts):
 		for c in x_poly(p):
 			self.y = self.y * self.x + c
 		self.genR()
-		print self.R
 		#print "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 		#print self.x
 		#print "YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY"
@@ -141,6 +140,16 @@ class XYpts_poly(XYpts):
 			#elif y > My: My = y
 		#self.R = R(self.x[0],self.x[-1],my,My)
 
+def frange(x0,x1,dx):
+	x = x0
+	if dx>0:
+		while x<x1:
+			yield x
+			x+=dx
+	else:
+		while x>x1:
+			yield x
+			x+=dx
 
 class XYspread:
 	#def __init__(self,XYs,scale=1,viewr=None,padx=0,pady=0):
@@ -156,48 +165,72 @@ class XYspread:
 				if t.R.dM > Mx: Mx = t.R.dM
 				if t.R.cm < my: my = t.R.cm
 				if t.R.cM > My: My = t.R.cM
-			self.R = R(mx-padx,Mx+padx,my-pady,My+pady)
+			self.R = R(mx-self.d['padx'],Mx+self.d['padx'],my-self.d['pady'],My+self.d['pady'])
 		else: self.R = self.d['view']
-		print self.R
 
 class XYPlane:
 	def __init__(self,master,G):
-		self.master,self.G,self.osd,R,scale = master,G,None,G.R,G.d['%']
+		self.master,self.G = master,G
 		Tk.Button(master, text="Close", command=quit).pack()
 		self.make_canvas()
+
 	def make_canvas(self):
 		self.osd,R,scale = None, self.G.R, self.G.d['%']
-		print R
-		W = 1+scale*(abs(R.dm)+abs(R.dM))
-		#H = 1+scale*(abs(R.cm)+abs(R.cM))
-		if (R.cm>0 and R.cM>0) or (R.cm<0 and R.cM<0): H = 1+scale*abs(R.cm-R.cM)
-		else: H = 1+scale*(abs(R.cm)+abs(R.cM))
-		print (H-1)/scale, R.cm, R.cM
-		scalex, scaley = lambda x: scale*(x+abs(R.dm)), lambda y: scale*(-y+abs(R.cM))
+		W = 1 + scale * abs(R.dM - R.dm)
+		H = 1 + scale * abs(R.cM - R.cm)
 		self.w = Tk.Canvas(self.master, width=W, height=H, background="white")
+		self.w.pack()
 		self.w.bind("<Button-1>", self.drawosd)
-		# draw origin lines if (0,0) is visible
-		if R.cm<=0 or R.cM>=0: self.w.create_line(0,scaley(0),W,scaley(0),fill="grey",dash=[4,4])
-		if R.dm<=0 or R.dM>=0: self.w.create_line(scalex(0),0,scalex(0),H,fill="grey",dash=[4,4])
-		for g in G.XYs:
+
+		'''
+		#if R.cm<=0 and R.dm<=0: self.w.create_text(-scale*R.dm,scale*R.cM,text='0,0')
+		#if R.cm<=0 and R.dm<=0: self.w.create_text(scale*abs(R.dm),scale*abs(R.cm),text='0,0')
+		#if R.dm<=0: self.w.create_line(-scale*R.dm,0,-scale*R.dm,H) #y-axis only occurs when R.dm is nonpositive
+		#if R.cm<=0<=R.cM: self.w.create_line(0,scale*abs(R.cm),W,scale*abs(R.cm)) #x-axis
+		#if R.dm<=0<=R.dM: self.w.create_line(scale*abs(R.dm),0,scale*abs(R.dm),H) #y-axis
+		#if R.cm<=0: self.w.create_line(0,scale*R.cM,W,scale*R.cM) #x-axis only occurs when
+		'''
+		scx = lambda x: scale*(x-R.dm)
+		scy = lambda y: scale*(R.cM-y)
+		scp = lambda x,y: (scale*(x-R.dm), scale*(R.cM-y))
+		if R.cm<=0 and R.dm<=0: self.w.create_text(*scp(0,0),text='0,0')
+		if R.cm<=0: self.w.create_line(0,scy(0),W,scy(0),fill="#333",dash=[1,2])
+		if R.dm<=0: self.w.create_line(scx(0),0,scx(0),H,fill="#333",dash=[1,2]) #y-axis only occurs when R.dm is nonpositive
+		if 'grid' in self.G.d:
+			for x in frange(self.G.d['grid'][0],R.dM,self.G.d['grid'][0]):
+				self.w.create_line(scx(x),0,scx(x),H,fill="#ccc",dash=[4,4])
+			for x in frange(-self.G.d['grid'][0],R.dm,-self.G.d['grid'][0]):
+				self.w.create_line(scx(x),0,scx(x),H,fill="#ccc",dash=[4,4])
+			for y in frange(self.G.d['grid'][1],R.cM,self.G.d['grid'][1]):
+				self.w.create_line(0,scy(y),W,scy(y),fill="#ccc",dash=[4,4])
+			for y in frange(-self.G.d['grid'][1],R.cm,-self.G.d['grid'][1]):
+				self.w.create_line(0,scy(y),W,scy(y),fill="#ccc",dash=[4,4])
+		if 'tick' in self.G.d:
+			for x in frange(self.G.d['tick'][0],R.dM,self.G.d['tick'][0]):
+				self.w.create_text(scx(x),scy(0),text=str(x))
+			for x in frange(-self.G.d['tick'][0],R.dm,-self.G.d['tick'][0]):
+				self.w.create_text(scx(x),scy(0),text=str(x))
+			for y in frange(self.G.d['tick'][1],R.cM,self.G.d['tick'][1]):
+				self.w.create_text(scx(0),scy(y),text=str(y))
+			for y in frange(-self.G.d['tick'][1],R.cm,-self.G.d['tick'][1]):
+				self.w.create_text(scx(0),scy(y),text=str(y))
+		for g in self.G.XYs:
 			if 'ls' in g.d:
 				# Least squares
 				lr = g.lsq()
-				self.w.create_line(scalex(lr[0]),scaley(lr[1]),scalex(lr[2]),scaley(lr[3]),dash=[2,6,2],fill=g.d['color'])
+				self.w.create_line(scx(lr[0]),scy(lr[1]),scx(lr[2]),scy(lr[3]),dash=[2,6,2],fill=g.d['color'])
 			if 'nls' in g.d:
 				# Naive
-				W = g.nslq(5)
-				if W is not None:
-					x0 = y0 = None
-					for (x,y) in W:
-						x1, y1 = scalex(x),scaley(y)
-						if x0 is not None: self.w.create_line(x0,y0,x1,y1,dash=[4,3,4],fill="purple")
-						x0, y0 = x1,y1
+				x0 = y0 = None
+				for x,y in g.nslq(5):
+					x1, y1 = scp(x,y)
+					if x0 is not None: self.w.create_line(x0,y0,x1,y1,dash=[4,3,4],fill="purple")
+					x0, y0 = x1, y1
 			x0 = y0 = None
 			lpad = rpad = 3
-			for (x,y) in g.xy_pts():
+			for x,y in g.xy_pts(): #sx = scale * (x-R.dm) #sy = scale * (R.cM-y)
 				b_draw = False
-				if x>=R.dm and x<=R.dM and y>=R.cm and y<=R.cM: b_draw = True
+				if R.dm<=x<=R.dM and R.cm<=y<=R.cM: b_draw = True
 				elif x<0 and lpad>0: # outside but make an exception
 					b_draw = True
 					lpad -=1
@@ -205,13 +238,11 @@ class XYPlane:
 					b_draw = True
 					rpad -=1
 					lpad -=1 # if left-exceptions were not used, use them now
-				x1,y1 = scalex(x),scaley(y)
+				x1,y1 = scp(x,y)
 				if b_draw is True:
 					self.w.create_oval(x1,y1,x1+1,y1+1,outline=g.d['color'])
 					if x0 is not None and 'lines' in g.d: self.w.create_line(x0,y0,x1,y1,fill=g.d['color'])
 				x0,y0=x1,y1
-		self.w.pack()
-
 	def drawosd(self,event):
 		if self.osd is not None:
 			for e in self.osd:
@@ -220,9 +251,7 @@ class XYPlane:
 		cx,cy = event.x,event.y
 		for g in self.G.XYs:
 			self.osd.append(self.w.create_rectangle(cx-16,cy+5,cx+16,cy+7,fill=g.d['color'],width=0))
-			#self.osd.append(self.w.create_rectangle(cx,cy,cx+16,cy+16,fill=g.color,width=0))
 			self.osd.append(self.w.create_text(cx,cy,text=g.d['name'],fill="black"))
-			#self.osd.append(self.w.create_text(cx+48,cy+8,text=g.name,fill=g.color))
 			cy += 16
 
 def draw_graph(G):
@@ -231,21 +260,14 @@ def draw_graph(G):
 	root.mainloop()
 
 if __name__=="__main__":
-	#normal = [(normalvariate(0,1),normalvariate(0,1)) for _ in xrange(500)]
 	gs = []
-	#gs.append(XYpts_linear(xs=x_range(-5,5,1),m=1,b=0,opts={"color":"red","name":"x","lines":0}))
-	#gs.append(XYpts_linear(xs=x_range(-10,-5,1),m=-2,b=0,opts={"color":"blue","name":"-2x"}))
-	gs.append(XYpts(pts=lfox(-4.0,4.0,0.1,lambda x:x*x-2),opts={"color":"green","name":"x^2-2","lines":0}))
-	#gs.append(XYpts_linear(xs=x_range(-1.0,1.0,0.1),m=2,b=-2,opts={"color":"pink","name":"2x-2"}))
-	gs.append(XYpts(pts=lfox(-2.0,2.0,0.1,lambda x:(x+1)**3-3),opts={"color":"orange","name":"(x+1)^3-3",'ls':0}))
+	gs.append(XYpts_linear(xs=x_range(-5,5,1),m=1,b=0,opts={"color":"red","name":"x","lines":0}))
+	#gs.append(XYpts(pts=lfox(-4.0,4.0,0.1,lambda x:x*x-2),opts={"color":"green","name":"x^2-2","lines":0}))
+	gs.append(XYpts_poly(xs=x_range(-4,4,0.1),p=[(2,1),(0,-2)],opts={"color":"green","name":"x^2-2","lines":0,"nls":0,"ls":0}))
 	#gs.append(XYpts(pts=lfox(-2.0,2.0,0.1,lambda x:(x+0.3)**4),opts={"color":"cyan","name":"(x+0.3)^4"}))
-	#gs.append(XYpts(pts=lfox(-2.0,2.0,0.1,lambda x:x**5+0.1),opts={"color":"grey","name":"x^5+0.1","lines":0,'nls':0}))
-	#gs.append(XYpts(pts=lfox(-2.0,2.0,0.1,lambda x:x**6-1),opts={"color":"brown","name":"x^6-1","lines":0}))
-	#gs.append(XYpts(pts=normal,opts={'name':'normal','color':'blue'}))
-	#gs.append(XYpts_poly(xs=x_range(-3.0,3.0,0.1),p=[(2,2),(1,-1),(0,+24)]))
 	viewr=None
 	padx=1
 	pady=1
 	#viewr=R(-4.5,4.5,-4.5,4.5)
-	G = XYspread(gs,opts={'%':25,'view':viewr,'padx':padx,'pady':pady})
+	G = XYspread(gs,opts={'%':50,'view':viewr,'padx':padx,'pady':pady,'grid':[0.5,0.5],'tick':[1,1]})
 	draw_graph(G)
