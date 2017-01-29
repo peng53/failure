e0708b8d287d6cc78212daf6ba0f9b32adb5b31d
Perry Eng <eng.perry@gmail.com>
2017-01-29 01:19:31 -0600

    initial add of srstrgen.py, an idea on hold

---
+++ srstrgen.py
@@ -0,0 +1,14 @@
+#srstrgen.py
+"""
+Random string generator of a certain format.
+
+Just an idea ATM, put on hold until work on other stuff has ceased.
+
+Not a ground breaking idea, just basic stuff using the google-heavy
+''.join(a for a in string.x)
+
+Not meant to beat any password generator.
+
+After this is complete, I'll try for an easy GUI
+Might collide with inc_test.py/rdata_gen.cpp later on
+"""
