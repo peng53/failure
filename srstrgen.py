a6a40ed86b72fa5336aac7e085e4e314e7b7bc5a
Perry Eng <eng.perry@gmail.com>
2017-02-15 14:30:38 -0600

    changed rr to output string instead of char, profiled array version of rr (rr_a) via cProfile; list version is 2 seconds faster for length of 1,000,000 (that is also article on this: 'Efficient String Concatenation in Python'; I'll stick with the list version as it is a bit more readable

--- srstrgen.py
+++ srstrgen.py
@@ -13,6 +13,7 @@ After this is complete, I'll try for an easy GUI
 Might collide with inc_test.py/rdata_gen.cpp later on
 """
 from random import randint
+from array import array
 
 C = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',\
 	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',\
@@ -25,16 +26,29 @@ C_L = [26,26,10,16,16,1]
 C_R2= [0,26,52,62,78,94]
 #r between C_R2[i] and C_R2[i]+C_L[i]
 
-def rr(incs):
+def rr(incs,length):
 	if not any(incs) or len(incs)>6: raise ValueError
+	o = []
 	r_l = sum(l for i,l in enumerate(C_L) if incs[i])
-	r = randint(0,r_l-1)
-	for i,is_inc in enumerate(incs):
-		if is_inc:
-			if C_R[i][0]<=r<=C_R[i][1]:
-				return C[r]
-		else: r+=C_L[i]
-	print r #error!
+	for _ in xrange(length):
+		r = randint(0,r_l-1)
+		for i,is_inc in enumerate(incs):
+			if is_inc:
+				if C_R2[i]<=r<=C_R2[i]+C_L[i]: o.append(C[r])
+			else: r+=C_L[i]
+	return ''.join(o)
+
+def rr_a(incs,length):
+	if not any(incs) or len(incs)>6: raise ValueError
+	o = array('c')
+	r_l = sum(l for i,l in enumerate(C_L) if incs[i])
+	for _ in xrange(length):
+		r = randint(0,r_l-1)
+		for i,is_inc in enumerate(incs):
+			if is_inc:
+				if C_R2[i]<=r<=C_R2[i]+C_L[i]: o.append(C[r])
+			else: r+=C_L[i]
+	return o.tostring()
 
 def rr_t(incs):
 	if not any(incs) or len(incs)>6: raise ValueError
