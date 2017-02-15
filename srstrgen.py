24703acdced6a2f3a4caf7956677ae7d2a0802b8
Perry Eng <eng.perry@gmail.com>
2017-02-15 15:23:10 -0600

    syntax change for readablity

--- srstrgen.py
+++ srstrgen.py
@@ -16,17 +16,19 @@ from random import randint, choice
 from array import array
 import string
 
-C = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',\
-	'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',\
-	'0','1','2','3','4','5','6','7','8','9',\
-	'`','~','!','@','#','$','%','^','&','*','(',')','_','+','-','=',\
-	',','.','/',';',"'",'[',']','<','>','?',':','"','{','}','|','\\',\
-	' ']
-
-C_s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()_+-=,./;'[]<>?:\"{}|\\ "
+#Character includes are the in following order:
+C=("abcdefghijklmnopqrstuvwxyz" #lowercase alpha
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" #uppercase alpha
+	"0123456789" #digits
+	"`~!@#$%^&*()_+-=" #number row symbols
+	",./;'[]<>?:\"{}|\\" #other symbols
+	" ") #space
+# C_L are include lengths
 C_L = [26,26,10,16,16,1]
+# C_R2 are include start points (which technically can be calculated..)
 C_R2= [0,26,52,62,78,94]
 #r between C_R2[i] and C_R2[i]+C_L[i]
+# C_tr and C_os are number row symbols and other symbols in the same order as above includes
 C_tr = r'`~!@#$%^&*()_+-='
 C_os = r',./;\'[]<>?:"{}|\\'
 
@@ -42,25 +44,12 @@ def rr(incs,length):
 			else: r+=C_L[i]
 	return ''.join(o)
 
-def rr_s(incs,length):
-	if not any(incs) or len(incs)>6: raise ValueError
-	o = []
-	r_l = sum(l for i,l in enumerate(C_L) if incs[i])
-	for _ in xrange(length):
-		r = randint(0,r_l-1)
-		for i,is_inc in enumerate(incs):
-			if is_inc:
-				if C_R2[i]<=r<=C_R2[i]+C_L[i]: o.append(C_s[r])
-			else: r+=C_L[i]
-	return ''.join(o)
-
 def rr_p(incs,length):
 	if not any(incs) or len(incs)>6: raise ValueError
 	c = ''.join(s for i,s in enumerate([string.lowercase,string.uppercase,string.digits,C_tr,C_os,' ']) if incs[i])
 	return ''.join(choice(c) for _ in xrange(length))
 
-
-def rr_t(incs):
+def rr_test(incs):
 	if not any(incs) or len(incs)>6: raise ValueError
 	r_l = sum(l for i,l in enumerate(C_L) if incs[i])
 	p_b = []
