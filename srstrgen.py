8e2eda47f6c4b1aa393a1af005664d9088bed79f
Perry Eng <eng.perry@gmail.com>
2017-02-17 13:17:52 -0600

    srstrgen.py: made funcs to generate list of possible chars for use in choice given a white and blacklist, where blacklist takes priority; the char one was the initial thought and the set one was a realization around like 75; the 'char' one should be a time complexity O(n) alg anyway (or O(m*n) where m is size of exc due reallocations?); thinking of reversing the order of iteration (and testing) or (also seems to be a good case for use of linked lists) of course the set version works best in python.

--- srstrgen.py
+++ srstrgen.py
@@ -20,8 +20,9 @@ import string
 C=("abcdefghijklmnopqrstuvwxyz" #lowercase alpha
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ" #uppercase alpha
 	"0123456789" #digits
-	"`~!@#$%^&*()_+-=" #number row symbols
+	"!#$%&()*+-=@^_`~" #number row symbols
 	",./;'[]<>?:\"{}|\\" #other symbols
+	"\"',./:;<>?[\\]{|}"
 	" ") #space
 # C_L are include lengths
 C_L = [26,26,10,16,16,1]
@@ -29,8 +30,9 @@ C_L = [26,26,10,16,16,1]
 C_R2= [0,26,52,62,78,94]
 #r between C_R2[i] and C_R2[i]+C_L[i]
 # C_tr and C_os are number row symbols and other symbols in the same order as above includes
-C_tr = r'`~!@#$%^&*()_+-='
-C_os = r',./;\'[]<>?:"{}|\\'
+C_tr = r'!#$%&()*+-=@^_`~'
+C_os = r'"\',./:;<>?[\\]{|}'
+
 
 def rr(incs,length):
 	if not any(incs) or len(incs)>6: raise ValueError
@@ -45,7 +47,7 @@ def rr(incs,length):
 	return ''.join(o)
 
 def rr_p(incs,length):
-	if not any(incs) or len(incs)>6: raise ValueError
+	if not any(incs): raise ValueError
 	c = ''.join(s for i,s in enumerate([string.lowercase,string.uppercase,string.digits,C_tr,C_os,' ']) if incs[i])
 	return ''.join(choice(c) for _ in xrange(length))
 
@@ -61,3 +63,28 @@ def rr_test(incs):
 					break
 			else: r+=C_L[i]
 	print p_b
+
+def inc_exc_chars(incs,exc):
+	# where incs is a list [lower,upper,digits,numberrowsymbols,othersymbols,space]
+	# exs is a string of excludes
+	if not any(incs): raise ValueError
+	c = sorted(''.join(s for i,s in enumerate([string.lowercase,string.uppercase,string.digits,C_tr,C_os,' ']) if incs[i]))
+	I = 0
+	# reduce(lambda x,y: x+y if x[-1]!=y else x, sorted(exc))
+	# Also works but uses string cat..
+	for e in sorted(set(exc)):
+		for i in xrange(I,len(c)):
+			if e==c[i]:
+				c.pop(i)
+				I=i
+				break
+	return c
+
+def inc_exc_chars_set(incs,exc):
+	# where incs is a list [lower,upper,digits,numberrowsymbols,othersymbols,space]
+	# exs is a string of excludes
+	if not any(incs): raise ValueError
+	# If I'm going to use sets, I might as well use it all the way..
+	c = set(''.join(s for i,s in enumerate([string.lowercase,string.uppercase,string.digits,C_tr,C_os,' ']) if incs[i]))
+	c.difference_update(exc)
+	return sorted(c)
