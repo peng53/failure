447bb9724d598aa3a05ceaaabb5a18b2422a75e1
Perry Eng <eng.perry@gmail.com>
2017-02-20 09:11:41 -0600

    srstrgen.cpp: moved several lines to own functions; replaced some old style loops with new ones for clarity; working on remove_excs logic, looks a bit convoluted. The random function that is soon to be added is just an random indexer so not much work to be done there.

--- srstrgen.cpp
+++ srstrgen.cpp
@@ -8,74 +8,69 @@
 
 using namespace std;
 
-vector<char> dif_a_b(vector<char> &I, char *E){
+vector<char> remove_excs(vector<char> I, char *E){
+	sort(I.begin(),I.end());
 	vector<char> O;
 	O.reserve(I.size());
-	//vector<char>::reverse_iterator i = I.rbegin();
-	//int e_len = strlen(E);
-	//sort(E,E+e_len);
-	//for (int e_i=e_len-1;e_i>-1;--e_i){
-		//while(i!=I.rend()){
-			//if (*i<E[e_i]) break;
+	auto i = I.begin();
+	unsigned e_len = strlen(E);
+	sort(E,E+e_len);
+	//for (unsigned e_i=0;e_i<e_len;++e_i){
+		//while(i!=I.end()){
+			//if (*i>E[e_i]) break;
 			//if (*i==E[e_i]){
 				//++i;
 				//break;
-			//} else O.push_back(*i++);//++i;}
+			//}
+			//O.push_back(*i++);
 		//}
 	//}
-	//while (i!=I.rend()) O.push_back(*i++);//++i;
-	vector<char>::iterator i = I.begin();
-	int e_len = strlen(E);
-	sort(E,E+e_len);
-	for (int e_i=0;e_i<e_len;++e_i){
-		while(i!=I.end()){
-			if (*i>E[e_i]) break;
-			if (*i==E[e_i]){
-				++i;
-				break;
+	for (char *e=E;e!=E+e_len;++e){
+		if (*i<=*e){
+			while(i!=I.end()){
+				if (*i==*e){ ++i; break; }
+				O.push_back(*i++);
 			}
-			O.push_back(*i++);//++i;}
 		}
 	}
-	while (i!=I.end()) O.push_back(*i++);//++i;
+	O.insert(O.end(),i,I.end());
 	return O;
 }
 
+vector<char> build_incs(char* I){
+	const char lo[]= "abcdefghijklmnopqrstuvwxyz",
+		up[]= "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+		dt[]= "0123456789",
+		s1[]= "!#$%&()*+-=@^_`~",
+		s2[]= "\"',./:;<>?[\\]{|}",
+		sp[]= " ";
+	const char *C[]= {lo,up,dt,s1,s2,sp};
+	unsigned C_L[] = {26,26,10,16,16,1};
+	bool inc[6] = {};
+	unsigned c_len = 0;
+	for (unsigned i=0;i<6;++i){
+		inc[i] = I[i]=='1';
+		if (inc[i]) c_len+=C_L[i];
+	}
+	vector <char> CV;
+	CV.reserve(c_len);
+	for (unsigned i=0;i<6;++i){
+		if (inc[i]) CV.insert(CV.end(),C[i],C[i]+C_L[i]);
+	}
+	return CV;
+}
+
 int main(int argc, char *argv[]){
+	if (argc<3){
+		cout << "Missing arguments..\n";
+		return 1;
+	}
 	if (strlen(argv[1])<6){
 		cout << "Need to specify all includes and excludes\n";
 		return 1;
 	}
-	int c_len = 0;
-	bool inc[6] = {};
-	char lo[]= "abcdefghijklmnopqrstuvwxyz";
-	char up[]= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char dt[]= "0123456789";
-	char s1[]= "!#$%&()*+-=@^_`~";
-	char s2[]= "\"',./:;<>?[\\]{|}";
-	char sp[]= " ";
-	int C_L[] = {26,26,10,16,16,1};
-	char *C[]= {lo,up,dt,s1,s2,sp};
-	vector <char> CV;
-	for (int i=0;i<6;++i){
-		inc[i] = argv[1][i]=='1';
-		if (inc[i]) c_len+=C_L[i];
-	}
-	CV.reserve(c_len);
-	for (int i=0;i<6;++i){
-		if (inc[i]){
-			CV.insert(CV.end(),C[i],C[i]+C_L[i]);
-		}
-	}
-	sort(CV.begin(),CV.end());
-	for (int i=0;i<c_len;++i){
-		cout << CV[i];
-	}
-	cout << '\n';
-	vector<char> D = dif_a_b(CV,argv[2]);
-	for (unsigned int i=0;i<D.size();++i){
-		cout<<D[i];
-	}
+	vector<char> C = remove_excs(build_incs(argv[1]),argv[2]);
+	for (char c : C) cout<<c;
 	cout << '\n';
 	return 0;
 }
