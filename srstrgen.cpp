b04d58c45c1c93543fd7eac08d2a7e794d929301
Perry Eng <eng.perry@gmail.com>
2017-02-19 21:16:26 -0600

    srstrgen.cpp can do exclude now. (via similar yet reversed alg from srstrgen.py 'inc_exc'; don't know whether I should just be using c++'s set data struct; may try in future

--- srstrgen.cpp
+++ srstrgen.cpp
@@ -8,8 +8,41 @@
 
 using namespace std;
 
+vector<char> dif_a_b(vector<char> &I, char *E){
+	vector<char> O;
+	O.reserve(I.size());
+	//vector<char>::reverse_iterator i = I.rbegin();
+	//int e_len = strlen(E);
+	//sort(E,E+e_len);
+	//for (int e_i=e_len-1;e_i>-1;--e_i){
+		//while(i!=I.rend()){
+			//if (*i<E[e_i]) break;
+			//if (*i==E[e_i]){
+				//++i;
+				//break;
+			//} else O.push_back(*i++);//++i;}
+		//}
+	//}
+	//while (i!=I.rend()) O.push_back(*i++);//++i;
+	vector<char>::iterator i = I.begin();
+	int e_len = strlen(E);
+	sort(E,E+e_len);
+	for (int e_i=0;e_i<e_len;++e_i){
+		while(i!=I.end()){
+			if (*i>E[e_i]) break;
+			if (*i==E[e_i]){
+				++i;
+				break;
+			}
+			O.push_back(*i++);//++i;}
+		}
+	}
+	while (i!=I.end()) O.push_back(*i++);//++i;
+	return O;
+}
+
 int main(int argc, char *argv[]){
-	if (strlen(argv[2])<6){
+	if (strlen(argv[1])<6){
 		cout << "Need to specify all includes and excludes\n";
 		return 1;
 	}
@@ -25,7 +58,7 @@ int main(int argc, char *argv[]){
 	char *C[]= {lo,up,dt,s1,s2,sp};
 	vector <char> CV;
 	for (int i=0;i<6;++i){
-		inc[i] = argv[2][i]=='1';
+		inc[i] = argv[1][i]=='1';
 		if (inc[i]) c_len+=C_L[i];
 	}
 	CV.reserve(c_len);
@@ -37,6 +70,11 @@ int main(int argc, char *argv[]){
 	sort(CV.begin(),CV.end());
 	for (int i=0;i<c_len;++i){
 		cout << CV[i];
+	}
+	cout << '\n';
+	vector<char> D = dif_a_b(CV,argv[2]);
+	for (unsigned int i=0;i<D.size();++i){
+		cout<<D[i];
 	}
 	cout << '\n';
 	return 0;
