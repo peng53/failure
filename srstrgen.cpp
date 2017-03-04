cf21f809b5ec4c9c39b0d0850644b937ca51ec28
Perry Eng <eng.perry@gmail.com>
2017-03-04 15:42:44 -0600

    untested cleanned version; enabled back 'case 0' but requires further tests; changed ST constructer to use initilizer lists but as said further testing needs to be done

--- srstrgen.cpp
+++ srstrgen.cpp
@@ -10,29 +10,7 @@ using namespace std;
 
 mt19937 RNG(time(0));
 
-int what_flag(const char *s){
-	switch (strlen(s)){
-		case 0: return 0;
-		case 2:
-			switch (s[1]){
-				case 's': return 1;
-				case 'b': return 2;
-				case 'B': return 3;
-			}
-	}
-	return 9;
-}
-
 string b_incs(const char* I){
-	/*
-	const char *lo= "abcdefghijklmnopqrstuvwxyz",
-		*up= "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-		*dt= "0123456789",
-		*s1= "!#$%&()*+-=@^_`~",
-		*s2= "\"',./:;<>?[\\]{|}",
-		*sp= " ";
-	const char *C[]= {lo,up,dt,s1,s2,sp};
-	*/
 	const char *C[]= {
 		"abcdefghijklmnopqrstuvwxyz",
 		"ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -68,52 +46,19 @@ string remove_excs(string &I, char *E){
 	O.insert(O.end(),i,I.cend());
 	return O;
 }
-struct ST {
-	unsigned L = 0, W = 0;
-	string * I;
-	string * D;
-	ST(string &s){
-		I=&s;
-		L=0;
-		W=1;
-	}
-	ST(string &s,unsigned c){
-		I=&s;
-		L=c;
-		W=1;
-	}
-	ST(string &s,string &d,unsigned r){
-		I=&s;
-		D=&d;
-		W=r;
-		L=0;
-	}
-	ST(string &s,unsigned c,string &d,unsigned r){
-		I=&s;
-		D=&d;
-		L=c;
-		W=r;
-	}
-};
-ostream& operator<<(ostream& out,ST &P){
-	if (P.L!=0){
-		uniform_int_distribution<unsigned> r(0,(*P.I).length()-1);
-		for (unsigned w=P.W;w>0;--w){
-			for (unsigned l=0;l<P.L;++l) out<<(*P.I)[r(RNG)];
-			if (w!=1) out<<*P.D;
-			//cout<<w<<'\n';
-		}
-	} else {
-		for (unsigned w=P.W;w>0;--w){
-			out<<*P.I;
-			if (w!=1) out<<*P.D;
-			//cout<<w<<'\n';
+int what_flag(const char *s){
+	switch (strlen(s)){
+	case 0: return 0;
+	case 2:
+		switch (s[1]){
+		case 's': return 1;
+		case 'b': return 2;
+		case 'B': return 3;
 		}
 	}
-	return out;
+	return 9;
 }
-
-int literal_storer(int C,char **A,vector<string> &lits){
+int literal_storer(const int C,char **A,vector<string> &lits){
 	string I;
 	for (int i=2;i<C;++i){
 		switch(what_flag(A[i])){
@@ -134,71 +79,78 @@ int literal_storer(int C,char **A,vector<string> &lits){
 	}
 	return C;
 }
-vector<ST> part_storer(int I,int C,char **A,vector<string> &lits){
-	unsigned U[4];
-	unsigned c;
-	string t;
-	vector<ST> parts;
-	regex r("[0-9]+");
-	for (int i=I;i<C;++i){
-		t=string {A[i]};
-		c=0;
-		for(auto j=sregex_iterator(t.begin(),t.end(),r);j!=sregex_iterator() && c<4;++j){
-			U[c]=atoi((*j).str().c_str());
-			//cout << U[c] << '\n';
-			c++;
+struct ST {
+	unsigned L = 0, W = 1;
+	string * I;
+	string * D;
+	ST(string &s) : I(&s){}
+		//I=&s;L=0;W=1;}
+	ST(string &s,unsigned c) : L(c),I(&s){}
+		//I=&s;L=c;W=1;}
+	ST(string &s,string &d,unsigned r) : W(r),I(&s),D(&d){}
+		//I=&s;D=&d;W=r;L=0;}
+	ST(string &s,unsigned c,string &d,unsigned r) : L(c),W(r),I(&s),D(&d){}
+		//I=&s;D=&d;L=c;W=r;}
+};
+ostream& operator<<(ostream& out,ST &P){
+	if (P.L!=0){
+		uniform_int_distribution<unsigned> r(0,(*P.I).length()-1);
+		for (unsigned w=P.W;w>0;--w){
+			for (unsigned l=0;l<P.L;++l) out<<(*P.I)[r(RNG)];
+			if (w!=1) out<<*P.D;
 		}
-		//cout << "counted " << c << '\n';
-		/*
-		for(unsigned i=0;i<c;++i){ cout << U[i] << '\n'; }
-		*/
-
-		switch (c){
-			/*
-			case 0:
-				lits.push_back(t);
-				parts.push_back(ST(lits.back()));
-				break;
-			*/
-			case 1:
-				if (U[0]<lits.size()){
-					//cout << "part with lit index " << U[0] << '\n';
-					parts.push_back(ST(lits.at(U[0])));
-				}
-				break;
-			case 2:
-				if (U[0]<lits.size()){
-					//cout << "part with lit index " << U[0] << '\n';
-					//cout << "length of " << U[1] << '\n';
-					parts.push_back(ST(lits.at(U[0]),U[1]));
-				}
-				break;
-			case 3:
-				if (U[0]<lits.size() && U[1]<lits.size()){
-					parts.push_back(ST(lits.at(U[0]),lits.at(U[1]),U[2]));
-					//cout << "part with lit index " << U[0] << '\n';
-					//cout << "length of " << U[1] << '\n';
-					//cout << "part with dim index " << U[2] << '\n';
-				}
-				break;
-			case 4:
-				if (U[0]<lits.size() && U[2]<lits.size()){
-					parts.push_back(ST(lits.at(U[0]),U[1],lits.at(U[2]),U[3]));
-					//cout << "part with lit index " << U[0] << '\n';
-					//cout << "length of " << U[1] << '\n';
-					//cout << "part with dim index " << U[2] << '\n';
-					//cout << "wordcount of " << U[3] << '\n';
-				}
-				break;
-			default:
-					cout << t <<" is an invalid option.";
-				break;
+	} else {
+		for (unsigned w=P.W;w>0;--w){
+			out<<*P.I;
+			if (w!=1) out<<*P.D;
 		}
 	}
-
+	return out;
+}
+void auto_part(const string &t,vector<string> &lits,vector<ST> &P){
+	if (t.length()==0): return;
+	unsigned U[4];
+	unsigned c = 0;
+	size_t lits_s = lits.size();
+	regex r("[0-9]+");
+	for(auto j=sregex_iterator(t.begin(),t.end(),r);j!=sregex_iterator() && c<4;++j){
+		U[c++]=atoi((*j).str().c_str());
+	}
+	if (c==0){
+		lits.push_back(t);
+		P.push_back(ST(lits.back()));
+	} else if (U[0]<lits_s){
+		switch (c){
+		case 1: P.push_back(ST(lits.at(U[0]))); break;
+		case 2: P.push_back(ST(lits.at(U[0]),U[1])); break;
+		case 3: if (U[1]<lits_s) P.push_back(ST(lits.at(U[0]),lits.at(U[1]),U[2])); break;
+		case 4: if (U[2]<lits_s) P.push_back(ST(lits.at(U[0]),U[1],lits.at(U[2]),U[3])); break;
+		}
+	}
+}
+vector<ST> part_storer(const int I,const int C,char **A,vector<string> &lits){
+	vector<ST> parts;
+	for (int i=I;i<C;++i) auto_part(string {A[i]},lits,parts);
 	return parts;
 }
-
+const char* stir_mix(string &C, const unsigned L, const unsigned W, const char *S){
+	string sS=S;
+	ST p(C,L,sS,W);
+	stringstream sout;
+	sout << p;
+	const string rs = sout.str();
+	const char* rs2 = rs.c_str();
+	return rs2;
+}
+extern "C" const char* instant_coffee(const char *C, const unsigned L, const unsigned W, const char *S){
+	string sC=C;
+	return stir_mix(sC,L,W,S);
+}
+extern "C" const char* plen(const char *I,char *E, const unsigned L, const unsigned W, const char *S){
+	string CV=b_incs(I);
+	string C=remove_excs(CV,E);
+	return stir_mix(C,L,W,S);
+}
 int main(int argc,char **argv){
 	if (argc==1) return 1;
 	cout << "Compiled on " << __DATE__ << " at " << __TIME__ << '\n';
@@ -210,10 +162,6 @@ int main(int argc,char **argv){
 	vector<string> lits;
 	int i = literal_storer(argc,argv,lits);
 	vector<ST> parts = part_storer(i,argc,argv,lits);
-	//cout << "MY LITERALS" << '\n';
-	//for (auto l : lits) cout << '\t' << l << '\n';
-	//C = atoi(argv[1]);
-	//cout << C << '\n';
 	for (C=atoi(argv[1]);C>0;--C){
 		for (auto p : parts){
 			cout << p;
