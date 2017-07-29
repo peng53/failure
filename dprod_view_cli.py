dd7ed392bfd2a0529936109b81aa51d98d7984ca
Perry Eng <eng.perry@gmail.com>
2017-07-29 00:02:59 -0500

    Date for cli should work now. Also minor ui change

--- dprod_view_cli.py
+++ dprod_view_cli.py
@@ -18,7 +18,7 @@ EXT = {
 DTE = {
 	'b': "SELECT * from prod_records WHERE date(end_time)<?",
 	'a': "SELECT * from prod_records WHERE date(start_time)>?",
-	'd': "SELECT * from prod_records WHERE date=?"
+	'd': "SELECT * from prod_records WHERE date(start_time)=?"
 }
 
 def open_db():
@@ -40,14 +40,14 @@ def open_db():
 def close_db(conn):
 	conn.close()
 def print_rows(rows,s,c):
-	print '-'*82
-	print "RN |   UID  |    Start Time    |     End Time     |  Code | Desc"
+	print '_'*82
+	print "RN  /  UID   /   Start Time     /    End Time      / Code  /  Desc"
 	print '-'*82
 	for n,i in enumerate(xrange(s,min(c+s,len(rows)))):
 		print "%2d | %6s | %16s | %16s | %5s | %-22s" %(n,rows[i][0],rows[i][1],rows[i][2],rows[i][3],rows[i][4])
-	for d in xrange(len(rows),s+c):
-		print "%2d | %6s | %16s | %16s | %5s |" %(d,'','','','')
-	print '-'*82
+	for _ in xrange(len(rows),s+c):
+		print "   | %6s | %16s | %16s | %5s |" %('','','','')
+	print '='*59, "%21s %d" %("Page",s//c)
 def draw_screen(results,r_s,r_c,notes,n_s,n_c):
 	print "Productivity Viewer"
 	print "Results table from lookup"
@@ -66,33 +66,31 @@ def dlookup_d():
 	print "( u)id ( c)ode ( s)tart ( e)nd ( d)esc"
 	s = raw_input("::")
 	if len(s)==1:
-		if s in EXT or s in DTE:
+		if s in DTE:
 			print "Enter requirement." # for now just the easy ones, later allow prepended operator
 			t = raw_input("::::")
-			if len(t)>0:
-				print t
-				if s in DTE:
-					try:
-						if '/' in t:
-							d = map(int,t.split('/',2))
-							if len(d)==3:
-								return (DTE[s],("%04d-%02d-%02d" %(d[2],d[0],d[1])),)
-						elif '-' in t:
-							d = map(int,t.split('-',2))
-							if len(t)==3:
-								return (DTE[s],("%04d-%02d-%02d" %(d[0],d[1],d[2]),))
-						elif len(t)==8:
-							return (DTE[s],('-'.join((t[4:8],t[0:2],t[2:4])),))
-						else:
-							print "Unrecognized date format."
-					except Exception as e:
-						print e
-						print "Date contains invalid characters."
-				else:
-					return [EXT[s],(t,)]
+			if len(t)==0:
+				print "Requirement is required."
+			elif '/' in t:
+				print 1
+				d = map(int,t.split('/',2))
+				if len(d)==3:
+					return (DTE[s],("%04d-%02d-%02d" %(d[2],d[0],d[1]),))
+			elif '-' in t:
+				print 2
+				d = map(int,t.split('-',2))
+				if len(d)==3:
+					return (DTE[s],("%04d-%02d-%02d" %(d[0],d[1],d[2]),))
+			elif len(t)==8:
+				print 3
+				return (DTE[s],('-'.join((t[4:8],t[0:2],t[2:4])),))
 			else:
-				print "A requirement is required."
-		print "Unknown option entered. 303"
+				print "Unrecognized date format."
+		elif s in EXT:
+			print "Enter requirement."
+			t = raw_input("::::")
+			if len(t)>0:
+				return [EXT[s],(t,)]
 	elif len(s)==0 or len(s)==2:
 		if s in ORD:
 			return [ORD[s]]
@@ -133,11 +131,12 @@ def main():
 	R,R_S = [],0
 	N,N_S = [],0
 	conn = c = None
-	draw_screen(R,R_S,10,N,N_S,10)
+	ic = 10
+	draw_screen(R,R_S,ic,N,N_S,ic)
 	delta = False
 	while True:
 		if delta:
-			draw_screen(R,R_S,10,N,N_S,10)
+			draw_screen(R,R_S,ic,N,N_S,ic)
 			delta = 0
 		s = raw_input(":")
 		if len(s)==0:
@@ -149,7 +148,7 @@ def main():
 		elif s=='Q':
 			if conn:
 				conn.close()
-				return
+			return
 		elif s=='l':
 			if not conn:
 				print "Please load a file first."
@@ -170,16 +169,38 @@ def main():
 			N,N_S = [], 0
 			delta = 1
 		elif s=='d':
-			q = drowchoose(len(N),N_S,10)
+			q = drowchoose(len(N),N_S,ic)
 			if q:
 				for n in q:
 					N.pop(n)
 				delta = 1
 		elif s=='a':
-			q = drowchoose(len(R),R_S,10)
+			q = drowchoose(len(R),R_S,ic)
 			if q:
 				for n in q:
 					N.append(R[n])
 				delta = 1
+		elif s=='nn':
+			if N_S+ic<len(N):
+				N_S+=ic
+				delta = 1
+		elif s=='nr':
+			if R_S+ic<len(R):
+				R_S+=ic
+				delta=1
+		elif s=='pn':
+			if N_S!=0:
+				N_S-=ic
+				delta = 1
+		elif s=='pr':
+			if R_S!=0:
+				R_S-=ic
+				delta = 1
+		elif s=='1n':
+			N_S = 0
+			delta = 1
+		elif s=='1r':
+			R_S = 0
+			delta = 1
 
 main()
