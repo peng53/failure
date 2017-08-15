0067bcf33525acfc14f11e55cbe05c4425d8e038
Perry Eng <eng.perry@gmail.com>
2017-08-15 15:03:01 -0500

    Working shift-click 'multi-select' function. Need to have working 'next/prev-month' before making anything else. Either use the tag system to hide non-current month or recreate on the fly. Allowing for multiple months may mean adding another dimension or changing the bset var. Currently it jsut records the row,col values but needs to be changed to record month day year (and prehaps calcuate row and col when drawing?). Also need to record previous bset var if I want to get a range between them (or just get the range before replacing it?)
    Work on hold on dprod_view_cli.tcl because at this point it
    feels like translating python to tcl more-or-less
    Might just finish it up to the python equavilant to get it
    off the plate

--- tcl/tcal.tcl
+++ tcl/tcal.tcl
@@ -11,16 +11,88 @@ array set days {
 set today [clock format [clock seconds] -format {%a %B %Y %m %d}]
 set dow $days([lindex $today 0])
 set first [expr ($days([lindex $today 0])-[lindex $today 4]+1)%7]
+set cur_yr [lindex $today 2]
+#proc is_lpyr {yr} {
+#	if {[expr $yr%4]!=0} { return false }
+#	elseif {[expr $yr%100]==0} { return true }
+#	elseif {[expr $yr%400]!=0} { return true }
+#	else { return false }
+#}
+proc is_lpyr {yr} {
+	# python to tcl
+	return [expr [expr $yr%4]==0 && [expr $yr%100]!=0 || [expr $yr%400]==0]
+}
+proc is_lpyr {y} {
+	return [expr $y%4==0 && $y%100!=0 || $y%400==0]
+}
 frame .fcal
 frame .fcal.mth
 button .fcal.mth.prv -text <
 button .fcal.mth.nxt -text >
 label .fcal.mth.mthl -text [lrange $today 1 2]
 canvas .fcal.can -width 168 -height 142 -bg white
+set bset false
+proc sel_at {r c} {
+	set x [expr $c*24]
+	set y [expr $r*24]
+	sel_24 $y $x
+	#.fcal.can create rect $x $y [expr $x+24] [expr $y+24] -outline red -tags sel
+}
+proc sel_24 {y x} {
+	.fcal.can create rect $x $y [expr $x+24] [expr $y+24] -outline red -tags sel
+}
 bind .fcal.can <ButtonPress-1> {
-	set x [expr int([%W canvasx %x]/24)*24]
-	set y [expr int([%W canvasy %y]/24)*24]
-	.fcal.can create rect $x $y [expr $x+24] [expr $y+24] -outline red
+	if {[set r [expr int([%W canvasy %y]/24)]]<1} { return } 
+	#if {$r<1} { return }
+	set c [expr int([%W canvasx %x]/24)]
+	set bset "$r $c"
+	.fcal.can delete -tags sel
+	sel_at $r $c
+}
+bind .fcal.can <Shift-ButtonPress-1> {
+	if {[set r [expr int([%W canvasy %y]/24)]]<1} { return }
+	#if {$r<1} { return }
+	set c [expr int([%W canvasx %x]/24)]
+	.fcal.can delete -tags sel
+	if {$bset=="false"} {
+		set bset "$r $c"
+		set x [expr $c*24]
+		set y [expr $r*24]
+		sel_24 $y $x
+		#sel_at $r $c
+	} else {
+		set r2 [lindex $bset 0]
+		set c2 [lindex $bset 1]
+		set bset "$r $c"
+		if {$r2<$r} {
+			set rt $r2
+			set ct $c2
+			set r2 $r
+			set c2 $c
+			set r $rt
+			set c $ct
+		}
+		set x [expr $c*24]
+		set y [expr $r*24]
+		while {$r<$r2} {
+			while {$c<7} {
+				sel_24 $y $x
+				#sel_at $r $c
+				incr c
+				incr x 24
+			}
+			set c 0
+			set x 0
+			incr r
+			incr y 24
+		}
+		while {$c<=$c2} {
+			#sel_at $r $c
+			sel_24 $y $x
+			incr c
+			incr x 24
+		}
+	}
 }
 grid .fcal.mth.prv -column 0 -row 0
 grid .fcal.mth.mthl -column 1 -row 0
