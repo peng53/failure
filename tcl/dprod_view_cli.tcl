d0211464433b5100be8dfd31f197636301099efb
Perry Eng <eng.perry@gmail.com>
2017-08-18 16:41:40 -0500

    No major change.

--- tcl/dprod_view_cli.tcl
+++ tcl/dprod_view_cli.tcl
@@ -15,9 +15,7 @@ namespace eval viewer {
 		db eval {pragma table_info(prod_records)} {
 			try {
 				if {$ccols($name) ne $type} { return 0 }
-			} on error {} {
-				return 0
-			}
+			} on error {} { return 0 }
 		}
 		return 1
 	}
@@ -38,9 +36,7 @@ namespace eval viewer {
 				puts "'$s' loaded for viewing."
 				variable db_open
 				set db_open 1
-			} else {
-				puts "Database schema does not match intended.\nClosing file."
-			}
+			} else { puts "Database schema does not match intended.\nClosing file." }
 		}
 	}
 	proc all_int {l} {
@@ -51,14 +47,11 @@ namespace eval viewer {
 	}
 	proc date_in {} {
 		if {[gets stdin s]==0} return
-		if {[string first / $s]!=-1 && [llength [set s [split $s /]]]==3} {
-			if {[all_int $s]==1} { return [format %04d-%02d-%02d [lindex $s 2] {*}[lrange $s 0 1]] }
-		} elseif {[string first - $s]!=-1 && [llength [set s [split $s -]]]==3} {
-			if {[all_int $s]==1} { return [format %04d-%02d-%02d {*}$s] }
-		} elseif {[string length $s]>=8} {
-			for {set i 0} {$i<8} {incr i} {
-				if {[string is integer [string index $s $i]]==0} return
-			}
+		if {[string first / $s]!=-1} {
+			if {[llength [set s [split $s /]]]==3 && [all_int $s]==1} { return [format %04d-%02d-%02d [lindex $s 2] {*}[lrange $s 0 1]] }
+		} elseif {[string first - $s]!=-1} {
+			if {[llength [set s [split $s -]]]==3 && [all_int $s]==1} { return [format %04d-%02d-%02d {*}$s] }
+		} elseif {[string length $s]>=8 && [string is integer $s]} {
 			return "[string range $s 4 7]-[string range $s 0 1]-[string range $s 2 3]"
 		}
 	}
@@ -70,18 +63,17 @@ namespace eval viewer {
 			u {WHERE uid=} c {WHERE code=} ou {ORDER by uid} oc {ORDER by code}
 			os {ORDER by start_time} oe {ORDER by end_time} od {ORDER by desc}
 		}
-		puts "Record lookup choices\n()all\nExact / Limited\n(u)id (c)ode (b)efore (d)ate (a)fter\nBy Order\n(ou)id (oc)ode (os)tart (oe)nd (od)esc"
+		puts "Lookup Options\nBy Order: (ou)id (oc)ode (os)tart (oe)nd (od)esc ()all\nExact / Limited: (u)id (c)ode (b)efore (d)ate (a)fter"
 		switch [gets stdin s] {
-			0 {
-				db_q $base
+			0 { db_q $base
 			} 1 {
-				puts {Enter requirement}
 				switch $s {
 					b - a - d {
-						if {[string length [set t [date_in]]]==0} {
-							puts {Requirement is required.}
+						puts {Enter requirement}
+						if {[string length [set t [date_in]]]==0} { puts {Requirement is required.}
 						} else { db_q "$base $WHE($s)\"$t\"" }
 					} u - c {
+						puts {Enter requirement}
 						if {[gets stdin t]==0} { puts {Requirement is required.}
 						} else { db_q "$base $WHE($s)\"$t\"" }
 					}
@@ -105,18 +97,12 @@ namespace eval viewer {
 	proc print_rows {rows off c} {
 		puts [string repeat _ 62]
 		puts { # {  UID /    Start Time   \ /    End Time     }  Code | Desc}
-		set n 0
+		set n -1
 		foreach r [lrange $rows $off [expr min($c+$off,[llength $rows])]] {
-			puts [format "%2d ! %4s | %10s %5s | %10s %5s | %5s | %-22s" [expr $n+$off] {*}$r]
-			incr n
+			puts [format "%2d ! %4s | %10s %5s | %10s %5s | %5s | %-22s" [expr [incr n]+$off] {*}$r]
 		}
-		if {$n!=0 || $n<$c} {
-			incr n 1
-			while {$n<$c} {
-				puts [format "   '%6s'%18s'%18s'%7s'" "" "" "" ""]
-				incr n
-			}
-		}
+		if {$n==0} { set n [expr max($c-3,0)]}
+		while {[incr n]<$c} { puts [format "   '%6s'%18s'%18s'%7s'" "" "" "" ""] }
 		puts "[string repeat # 62] Pg [expr $off/$c]"
 	}
 	proc draw_screen {} {
@@ -126,58 +112,37 @@ namespace eval viewer {
 		print_rows $viewer::R $viewer::R_o $viewer::R_c
 		puts {Noted records}
 		print_rows $viewer::N $viewer::N_o $viewer::N_c
-		puts "Command Bar\n(L)oad (Q)uit (l)ookup (a)dd-to-notes (d)elete-from-notes\n(S)ort (C)lear (1)st-page (n)ext-page (p)rev-page (s)ummarize\n(e)mployee (f)ilter"
+		puts "(L)oad (Q)uit (l)ookup (a)dd-to-notes (d)elete-from-notes\n(S)ort (C)lear (1)st-page (n)ext-page (p)rev-page (s)ummarize\n(e)mployee (f)ilter"
 	}
 	proc row_choose {max} {
-		puts "Row selection from 0 to [expr $max-1]\nTo indicate a range, use a hyphen to seperate two numbers.\nFor specific rows, use commas to seperate numbers."
+		puts "Row selection from 0-[expr $max-1]\nRange (-) List (,)"
 		if {[gets stdin s]>0} {
 			if {[string first - $s]!=-1} {
-				set r [split $s -]
-				foreach n $r {
-					if {[expr !{[string is integer $n]}]} {
-						puts {Invalid numbers.}
-						return
-					}
-				}
-				if {[lindex $r 1]<=[lindex $r 0] || [lindex $r 0]<0 || [lindex $r 1]>=$max} {
-					puts {Invalid range given.}
-					return
-				}
+				lassign [split $s -] a b
+				if {[string is integer $a] && [string is integer $b] && 0<=$a && $a<$b && $b<$max} {
+					set l $a
+					while {$a<$b} { lappend l [incr a] }
+					return $l
+				} else { puts {Invalid range / number(s) given.} }
+			} elseif {[string first , $s]!=-1} {
 				set l [list]
-				for {set i [lindex $r 0]} {$i < [lindex $r 1]+1} {incr i} {
-					lappend l $i
+				foreach n [split $s ,] {
+					if {[string is integer $n] && 0<=$n && $n<$max} { lappend l $n }
 				}
 				return $l
-			} elseif {[string first , $s]!=-1} {
-				set r [split $s ,]
-				foreach n $r {
-					if {[expr !{[string is integer $n]}] || $n >= $max || $n < 0} {
-						puts {Invalid number.}
-						return
-					}
-				}
-				return $r
-			} else {
-				if {[string is integer $s] && $s<$max} {
-					return $s
-				} else {
-					puts {Bad input??}
-					return
-				}
-			}
+			} elseif {[string is integer $s] && 0<=$s && $s<$max} { return $s }
 		}
 	}
 	proc d_addnote {} {
 		variable N
 		variable delta
-		foreach i [row_choose [llength $viewer::R]] {
-			lappend N [lindex $viewer::R $i]
-		}
 		set delta 1
+		foreach i [row_choose [llength $viewer::R]] { lappend N [lindex $viewer::R $i] }
 	}
 	proc d_delnote {} {
 		variable N
 		variable delta
+		set delta 1
 		set n_l [llength $N]
 		set q [lsort -unique [row_choose $n_l]]
 		set q_i 0
@@ -187,16 +152,14 @@ namespace eval viewer {
 			if {$n_i==[lindex $q $q_i]} {
 				incr q_i
 				if {$q_i == [llength $q]} {
-					lappend NN {*}[lrange $N $n_i+1 [llength $N]+1]
+					lappend NN {*}[lrange $N $n_i+1 $n_l+1]
 					break
 				}
-			} else {
-				lappend NN $n
-			}
+			} else { lappend NN $n }
 			incr n_i
 		}
 		set N $NN
-		set delta 1
+
 	}
 	proc d_next {c} {
 		switch $c {
@@ -267,35 +230,16 @@ namespace eval viewer {
 		}
 	}
 	proc active {} {
-		variable delta
-		if {$viewer::delta==1} {
-			draw_screen
-		}
-		if {[gets stdin s]==0} {
-			draw_screen
-			return 1
-		}
+		if {$viewer::delta==1} { draw_screen }
+		if {[gets stdin s]==0} { return 1 }
 		switch $s {
-			L { open_db }
-			l { d_lookup }
-			a { d_addnote }
-			d { d_delnote }
-			nr { d_next R }
-			nn { d_next N }
-			pr { d_prev R }
-			pn { d_prev N }
-			1r { d_first R }
-			1n { d_first N }
-			Cr { d_clear R }
-			Cn { d_clear N }
-			Q { return 0 }
+			" " { draw_screen }
+			L { open_db } Q { return 0 } l { d_lookup } a { d_addnote } d { d_delnote }
+			nr { d_next R } nn { d_next N } pr { d_prev R } pn { d_prev N }
+			1r { d_first R } 1n { d_first N } Cr { d_clear R } Cn { d_clear N }
 		}
 		return 1
 	}
 }
-if {[llength $argv]>0} {
-	viewer::open_db_silent [lindex $argv 0]
-}
-while {[viewer::active]==1} {
-	continue
-}
+if {[llength $argv]>0} { viewer::open_db_silent [lindex $argv 0] }
+while {[viewer::active]==1} { continue }
