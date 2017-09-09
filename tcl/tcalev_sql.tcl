cb5a5937652192c948a0f99c9c5731dd9c9d1c40
Perry Eng <eng.perry@gmail.com>
2017-09-09 09:50:22 -0500

    Added some variable holidays (using xth_dow). Except Memorial day.
    Noted caveat: when using order but not where in get_all_rows or get_base_rows
    one must put a {} before the order list. Said procs require a where-list in
    position before the order-list. The {} being the empty-where-list.

--- tcl/tcalev_sql.tcl
+++ tcl/tcalev_sql.tcl
@@ -15,22 +15,22 @@ namespace eval EventStor {
 		sqlite3 conn $file_name
 		return 1
 	}
-	proc add_row {start_date end_date event_name desc_more} {
+	proc add_row {start_date end_date event_name {desc_more {}}} {
 		# Add row to event. Returns 0 if database isn't open.
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		conn eval {INSERT into events VALUES(null,$start_date,$end_date,$event_name,$desc_more)}
 		return 1
 	}
 	proc update_row {rowid start_date end_date event_name desc_more} {
 		# Update row in event. Returns 0 if database isn't open.
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		conn eval {UPDATE events SET start_date=$start_date, end_date=$end_date, event_name=$event_name, desc_more=$desc_more WHERE rowid=$rowid}
 		return 1
 	}
 	proc delete_row {rowid} {
 		# Deletes row in event. Returns 0 if database isn't open
 		# and -1 if rowid is zero-length string.
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		if {[string length $rowid]==0} { return -1 }
 		conn eval {DELETE from events WHERE rowid=$rowid}
 		return 1
@@ -48,18 +48,6 @@ namespace eval EventStor {
 			return "WHERE [join $ws { AND }]"
 		}
 	}
-	proc second_date {year {mth 1} {day 1}} {
-		# Returns seconds since unix epoch. (shortcut)
-		return [clock scan [format %02d/%02d/%04d $mth $day $year] -format %D]
-	}
-	proc exp_date {year {mth 0}} {
-		# Returns where-value-list for a year (and month)
-		set vs [list {strftime('%Y',start_date)} == $year]
-		if {$mth!=0} {
-			lappend vs {strftime('%m',start_date)} == $mth
-		}
-		return vs
-	}
 	proc orders {vs} {
 		# Checks order cols for validity.
 		set os [list]
@@ -72,10 +60,22 @@ namespace eval EventStor {
 			return "ORDER BY [join $os ,]"
 		}
 	}
+	proc second_date {year {mth 1} {day 1}} {
+		# Returns seconds since unix epoch. (shortcut)
+		return [clock scan [format %02d/%02d/%04d $mth $day $year] -format %D]
+	}
+	proc exp_date {year {mth 0}} {
+		# Returns where-value-list for a year (and month)
+		set vs [list {strftime('%Y',start_date)} == $year]
+		if {$mth!=0} {
+			lappend vs {strftime('%m',start_date)} == $mth
+		}
+		return vs
+	}
 	proc get_all_rows {{ws {}} {os {}}} {
 		# Yields single row at a time from open database.
 		# Returns 0 if not open.
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		set rs [list]
 		conn eval "SELECT * from events [wheres $ws] [orders $os]" {
 			lappend rs [list $rowid $start_date $end_date $event_name $desc_more]
@@ -86,16 +86,16 @@ namespace eval EventStor {
 		# Yields single row at a time from open database.
 		# Returns 0 if not open.
 		# Only basic stuff
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		set rs [list]
-		conn eval "SELECT rowid,datetime(start_date,'unixepoch','localtime') as date,event_name from events [wheres $ws] [orders $os]" {
+		conn eval "SELECT rowid,date(start_date,'unixepoch','localtime') as date,event_name from events [wheres $ws] [orders $os]" {
 			lappend rs [list $rowid $date $event_name]
 		}
 		return $rs
 	}
 	proc get_month_rows {month year} {
 		# Yields rows for one calendar page.
-		if {$v::is_open==0} { return 0 }
+		if {!$v::is_open} { return 0 }
 		yield 1
 		conn eval {SELECT rowid,day($start_date),$event_name from events WHERE year(start_date)=$year AND month(start_date)=$month} {
 			yield [list $rowid $start_date $event_name]
@@ -116,7 +116,7 @@ namespace eval EventStor {
 	}
 	proc close_db {} {
 		# Closes the open database.
-		if {$v::is_open==0 } { return 0 }
+		if {!$v::is_open} { return 0 }
 		set v::is_open 0
 		conn close
 		return 1
@@ -134,7 +134,17 @@ namespace eval EventStor {
 	}
 	proc holidays_us {year} {
 		if {!$v::is_open} { return 0 }
-		set hs [list 12 25 Christmas 11 4thThurs Thanksgiving 5 2ndSun Mothers 7 4 Independence 6 3rdSun Fathers 10 31 Halloween 2 14 Valentines 3 17 St.Patrick 1 1 NewYearsEve 1 15 MLK 5 LastMon Memorial 9 1stMon Labor 10 2ndMon Columbus 11 11 Veterans]
+		foreach {mth day name} {12 25 Christmas 7 4 {Independence Day} 10 31 Halloween 2 14 {Valentines Day} 3 17 {St Patrick's Day} 1 1 {New Years Eve} 1 15 {Martin Luther King Jr Day} 11 11 {Veterans Day}} {
+			set date [second_date $year $mth $day]
+			add_row $date [expr {$date+86399}] $name
+		}
+		foreach {mth xth day name} {11 3 4 Thanksgiving 5 1 0 {Mothers Day} 6 2 0 {Fathers Day} 9 0 1 {Labor Day} 10 1 1 {Columbus Day}} {
+			set date [second_date $year $mth [xth_dow $mth $year $day $xth]]
+			add_row $date [expr {$date+86399}] $name
+		}
+		return 1
+		
+		#~ 5 LastMon Memorial 
 	}
 	proc test {} {
 		puts {Building DB in memory}
@@ -147,7 +157,6 @@ namespace eval EventStor {
 		foreach {year mth day} {2017 1 23 2018 3 22 2017 1 25 2017 2 11 } {
 			set date [second_date $year $mth $day]
 			add_row $date $date $n {;DROP TABLE events;}
-			add_row $date $date $n {red}
 			incr n
 		}
 		puts {Getting rows}
@@ -155,34 +164,25 @@ namespace eval EventStor {
 			puts {get_base_rows failed}
 			return 1
 		}
-		foreach r $rs { puts [join $r] }
+		foreach r $rs { puts [join $r ,] }
+
 		puts {Getting rows where event_name is > 1 ordered by start_date}
-		if {[set rs [get_base_rows {event_name >= 1} {start_date}]]==0} {
-			puts {get_base_rows failed}
-			return 1
-		}
-		foreach r $rs { puts [join $r] }
+		foreach r [get_base_rows {event_name >= 1} {start_date}] { puts [join $r ,] }
+
 		puts {Getting rows where 0<rowid<4 ordered by rowid}
-		if {[set rs [get_base_rows {rowid > 0 rowid < 4} {rowid}]]==0} {
-			puts {get_base_rows failed}
-			return 1
-		}
-		foreach r $rs { puts [join $r] }
+		foreach r [get_base_rows {rowid > 0 rowid < 4} {rowid}] { puts [join $r ,] }
+
 		puts {Getting rows where desc_more = ;DROP TABLE events;}
-		if {[set rs [get_base_rows {desc_more = "';DROP TABLE events;'"}]]==0} {
-			puts {get_base_rows failed}
-			return 1
-		}
-		foreach r $rs { puts [join $r] }
+		foreach r [get_base_rows {desc_more = "';DROP TABLE events;'"}] { puts [join $r ,] }
+
 		puts {Confirming table & row still exists with get_all_rows}
-		if {[set rs [get_all_rows]]==0} {
-			puts {get_base_rows failed}
-			return 1
-		}
-		foreach r $rs { puts [join $r] }
-		puts <
-		puts [get_all_rows {desc_more = "'red'"} ]
-		puts >
+		foreach r [get_all_rows] { puts [join $r ,] }
+
+		puts {Adding Holidays for year 2017}
+		holidays_us 2017
+		puts {Gettings rows ordered by event_name then start_date}
+		foreach r [get_base_rows {} {event_name start_date}] { puts [join $r ,] }
+
 		puts {Closing DB}
 		if {![close_db]} {
 			puts {close_db failed}
