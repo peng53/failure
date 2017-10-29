d333c8c452d8a5bdca6646b4224ad52bef2aab59
Perry Eng <eng.perry@gmail.com>
2017-10-29 04:03:07 -0500

    Fixed funny errors; Renamed variables to be more reasonable; Etc D is for dinosaur

--- testsq.cpp
+++ testsq.cpp
@@ -2,26 +2,24 @@
 #include <sqlite3.h>
 #include <ctime>
 
+void pretty_date(time_t s){
+	struct tm * X = localtime(&s);
+	printf("%02d/%02d/%4d %02d:%02d", X->tm_mon,X->tm_mday,X->tm_year+1900,X->tm_hour,X->tm_min);
+}
+
 int main(){
-	sqlite3 *test;
-	sqlite3_stmt **s;
-	printf("%d",sqlite3_open("dates.db",&test));
-	printf("%d",sqlite3_prepare_v2(test,"SELECT * from events",-1,s,0));
-	int t = 0;
-	time_t rawtime;
-	struct tm * timeinfo;
-	time(&rawtime);
-	//timeinfo = localtime(&rawtime);
-	//printf("Current local time and date: %s", asctime(timeinfo));
-
-
-	while(sqlite3_step(*s) && t<25){
-		printf("\n%2d | %d | %d | ",sqlite3_column_int(*s,0),sqlite3_column_int(*s,1),sqlite3_column_int(*s,2));
-		printf("%s",sqlite3_column_text(*s,3));
-		t++;
+	sqlite3* db;
+	sqlite3_stmt* s;
+	sqlite3_open("dates.db",&db);
+	sqlite3_prepare_v2(db,"SELECT * from events",-1,&s,0);
+	for (int n = 0; sqlite3_step(s)==100 && n<25; ++n){
+		printf("%02d| ", sqlite3_column_int(s,0));
+		pretty_date(sqlite3_column_int(s,1));
+		fputs(" - ", stdout);
+		pretty_date(sqlite3_column_int(s,2));
+		printf(" | %-25s / %s\n",sqlite3_column_text(s,3),sqlite3_column_text(s,4));
 	}
-	//printf("%d",sqlite3_step(*s));
-	//printf("\n%s\n",sqlite3_column_text(*s,3));
-	printf("%d",sqlite3_finalize(*s));
+	sqlite3_finalize(s);
+	sqlite3_close(db);
 	return 0;
 }
