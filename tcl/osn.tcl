43035d2357b2cf20adca11fd4cf29a36e9361bff
Perry Eng <eng.perry@gmail.com>
2017-11-03 18:04:16 -0500

    Added a few macros to osn.tcl like a rep(eat) and reg(ister). repeat takes non digit input and repeats the entered after a digit it entered. register adds the registered word to a listbox on the side (after space)
    Also some work in binding (physical-)keyboard to same (some not all) functionality.
    
    While it looks like I'm rebinding the entire keyboard, this is
    hopefully not the case!

--- tcl/osn.tcl
+++ tcl/osn.tcl
@@ -1,5 +1,10 @@
 #!/bin/tclsh8.6
 package require Tk
+namespace eval osnv {
+	variable MAC off
+	variable s {}
+	variable locked 0
+}
 bind . <Control-Key-q> {
 	exit
 }
@@ -7,47 +12,51 @@ bind . <Shift_L> {
 	shift_lock
 }
 pack [frame .maintext] -expand 1 -fill both
+pack [frame .maintext.mac_words] -side right -fill y
+pack [listbox .maintext.mac_words.lb -yscrollcommand {.maintext.mac_words.sb set} -width 10] -side left -expand 1 -fill y
+pack [scrollbar .maintext.mac_words.sb -command {.maintext.mac_words.lb yview}] -side left -fill y
 pack [scrollbar .maintext.sb_v -command {.maintext.t yview}] -side right -fill y
 pack [text .maintext.t -wrap none -yscrollcommand {.maintext.sb_v set} -xscrollcommand {.maintext.sb_h set}] -expand 1 -fill both
 pack [scrollbar .maintext.sb_h -orient horizontal -command {.maintext.t xview}] -side bottom -fill x
+pack [labelframe .frame_m_bar -text Macro] -fill x
+pack [radiobutton .frame_m_bar.off -text Off -variable osnv::MAC -value off -command mac_off] -side left
+pack [radiobutton .frame_m_bar.rep -text Repeat -variable osnv::MAC -value rep] -side left
+pack [radiobutton .frame_m_bar.reg -text Register -variable osnv::MAC -value reg] -side left
+pack [checkbutton .frame_m_bar.lock -text Lock -variable osnv::locked -onvalue 1 -offvalue 0] -side left
+pack [button .frame_m_bar.all -text All -command {.maintext.t tag add sel 0.0 end}] -side left
+#pack [button .frame_m_bar.text -text Test -command {puts $osnv::MAC}] -side left
 pack [labelframe .frame_kb -text Keyboard] -expand 1 -fill x -side left
-#~ bind .maintext.t <Key> {
-	#~ if {{%K}=={Shift_L}} {
-		#~ shift_lock
-	#~ } else {
-		#~ break
-	#~ }
-#~ }
 set r 0
-foreach {s} {{1234567890} {qweryuiop} {asdfghjkl} {zxcvbnm}} {
+foreach {s} {{1234567890} {qwertyuiop} {asdfghjkl} {zxcvbnm}} {
 	pack [frame .frame_kb.r$r] -fill x
 	foreach {c} [split $s {}] {
-		pack [button .frame_kb.r$r.b$c -text $c -command ".maintext.t insert insert $c"] -fill both -expand 1 -side left
+		#pack [button .frame_kb.r$r.b$c -text $c -command ".maintext.t insert insert $c"] -fill both -expand 1 -side left
+		pack [button .frame_kb.r$r.b$c -text $c -command "pressed $c"] -fill both -expand 1 -side left
 	}
 	incr r
 }
 pack [frame .frame_kb.r$r] -fill x
-pack [button .frame_kb.r$r.bBackspace -text Backspace -command ".maintext.t delete {insert - 1 chars} {insert}"] -side left
+pack [button .frame_kb.r$r.bBackspace -text Backspace -command {pressed BackSpace}] -side left
 set SPACE { }
-foreach {l c} {Tab \\t Space $SPACE Line \\n} {
-	pack [button .frame_kb.r$r.b$l -text $l -command ".maintext.t insert insert $c"] -side left
+foreach {l c} {Tab \\t Space $SPACE Line Enter} {
+	pack [button .frame_kb.r$r.b$l -text $l -command "pressed $c"] -side left
 }
 pack configure .frame_kb.r$r.bSpace -fill x -expand 1
 pack [button .frame_kb.r$r.bShift -text Shift -command shift_last] -side left -fill x
 pack [button .frame_kb.r$r.bShiftL -text L -command shift_lock] -side left -fill x
-pack [button .frame_kb.r$r.bDelete -text Delete -command ".maintext.t delete {insert} {insert + 1 chars}"] -side left
+pack [button .frame_kb.r$r.bDelete -text Delete -command {pressed Delete}] -side left
 
 pack [labelframe .frame_sp -text Symbols] -side left -anchor ne
 set r 0
 foreach {s} {{`-=} {[]} {;'} {,/}} {
 	pack [frame .frame_sp.r$r] -fill x
 	foreach {c} [split $s {}] {
-		pack [button .frame_sp.r$r.b$c -text $c -command ".maintext.t insert insert \{$c\}"] -fill both -expand 1 -side left
+		pack [button .frame_sp.r$r.b$c -text $c -command "pressed \{$c\}"] -fill both -expand 1 -side left
 	}
 	incr r
 }
-pack [button .frame_sp.r1.bBackslash -text \\ -command ".maintext.t insert insert \\"] -fill both -expand 1 -side left
-pack [button .frame_sp.r3.bPeriod -text . -command ".maintext.t insert insert ."] -fill both -expand 1 -side left
+pack [button .frame_sp.r1.bBackslash -text \\ -command "pressed \\"] -fill both -expand 1 -side left
+pack [button .frame_sp.r3.bPeriod -text . -command "pressed ."] -fill both -expand 1 -side left
 proc rep_indices_with {b e c} {
 	.maintext.t delete $b $e
 	.maintext.t insert $b $c
@@ -57,40 +66,28 @@ set r 0
 foreach {s} {{789/} {456*} {123-} {0+}} {
 	pack [frame .frame_np.r$r] -fill x
 	foreach {c} [split $s {}] {
-		pack [button .frame_np.r$r.b$c -text $c -command ".maintext.t insert insert $c"] -fill both -expand 1 -side left
+		pack [button .frame_np.r$r.b$c -text $c -command "pressed $c"] -fill both -expand 1 -side left
 	}
 	incr r
 }
 pack [frame .frame_np.r4] -fill x
 pack [button .frame_np.r4.bLock -text L] -side left
-pack [button .frame_np.r4.bEnter -text = -command ".maintext.t insert insert \\n"] -fill both -expand 1 -side left
+pack [button .frame_np.r4.bEnter -text = -command "pressed \\n"] -fill both -expand 1 -side left
 
 proc sel_or_insert {} {
 	lassign [.maintext.t tag ranges sel] b e
 	if {[string length $b]==0} {
 		set e [.maintext.t index insert]
 		lassign [split $e .] r c
-		if {$c==0} { return "0 0" }
+		if {$c==0} { return }
 		set b $r.[expr {$c-1}]
 	}
 	return "$b $e"
 }
+proc have_selection {} {
+	return [expr [string length [.maintext.t tag ranges sel]]>0]
+}
 proc alternate {s} {
-##
- #set S {}
- # 	foreach {c} [split $s {}] {
- # 		if {[string is lower $c]} {
- # 			append S [string toupper $c]
- # 		} elseif {[string is upper $c]} {
- # 			append S [string tolower $c]
- # 		} elseif {[string is digit $c]} {
- # 			append S [string map {1 ! 2 @ 3 # 4 $ 5 % 6 ^ 7 & 8 * 9 ( 0 )} $c]
- # 		} else {
- # 			append S [string map {! 1 @ 2 # 3 $ 4 % 5 ^ 6 & 7 * 8 ( 9 ) 0} $c]
- # 		}
- # 	}
- # 	rep_indices_with $b $e $S
- ##
 	return [string map {
 		! 1 @ 2 # 3 $ 4 % 5 ^ 6 & 7 * 8 ( 9 ) 0
 		1 ! 2 @ 3 # 4 $ 5 % 6 ^ 7 & 8 * 9 ( 0 )
@@ -117,11 +114,141 @@ proc shift_lock {} {
 	for {set i 0} {$i<4} {incr i} {
 		foreach {c} [winfo children .frame_kb.r$i] {
 			set C [alternate [$c cget -text]]
-			$c configure -text $C -command ".maintext.t insert insert $C"
+			#$c configure -text $C -command ".maintext.t insert insert $C"
+			$c configure -text $C -command "pressed $C"
 		}
 		foreach {c} [winfo children .frame_sp.r$i] {
 			set C [alternate [$c cget -text]]
-			$c configure -text $C -command ".maintext.t insert insert \{$C\}"
+			#$c configure -text $C -command ".maintext.t insert insert \{$C\}"
+			$c configure -text $C -command "pressed \{$C\}"
 		}
 	}
+}
+proc kbackspace {} {
+	if {[have_selection]} {
+		.maintext.t delete sel.first sel.last
+	} else {
+		.maintext.t delete {insert - 1 chars} insert
+	}
+}
+proc kdelete {} {
+	if {[have_selection]} {
+		.maintext.t delete sel.first sel.last
+	} else {
+		.maintext.t delete insert {insert + 1 chars}
+	}
+}
+proc pressed {c} {
+	set o $osnv::MAC
+	switch $o {
+		off {
+			switch $c {
+				BackSpace {
+					kbackspace
+				}
+				Delete {
+					kdelete
+				}
+				Enter {
+					.maintext.t insert insert \n
+				}
+				default {
+					if {[have_selection]} {
+						.maintext.t delete sel.first sel.last
+					} 
+					.maintext.t insert insert $c
+				}
+			}
+		}
+		rep {
+			switch $c {
+				BackSpace {
+					set osnv::s [string range $osnv::s 0 [string length $osnv::s]-2]
+					.maintext.t delete {insert - 1 chars} insert
+				}
+				Delete {
+					set osnv::s {}
+					if {!$osnv::locked} {
+						.frame_m_bar.off select
+					}
+				}
+				1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 0 {
+					for {set i 0} {$i<$c} {incr i} {
+						.maintext.t insert insert $osnv::s
+					}
+					set osnv::s {}
+					if {!$osnv::locked} {
+						.frame_m_bar.off select
+					}
+				}
+				Enter {
+					append osnv::s \n
+					.maintext.t insert insert \n
+					
+				}
+				default {
+					append osnv::s $c
+					.maintext.t insert insert $c
+				}
+			}
+			#~ if {[string is digit $c]} {
+				#~ for {set i 0} {$i<$c} {incr i} {
+					#~ .maintext.t insert insert $osnv::s
+				#~ }
+				#~ set osnv::s {}
+				#~ if {!$osnv::locked} {
+					#~ .frame_m_bar.off select
+				#~ }
+			#~ } else {
+				#~ append osnv::s $c
+				#~ .maintext.t insert insert $c
+			#~ }
+		}
+		reg {
+			switch $c {
+				{ } {
+					.maintext.mac_words.lb insert end $osnv::s
+					set osnv::s {}
+					if {!$osnv::locked} {
+						.frame_m_bar.off select
+					}
+				}
+				Enter {
+					.maintext.t insert insert [.maintext.mac_words.lb get active]
+					if {!$osnv::locked} {
+						.frame_m_bar.off select
+					}
+				}
+				Delete {
+					.maintext.mac_words.lb delete end
+					set osnv::s {}
+					if {!$osnv::locked} {
+						.frame_m_bar.off select
+					}
+				}
+				BackSpace {
+					set osnv::s [string range $osnv::s 0 [string length $osnv::s]-2]
+				}
+				default {
+					append osnv::s $c
+					.maintext.t insert insert $c
+				}
+			}
+		}
+	}
+}
+proc mac_off {} {
+	set osnv::s {}
+}
+bind .maintext.t <KeyPress> {
+	if {[string length %K]==1 && [string is alnum %K]} {
+		pressed %K
+		break
+	} elseif {{%K}=={space}} {
+		pressed { }
+		break
+	}
+}
+bind .maintext.t <Control-Key-q> {
+	exit
 }
