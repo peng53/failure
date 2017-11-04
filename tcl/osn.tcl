0196e1bfc359a15edfbd408b20c4d30c88644c3a
Perry Eng <eng.perry@gmail.com>
2017-11-04 08:09:03 -0500

    Changed how macros work if text is selected.
    Rearranged symbols pad & removed repeated symbols in numpad.
    Renamed some buttons in alpha/number bottom row.
    Moved ShiftLock (and NumLock) to macro bar.
    Changed macros to have uniform 'box' look.
    Adjusted 'lock' macro to update the s index at completion.
    New double zero button on numpad.
    
    Etc

--- tcl/osn.tcl
+++ tcl/osn.tcl
@@ -19,10 +19,12 @@ pack [scrollbar .maintext.sb_v -command {.maintext.t yview}] -side right -fill y
 pack [text .maintext.t -wrap none -yscrollcommand {.maintext.sb_v set} -xscrollcommand {.maintext.sb_h set}] -expand 1 -fill both
 pack [scrollbar .maintext.sb_h -orient horizontal -command {.maintext.t xview}] -side bottom -fill x
 pack [labelframe .frame_m_bar -text Macro] -fill x
-pack [radiobutton .frame_m_bar.off -text Off -variable osnv::MAC -value off -command mac_off] -side left
-pack [radiobutton .frame_m_bar.rep -text Repeat -variable osnv::MAC -value rep -command mac_on] -side left
-pack [radiobutton .frame_m_bar.reg -text Register -variable osnv::MAC -value reg -command mac_on] -side left
-pack [checkbutton .frame_m_bar.lock -text Lock -variable osnv::locked -onvalue 1 -offvalue 0] -side left
+pack [radiobutton .frame_m_bar.off -indicatoron 0 -text Off -variable osnv::MAC -value off -command mac_off] -fill y -side left
+pack [radiobutton .frame_m_bar.rep -indicatoron 0 -text Repeat -variable osnv::MAC -value rep -command mac_on] -fill y -side left
+pack [radiobutton .frame_m_bar.reg -indicatoron 0 -text Register -variable osnv::MAC -value reg -command mac_on] -fill y -side left
+pack [checkbutton .frame_m_bar.lock -indicatoron 0 -text Lock -variable osnv::locked -onvalue 1 -offvalue 0] -fill y -side left
+pack [button .frame_m_bar.slck -text ShiftL -command shift_lock] -side left
+#pack [button .frame_m_bar.nlck -text NumL] -side left
 pack [button .frame_m_bar.all -text All -command {.maintext.t tag add sel 0.0 end}] -side left
 pack [button .frame_m_bar.text -text Test -command {puts $osnv::MAC}] -side left
 pack [labelframe .frame_kb -text Keyboard] -expand 1 -fill x -side left
@@ -35,42 +37,40 @@ foreach {s} {{1234567890} {qwertyuiop} {asdfghjkl} {zxcvbnm}} {
 	incr r
 }
 pack [frame .frame_kb.r$r] -fill x
-pack [button .frame_kb.r$r.bBackspace -text Backspace -command kbackspace] -side left
-set SPACE { }
-foreach {l c} {Tab \\t Space $SPACE Line Enter} {
+pack [button .frame_kb.r$r.bBackspace -text >) -command kbackspace] -side left
+foreach {l c} {==> \\t Space space LF Enter} {
 	pack [button .frame_kb.r$r.b$l -text $l -command "pressed $c"] -side left
 }
 pack configure .frame_kb.r$r.bSpace -fill x -expand 1
-pack [button .frame_kb.r$r.bShift -text Shift -command shift_last] -side left -fill x
-pack [button .frame_kb.r$r.bShiftL -text L -command shift_lock] -side left -fill x
-pack [button .frame_kb.r$r.bDelete -text Delete -command {pressed Delete}] -side left
+pack [button .frame_kb.r$r.bShift -text q/Q -command shift_last] -side left -fill x
+pack [button .frame_kb.r$r.bDelete -text (< -command {pressed Delete}] -side left
 
 pack [labelframe .frame_sp -text Symbols] -side left -anchor ne
 set r 0
-foreach {s} {{`-=} {[]} {;'} {,/}} {
-	pack [frame .frame_sp.r$r] -fill x
+foreach {s} {{`'/} {[]*} {;-+} {,}} {
+	pack [frame .frame_sp.r$r]
 	foreach {c} [split $s {}] {
-		pack [button .frame_sp.r$r.b$c -text $c -command "wri_rep \\$c"] -fill both -expand 1 -side left
+		pack [button .frame_sp.r$r.b$c -text $c -width 1 -command "wri_rep \\$c"] -side left
 	}
 	incr r
 }
-pack [button .frame_sp.r1.bBackslash -text \\ -command {wri_rep \\}] -fill both -expand 1 -side left
-pack [button .frame_sp.r3.bPeriod -text . -command {wri_rep .}] -fill both -expand 1 -side left
+pack [button .frame_sp.r3.bPeriod -text . -width 1 -command {wri_rep .}] -side left
+pack [button .frame_sp.r3.bBackslash -text \\ -width 1 -command {wri_rep \\}] -side left
 proc rep_indices_with {b e c} {
 	.maintext.t delete $b $e
 	.maintext.t insert $b $c
 }
 pack [labelframe .frame_np -text Num] -side left -anchor ne
 set r 0
-foreach {s} {{789/} {456*} {123-} {0+}} {
+foreach {s} {{789} {456} {123} {0}} {
 	pack [frame .frame_np.r$r] -fill x
 	foreach {c} [split $s {}] {
 		pack [button .frame_np.r$r.b$c -text $c -command "pressed $c"] -fill both -expand 1 -side left
 	}
 	incr r
 }
+pack [button .frame_np.r3.bDZero -text 00 -command {pressed 00}] -side left
 pack [frame .frame_np.r4] -fill x
-pack [button .frame_np.r4.bLock -text L] -side left
 pack [button .frame_np.r4.bEnter -text = -command {pressed Enter}] -fill both -expand 1 -side left
 
 proc have_selection {} {
@@ -137,46 +137,73 @@ proc pressed {c} {
 			switch $c {
 				Delete { kdelete }
 				Enter { .maintext.t insert insert \n }
+				space { wri_rep { } }
 			}
 		}
 		rep {
 			switch $c {
+				Enter { .maintext.t insert insert \n }
 				Delete {
-					set osnv::s {}
-					if {!$osnv::locked} { .frame_m_bar.off select }
+					if {!$osnv::locked} {
+						mac_off
+					} else {
+						mac_on
+					}
 				}
 				1 - 2 - 3 - 4 - 5 - 6 - 7 - 8 - 9 - 0 {
-					set s [.maintext.t get $osnv::s insert]
+					if [have_selection] {
+						set s [.maintext.t get sel.first sel.last]
+					} else {
+						set s [.maintext.t get $osnv::s insert]
+					}
 					for {set i 0} {$i<$c} {incr i} {
 						.maintext.t insert insert $s
 					}
-					set osnv::s {}
-					if {!$osnv::locked} { .frame_m_bar.off select }
+					if {!$osnv::locked} {
+						mac_off
+					} else {
+						mac_on
+					}
 				}
-				Enter { .maintext.t insert insert \n }
 			}
 		}
 		reg {
 			switch $c {
-				{ } {
-					.maintext.mac_words.lb insert end [.maintext.t get $osnv::s insert]
-					set osnv::s {}
-					if {!$osnv::locked} { .frame_m_bar.off select }
+				space {
+					if [have_selection] {
+						set s [.maintext.t get sel.first sel.last]
+					} else {
+						set s [.maintext.t get $osnv::s insert]
+					}
+					.maintext.mac_words.lb insert end $s
+					if {!$osnv::locked} {
+						mac_off
+					} else {
+						mac_on
+					}
 				}
 				Enter {
 					.maintext.t insert insert [.maintext.mac_words.lb get active]
-					if {!$osnv::locked} { .frame_m_bar.off select }
+					if {!$osnv::locked} {
+						mac_off
+					} else {
+						mac_on
+					}
 				}
 				Delete {
 					.maintext.mac_words.lb delete end
-					set osnv::s {}
-					if {!$osnv::locked} { .frame_m_bar.off select }
+					if {!$osnv::locked} {
+						mac_off
+					} else {
+						mac_on
+					}
 				}
 			}
 		}
 	}
 }
 proc mac_off {} {
+	.frame_m_bar.off select
 	set osnv::s {}
 }
 proc mac_on {} {
@@ -189,5 +216,7 @@ bind .maintext.t <Delete> {
 	pressed Delete
 }
 bind .maintext.t <space> {
-	pressed { }
+	if {$osnv::MAC=={reg}} {
+		pressed space
+	}
 }
