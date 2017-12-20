59ae241afb7537f98147316318ee47fe18ff4f60
Perry Eng <eng.perry@gmail.com>
2017-12-20 14:35:12 -0600

    2nd revision I had.
    Looks like the main change was from a vector of char for rf
    (which I think is the users 'revealed field') to a vector of
    bool.
    
    I think around this time I found out that vector<bool> was
    inefficent? (after this)

--- mines/main.cpp
+++ mines/main.cpp
@@ -51,35 +51,37 @@ void printfus(std::vector<char> &aj,short hw,short t){
 	}
 	putchar('\n');
 }
-void printrf(std::vector<char> &rf, short hw, short t,short left){
+void printrf(std::vector<bool> &rf,std::vector<char> &aj,short hw, short t,short left){
 	putchar('/');
 	for (short i=0;i<hw;i++) printf("%d",i%10);
 	printf("->left:%d",left);
 	for (short i=0;i<t;++i){
 		if (i%hw==0) printf("\n%d",(i/hw)%10);
-		switch (rf[i]){
-			case -2: putchar('.'); break;
-			case -1: putchar('*'); break;
-			case 0: putchar(' '); break;
-			default: printf("%d",rf[i]); break;
-		}
+		if (rf[i]){
+			switch (aj[i]){
+				case -1: putchar('*'); break;
+				case 0: putchar(' '); break;
+				default: printf("%d",aj[i]); break;
+			}
+		} else putchar('.');
+
 	}
 	putchar('\n');
 }
 short xyn(short x,short y,short hw){
 	return (hw*y)+x;
 }
-void rfil(std::vector<char> &aj,std::vector<char> &rf, short n,short hw,short &l){
-	if (rf[n]!=-2) return; // no change
+void rfil(std::vector<char> &aj,std::vector<bool> &rf, short n,short hw,short &l){
+	if (rf[n]) return; // no change
 	if (aj[n]!=0){
 		if (aj[n]!=-1){
-			rf[n]=aj[n];
+			rf[n]=1;
 			l--;
 		}
 		return;
 	}
 	l--;
-	rf[n]=0;
+	rf[n]=1;
 	bool tw=(n%hw)!=0;
 	bool te=(n%hw)!=hw-1;
 	if (tw) rfil(aj,rf,n-1,hw,l);
@@ -95,17 +97,17 @@ void rfil(std::vector<char> &aj,std::vector<char> &rf, short n,short hw,short &l
 		if (te) rfil(aj,rf,n+hw+1,hw,l);
 	}
 }
-void pickc(std::vector<char> &aj,std::vector<char> &rf, short n,short hw,short &l){
-	if (rf[n]!=-2) return;
+void pickc(std::vector<char> &aj,std::vector<bool> &rf, short n,short hw,short &l){
+	if (rf[n]) return;
 	if (aj[n]==-1){
-		rf[n]=-1;
+		rf[n]=1;
 		l=-1;
 		return;
 	}
 	rfil(aj,rf,n,hw,l);
 }
-void revil(std::vector<char> &rf, std::vector<short> &mines,short m){
-	for (short i=0;i<m;++i) rf[mines[i]]=-1;
+void revil(std::vector<bool> &rf, std::vector<short> &mines,short m){
+	for (short i=0;i<m;++i) rf[mines[i]]=1;
 }
 int main(int argc,char *argv[]){
 	short hw = (argc>1) ? atoi(argv[1]) : 10;
@@ -118,18 +120,18 @@ int main(int argc,char *argv[]){
 	std::vector<short> mines(t);
 	std::vector<char> aj = nfield(mines,hw,t,m);
 	printfus(aj,hw,t);
-	std::vector<char> rf(t,-2);
-	printrf(rf,hw,t,left);
+	std::vector<bool> rf(t,0);
+	printrf(rf,aj,hw,t,left);
 	while (left>=1){
 		printf("Enter x,y: ");
 		scanf("%hd,%hd",&x,&y);
 		if (x<0 || y<0 || x>hw || y>hw) continue;
 		pickc(aj,rf,xyn(x,y,hw),hw,left);
-		printrf(rf,hw,t,left);
+		printrf(rf,aj,hw,t,left);
 	}
 	if (left!=0){
 		revil(rf,mines,m);
-		printrf(rf,hw,t,left);
+		printrf(rf,aj,hw,t,left);
 	}
 	printf("you %s\n",(left==0) ? "win" : "lose");
 	return 0;
