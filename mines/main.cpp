5da7faa923ce6806a6fbe4bada450303a94b0a8d
Perry Eng <eng.perry@gmail.com>
2017-12-20 14:44:27 -0600

    7th revision I had on disk (5,6,7 were the ones I saved)
    Looks like a massive change (doesn't seem like the same
    program..) removing the use of argv to a compile time determined
    mine count and field size using macros.
    
    Also for whatever reason I changed from using vector to raw arrays
    (which was made possible by the previous point)
    
    Last major point is change from shuffle from algorithm to use of
    rand from cstdlib, implementing the shuffle naively.
    
    Wondering if this change easily be compiled as C..

--- mines/main.cpp
+++ mines/main.cpp
@@ -1,47 +1,66 @@
 #include <cstdio>
-#include <vector>
 #include <cstdlib>
-#include <algorithm>
 #include <ctime>
 
-std::vector<char> nfield(std::vector<short> &mines,short hw,short t,short m){
-	for (short i=0;i<t;++i) mines[i]=i;
+#define N_MINES 20
+#define N_WIDTH 10
+#define N_CELLS 100
+#define T_CELLS short
+
+void rmine(T_CELLS (&mines)[N_CELLS]){
 	srand(time(NULL));
-	std::random_shuffle(mines.begin(),mines.end());
-	mines.resize(m);
-	std::vector<char> aj(t,0);
+	T_CELLS j,t;
+	for (T_CELLS i=N_CELLS-1;i>0;--i){
+		j=rand()%i;
+		t=mines[j];
+		mines[j]=mines[i];
+		mines[i]=t;
+	}
+}
+void nfields(char (&aj)[N_CELLS],T_CELLS (&mines)[N_CELLS]){
 	bool tw,te;
 	short n;
-	for (short i=0;i<m;++i){
+	for (T_CELLS i=N_MINES;i>0;--i){
 		n=mines[i];
 		aj[n]=-1;
-		tw=(n%hw)!=0;
-		te=(n%hw)!=hw-1;
+		tw=(n%N_WIDTH)!=0;
+		te=(n%N_WIDTH)!=N_WIDTH-1;
 		if (tw && aj[n-1]!=-1) ++aj[n-1];
 		if (te && aj[n+1]!=-1) ++aj[n+1];
-		if (n>hw){
-			if (aj[n-hw]!=-1) ++aj[n-hw];
-			if (tw && aj[n-hw-1]!=-1) ++aj[n-hw-1];
-			if (te && aj[n-hw+1]!=-1) ++aj[n-hw+1];
+		if (n>N_WIDTH){
+			if (aj[n-N_WIDTH]!=-1) ++aj[n-N_WIDTH];
+			if (tw && aj[n-N_WIDTH-1]!=-1) ++aj[n-N_WIDTH-1];
+			if (te && aj[n-N_WIDTH+1]!=-1) ++aj[n-N_WIDTH+1];
 		}
-		if (n<(hw*(hw-1))){
-			if (aj[n+hw]!=-1) ++aj[n+hw];
-			if (tw && aj[n+hw-1]!=-1) ++aj[n+hw-1];
-			if (te && aj[n+hw+1]!=-1) ++aj[n+hw+1];
+		if (n<(N_WIDTH*(N_WIDTH-1))){
+			if (aj[n+N_WIDTH]!=-1) ++aj[n+N_WIDTH];
+			if (tw && aj[n+N_WIDTH-1]!=-1) ++aj[n+N_WIDTH-1];
+			if (te && aj[n+N_WIDTH+1]!=-1) ++aj[n+N_WIDTH+1];
 		}
 	}
-	return aj;
 }
-void printfus(std::vector<char> &aj,short hw,short t){
+void printfus(char (&aj)[N_CELLS]){
+	for (T_CELLS i=0;i<N_CELLS;++i){
+		if (i%N_WIDTH==0) putchar('\n');
+		printf("(%2d)",i);
+		switch (aj[i]){
+			case -1: putchar('*');break;
+			case 0: putchar(' ');break;
+			default: printf("%d",aj[i]); break;
+		}
+	}
+	putchar('\n');
+}
+void printfus2(char (&aj)[N_CELLS]){
 	putchar('/');
-	for (short i=0;i<hw;i++) printf("%d",i%10);
+	for (short i=0;i<N_WIDTH;++i) printf("%d",i%10);
 	putchar('+');
-	for (short i=0;i<hw;i++) printf("%d",i%10);
-	for (short i=0;i<t;i+=hw){
-		printf("\n%d",(i/hw)%10);
-		for (short j=i;((j==i) || (j%hw!=0)); ++j) putchar((aj[j]==-1) ? '*' : ' ');
+	for (short i=0;i<N_WIDTH;++i) printf("%d",i%10);
+	for (short i=0;i<N_CELLS;i+=N_WIDTH){
+		printf("\n%d",(i/N_WIDTH)%10);
+		for (short j=i;((j==i) || (j%N_WIDTH!=0)); ++j) putchar((aj[j]==-1) ? '*' : ' ');
 		putchar('|');
-		for (short j=i;((j==i) || (j%hw!=0)); ++j){
+		for (short j=i;((j==i) || (j%N_WIDTH!=0)); ++j){
 			switch (aj[j]){
 				case 0: putchar(' '); break;
 				case -1: putchar('*'); break;
@@ -51,88 +70,102 @@ void printfus(std::vector<char> &aj,short hw,short t){
 	}
 	putchar('\n');
 }
-void printrf(std::vector<bool> &rf,std::vector<char> &aj,short hw, short t,short left){
-	putchar('/');
-	for (short i=0;i<hw;i++) printf("%d",i%10);
-	printf("->left:%d",left);
-	for (short i=0;i<t;++i){
-		if (i%hw==0) printf("\n%d",(i/hw)%10);
+/*
+void printrf(bool (&rf)[N_CELLS],char (&aj)[N_CELLS]){
+	for (T_CELLS i=0;i<N_CELLS;++i){
+		if (i%N_WIDTH==0) putchar('\n');
 		if (rf[i]){
 			switch (aj[i]){
-				case -1: putchar('*'); break;
-				case 0: putchar(' '); break;
-				default: printf("%d",aj[i]); break;
+				case -1: printf("%4s","*"); break;
+				case 0: printf("%4s"," "); break;
+				default: printf("%4d",aj[i]); break;
 			}
-		} else putchar('.');
-
+		} else {
+			printf("(%2d)",i);
+		}
 	}
 	putchar('\n');
 }
-short xyn(short x,short y,short hw){
-	return (hw*y)+x;
+*/
+T_CELLS get_r(T_CELLS n){
+	return n/N_WIDTH;
 }
-void rfil(std::vector<char> &aj,std::vector<bool> &rf, short n,short hw,short &l){
-	if (rf[n]) return; // no change
-	if (aj[n]!=0){
-		if (aj[n]!=-1){
-			rf[n]=1;
-			l--;
-		}
-		return;
+T_CELLS get_c(T_CELLS n){
+	return n%N_WIDTH;
+}
+void printrf(bool (&rf)[N_CELLS],char (&aj)[N_CELLS]){
+	printf("%2s|"," ");
+	for (T_CELLS i=0;i<N_WIDTH;++i) printf("%3d",i);
+	for (T_CELLS i=0;i<N_CELLS;++i){
+		if (i%N_WIDTH==0) printf("\n%2d|",i/N_WIDTH);
+		if (rf[i]){
+			switch (aj[i]){
+				case -1: printf("%3s","*"); break;
+				case 0: printf("%3s"," "); break;
+				default: printf("%3d",aj[i]); break;
+			}
+		} else printf("%3s","+");
 	}
-	l--;
+	putchar('\n');
+}
+T_CELLS rfil(char (&aj)[N_CELLS],bool (&rf)[N_CELLS],T_CELLS n,T_CELLS left){
+	if (rf[n]) return left;
+	if (aj[n]==-1) return left;
 	rf[n]=1;
-	bool tw=(n%hw)!=0;
-	bool te=(n%hw)!=hw-1;
-	if (tw) rfil(aj,rf,n-1,hw,l);
-	if (te) rfil(aj,rf,n+1,hw,l);
-	if (n>hw){
-		rfil(aj,rf,n-hw,hw,l);
-		if (tw) rfil(aj,rf,n-hw-1,hw,l);
-		if (te) rfil(aj,rf,n-hw+1,hw,l);
+	/*
+	--left;
+	if (aj[n]!=0) return left;
+	*/
+	if (aj[n]!=0) return left-1;
+	bool tw=(n%N_WIDTH)!=0;
+	bool te=(n%N_WIDTH)!=N_WIDTH-1;
+	if (tw) left=rfil(aj,rf,n-1,left);
+	if (te) left=rfil(aj,rf,n+1,left);
+	if (n>N_WIDTH){
+		left=rfil(aj,rf,n-N_WIDTH,left);
+		if (tw) left=rfil(aj,rf,n-N_WIDTH-1,left);
+		if (te) left=rfil(aj,rf,n-N_WIDTH+1,left);
 	}
-	if (n<(hw)*(hw-1)){
-		rfil(aj,rf,n+hw,hw,l);
-		if (tw) rfil(aj,rf,n+hw-1,hw,l);
-		if (te) rfil(aj,rf,n+hw+1,hw,l);
+	if (n<(N_WIDTH)*(N_WIDTH-1)){
+		left=rfil(aj,rf,n+N_WIDTH,left);
+		if (tw) left=rfil(aj,rf,n+N_WIDTH-1,left);
+		if (te) left=rfil(aj,rf,n+N_WIDTH+1,left);
 	}
+	//return left;
+	return --left;
 }
-void pickc(std::vector<char> &aj,std::vector<bool> &rf, short n,short hw,short &l){
-	if (rf[n]) return;
+T_CELLS pickc(char (&aj)[N_CELLS],bool (&rf)[N_CELLS],T_CELLS n,T_CELLS left){
+	//printf("left start at %d\n",left);
+	if (rf[n]) return left;
 	if (aj[n]==-1){
 		rf[n]=1;
-		l=-1;
-		return;
+		return -1;
 	}
-	rfil(aj,rf,n,hw,l);
+	return rfil(aj,rf,n,left);
 }
-void revil(std::vector<bool> &rf, std::vector<short> &mines,short m){
-	for (short i=0;i<m;++i) rf[mines[i]]=1;
-}
-int main(int argc,char *argv[]){
-	short hw = (argc>1) ? atoi(argv[1]) : 10;
-	short m = (argc>2) ? atoi(argv[2]) : 10;
-	if (hw<1 || m<1) return 1;
-	short t = hw*hw;
-	if (t<m) return 1;
-	short x,y;
-	short left = t-m;
-	std::vector<short> mines(t);
-	std::vector<char> aj = nfield(mines,hw,t,m);
-	printfus(aj,hw,t);
-	std::vector<bool> rf(t,0);
-	printrf(rf,aj,hw,t,left);
-	while (left>=1){
-		printf("Enter x,y: ");
-		scanf("%hd,%hd",&x,&y);
-		if (x<0 || y<0 || x>hw || y>hw) continue;
-		pickc(aj,rf,xyn(x,y,hw),hw,left);
-		printrf(rf,aj,hw,t,left);
+int main(){
+	//multigame
+	T_CELLS left,n;
+	T_CELLS mines[N_CELLS];
+	char aj[N_CELLS];
+	bool rf[N_CELLS];
+	while (1){
+		left = N_CELLS - N_MINES;
+		for (T_CELLS i=N_CELLS-1;i>=0;--i){
+			aj[i]=rf[i]=0;
+			mines[i]=i;
+		}
+		rmine(mines);
+		nfields(aj,mines);
+		printfus2(aj);
+		while (left>0){
+			printrf(rf,aj);
+			scanf("%d",&n);
+			if (n<0 || n>=N_CELLS) continue;
+			left=pickc(aj,rf,n,left);
+		}
+		//printf("%d\n",left);
+		printrf(rf,aj);
 	}
-	if (left!=0){
-		revil(rf,mines,m);
-		printrf(rf,aj,hw,t,left);
-	}
-	printf("you %s\n",(left==0) ? "win" : "lose");
 	return 0;
 }
