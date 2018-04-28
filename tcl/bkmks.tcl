5fb33f249147c21080e7adbc1924cb7e3c241155
Perry Eng <eng.perry@gmail.com>
2018-04-28 10:57:12 -0500

    tcl/bkmks.tcl
    Made modify window which can raed & write to treeview.
    ATM no sql is implemented so the data is just stuck on the
    treeview.

--- tcl/bkmks.tcl
+++ tcl/bkmks.tcl
@@ -3,9 +3,9 @@ menu .men
 . configure -menu .men
 .men add command -label Open
 .men add command -label Save
-.men add command -label Close
-.men add command -label {Add Link}
-.men add command -label Modify
+.men add command -label Close -command {destroy .}
+.men add command -label {Add Link} -command {modify_row -1}
+.men add command -label Modify -command {mod_if_sel [.tv_links selection]}
 .men add command -label Delete
 pack [ttk::treeview .tv_links -columns {title url mtime}] -side left -fill both -expand 1
 pack [scrollbar .tv_links_sb] -side left -fill y
@@ -13,7 +13,44 @@ foreach {c l w} [list #0 # 32 title Name 128 url URL 256 mtime {Time Modified} 1
 	.tv_links heading $c -text $l -anchor w
 	.tv_links column $c -minwidth 16 -width $w
 }
-
-
-.tv_links insert "" end -text 1 -values [list {Some Site} http://123fakesite.456 18-04-28]
+proc mod_if_sel {rownumber} {
+	if {[string length $rownumber] != 0} {
+		modify_row $rownumber
+	} else {
+		tk_messageBox -type ok -icon error -message {Please select link to modify.}
+	}
+}
+proc save_row {rownumber} {
+	puts $rownumber
+	set t [.win_row_mod.t.e get]
+	set u [.win_row_mod.u.e get]
+	puts $t
+	puts $u
+	if {$rownumber == -1} {
+		# note : need sql for get id (which is also text)
+		.tv_links insert {} end -value [list $t $u ?NEWDATE?]
+	} else {
+		.tv_links item $rownumber -value [list $t $u ?NEWDATE?]
+	}
+	destroy .win_row_mod
+}
+proc modify_row {rownumber} {
+	toplevel .win_row_mod
+	wm title .win_row_mod {Modify Row}
+	wm resizable .win_row_mod 0 0
+	pack [labelframe .win_row_mod.t -text Title]
+	pack [entry .win_row_mod.t.e]
+	pack [labelframe .win_row_mod.u -text URL]
+	pack [entry .win_row_mod.u.e]
+	pack [frame .win_row_mod.buttons] -side bottom
+	pack [button .win_row_mod.buttons.save -text Save -command "save_row $rownumber"] -side left
+	pack [button .win_row_mod.buttons.cancel -text Cancel -command {destroy .win_row_mod}] -side left
+	if {$rownumber != -1} {
+		lassign [.tv_links item $rownumber -values] t u
+		.win_row_mod.t.e insert 0 $t
+		.win_row_mod.u.e insert 0 $u
+	}
+}
+.tv_links insert {} end -id 0 -text 0 -values [list {Some Site} http://123fakesite.456 18-04-28]
+.tv_links insert {} end -id 1 -text 1 -values [list {Some Site} http://123fakesite.456 18-04-28]
 
