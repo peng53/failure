3a5c4c7d5681ae5280b96f5b5b2b6eda8d1efa44
Perry Eng <eng.perry@gmail.com>
2018-08-05 09:28:29 -0500

    test

--- trie_fun/wordbank.h
+++ trie_fun/wordbank.h
@@ -14,29 +14,42 @@
 #include <iostream>
 using std::vector;
 using std::string;
-using std::ofstream;
+using std::ostream;
 
 struct Node;
-
 class WordBank {
 	private:
 		Node* p[26];
+		Node* prefix(const char* s,size_t lc);
 	public:
 		WordBank();
 		~WordBank();
+		bool prefix_exists(const string& prefix,size_t lc);
 		bool prefix_exists(const string& prefix);
+		bool prefix_exists(const char* s,size_t lc);
+		bool prefix_exists(const char* s);
 		vector<char> next_possible_letters(string& prefix);
 		vector<string> with_prefix(string& prefix);
+
+		void add_word(const string& w,size_t letters);
 		void add_word(const string& w);
+		void add_word(const char* w,size_t letters);
 		void add_word(const char* w);
+
+		void remove_word(const char* w,size_t lc);
+		void remove_word(const char* w);
+		void remove_word(const string& w,size_t lc);
 		void remove_word(const string& w);
-		int import_words(ofstream& o);
+
+		ostream& import_words(ostream& o);
 		int import_words(const vector<string>& o);
-		int export_words(ofstream& o);
+		ostream& export_words(ostream& o);
 		vector<string> as_vector();
 		int count_words();
-		int with_prefix_count_words(const string& prefix);
+		int with_prefix_count_word(const string& prefix);
 		int with_prefix_remove_words(const string& prefix);
+		bool operator[](const string &key);
+		bool operator[](const char* s);
 };
 
 #endif
