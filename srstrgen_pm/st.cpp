bb2f99e3a9a9cce8179469190fde69de30eba8f4
Perry Eng <eng.perry@gmail.com>
2018-09-07 11:54:29 -0500

    st.h: 	Moved stuff to source. 	Changed lits_size to return size_t (as in ste) 	Changed friend << overloads to member overloads (as in ste)
    st.cpp:
    	Due to the 'parts' being here, there functions are now bundled
    		together.
    	Created DPart constructor that takes a const vector<unique_ptr<string>>&
    		This solves the 'witchcraft' in exchanging unique_ptr in
    			add_part 4arg.
    
    If attempting to test st, please replace:
    	#include "ste.h"
    
    in main.cpp, with:
    	#include "st.h"
    
    Also compile with new object file, etc..

--- srstrgen_pm/st.cpp
+++ srstrgen_pm/st.cpp
@@ -1,29 +1,58 @@
 #include "st.h"
 
+#include <random>
+#include <ctime>
+
+std::mt19937 RNG(time(0));
+
+struct CPart : Part {
+	string* s;
+	string* d = nullptr;
+	unsigned r = 1;
+	CPart(string* _s) : s(_s){}
+	CPart(string* _s,string* _d,unsigned _r) : s(_s),d(_d),r(_r){}
+	void out(ostream& sout){
+		for (unsigned i=r;i>0;--i){
+			sout<<*s;
+			if (i!=1) sout<<*d;
+		}
+	}
+};
+struct RPart : Part {
+	string* c;
+	unsigned l;
+	string* d = nullptr;
+	unsigned t = 1;
+	RPart(string* _c,unsigned _l) : c(_c),l(_l){};
+	RPart(string* _c,unsigned _l,string* _d,unsigned _t) : c(_c),l(_l),d(_d),t(_t){};
+	void out(ostream& sout){
+		std::uniform_int_distribution<unsigned> r(0,c->length()-1);
+		for (unsigned i=t;i>0;--i){
+			for (unsigned n=0;n<l;++n){
+				sout<< (*c)[r(RNG)];
+			}
+			if (i!=1) sout<<*d;
+		}
+	}
+};
+struct DPart : Part {
+	vector<string*> f;
+	void out(ostream& sout){
+		std::uniform_int_distribution<unsigned> r(0,f.size()-1);
+		sout << *(f[r(RNG)]);
+	}
+	DPart(const vector<unique_ptr<string>> &s, const unsigned b, const unsigned e){
+		for (unsigned i=b;i<=e; ++i){
+			f.push_back(s[i].get());
+		}
+	}
+};
+
 ostream& operator<<(ostream& sout,Part& p){
 	p.out(sout);
 	return sout;
 }
-void CPart::out(ostream& sout){
-	for (unsigned i=r;i>0;--i){
-		sout<<*s;
-		if (i!=1) sout<<*d;
-	}
-}
-std::mt19937 RNG(time(0));
-void RPart::out(ostream& sout){
-	std::uniform_int_distribution<unsigned> r(0,c->length()-1);
-	for (unsigned i=t;i>0;--i){
-		for (unsigned n=0;n<l;++n){
-			sout<< (*c)[r(RNG)];
-		}
-		if (i!=1) sout<<*d;
-	}
-}
-void DPart::out(ostream& sout){
-	std::uniform_int_distribution<unsigned> r(0,f.size()-1);
-	sout << *(f[r(RNG)]);
-}
+
 ostream& operator<<(ostream& out,PartedString &PS){
 	for (auto const &p : PS.parts){
 		out<<*p.get();
@@ -38,20 +67,20 @@ std::unique_ptr<T> make_unique(Args&&... args) {
 void PartedString::add_lit(const string& s){
 	lits.push_back(make_unique<string>(s));
 }
-PartedString& operator<<(PartedString& p, const string& s){
-	p.add_lit(s);
-	return p;
+PartedString& PartedString::operator<<(const string& s){
+	add_lit(s);
+	return *this;
 }
 void PartedString::add_part(const string& s){
 	add_lit(s);
-	parts.emplace_back(make_unique<CPart>(lits.back().get()));
+	add_part(lits.size()-1);
 }
 void PartedString::add_part(const unsigned I){
 	parts.emplace_back(make_unique<CPart>(lits[I].get()));
 }
-PartedString& operator<<(PartedString& p, const unsigned i){
-	p.add_part(i);
-	return p;
+PartedString& PartedString::operator<<(const unsigned i){
+	add_part(i);
+	return *this;
 }
 void PartedString::add_part(const unsigned I,const unsigned L){
 	parts.emplace_back(make_unique<RPart>(lits[I].get(),L));
@@ -60,14 +89,12 @@ void PartedString::add_part(const unsigned I,const unsigned D,const unsigned R){
 	if (R!=0){
 		parts.emplace_back(make_unique<CPart>(lits[I].get(),lits[D].get(),R));
 	} else {
-		unique_ptr<DPart> t = make_unique<DPart>();
-		for (unsigned i=I;i<=D;++i){
-			t.get()->f.push_back(lits[i].get());
-		}
-		parts.push_back(move(t));
+		parts.emplace_back(make_unique<DPart>(lits,I,D));
 	}
 }
 void PartedString::add_part(const unsigned I,const unsigned L,const unsigned D,const unsigned W){
 	parts.emplace_back(make_unique<RPart>(lits[I].get(),L,lits[D].get(),W));
 }
-
+size_t PartedString::lits_size(){
+	return lits.size();
+}
--- srstrgen_pm/st.h
+++ srstrgen_pm/st.h
@@ -2,8 +2,6 @@
 #define MY_ST
 #include <ostream>
 #include <string>
-#include <random>
-#include <ctime>
 #include <vector>
 #include <list>
 #include <memory>
@@ -13,30 +11,9 @@ using std::string;
 using std::vector;
 using std::list;
 using std::unique_ptr;
+
 struct Part {
 	virtual void out(ostream& sout) = 0;
-};
-ostream& operator<<(ostream& sout,Part& p);
-struct CPart : Part {
-	string* s;
-	string* d = nullptr;
-	unsigned r = 1;
-	CPart(string* _s) : s(_s){}
-	CPart(string* _s,string* _d,unsigned _r) : s(_s),d(_d),r(_r){}
-	void out(ostream& sout);
-};
-struct RPart : Part {
-	string* c;
-	unsigned l;
-	string* d = nullptr;
-	unsigned t = 1;
-	RPart(string* _c,unsigned _l) : c(_c),l(_l){};
-	RPart(string* _c,unsigned _l,string* _d,unsigned _t) : c(_c),l(_l),d(_d),t(_t){};
-	void out(ostream& sout);
-};
-struct DPart : Part {
-	vector<string*> f;
-	void out(ostream& sout);
 };
 class PartedString {
 	private:
@@ -44,16 +21,16 @@ class PartedString {
 		list<unique_ptr<Part>> parts;
 		friend ostream& operator<<(ostream& out,PartedString &PS);
 		void add_lit(const string& s);
-		friend PartedString& operator<<(PartedString& p,const string& s);
-		friend PartedString& operator<<(PartedString& p,const unsigned i);
 	public:
-		unsigned lits_size(){
-			return lits.size();
-		}
+		PartedString(){}
+		~PartedString(){}
+		size_t lits_size();
 		void add_part(const string& s);
 		void add_part(const unsigned I);
 		void add_part(const unsigned I,const unsigned L);
 		void add_part(const unsigned I,const unsigned D,const unsigned R);
 		void add_part(const unsigned I,const unsigned L,const unsigned D,const unsigned W);
+		PartedString& operator<<(const string& s);
+		PartedString& operator<<(const unsigned i);
 };
 #endif
