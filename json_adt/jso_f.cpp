c3fbe02214a6182ad3aab0aaf3f978faa2c4419d
Perry Eng <eng.perry@gmail.com>
2018-11-08 09:56:55 -0600

    Made a 'head' node class resulting in Jso becoming a struct; the main reason for doing this is for a destructor which is called when the head node is destroyed only so no recursion occurs. This is taken cared with a stack-based destroyer.
    ATM making a copy constructor for JSON (because cpp-check suggests it), but
    just noticed the stack used only contains 1 ptr per run. (see trie_fun for
    how it prob should be implemented: should be the same except this one will
    only add new runs at an object)
    
    Dunno whether I should replace the stacks with queues; need to research this.

--- json_adt/jso_f.cpp
+++ json_adt/jso_f.cpp
@@ -2,15 +2,17 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <stack>
 
 using std::string;
 using std::vector;
 using std::map;
 using std::queue;
+using std::stack;
 
 enum class JType { Obj, Arr, Str, Num};
 
-class Jso {
+struct Jso {
 	JType t;
 	union {
 		string* s;
@@ -41,12 +43,18 @@ class Jso {
 				break;
 		}
 	}
-	void destroy(){
-		queue<Jso*> D;
-		Jso* d = this;
+};
+
+class JSON {
+	Jso* o;
+	public:
+	JSON(): o(new Jso(JType::Obj)){}
+	~JSON(){
+		stack<Jso*> D;
+		Jso* d = o;
 		D.push(d);
 		while (!D.empty()){
-			d = D.front();
+			d = D.top();
 			D.pop();
 			switch (d->t){
 				case JType::Num:
@@ -69,5 +77,33 @@ class Jso {
 			}
 		}
 	}
-
+	JSON(const JSON& lhs): JSON(){
+		Jso* s = lhs.o;
+		Jso* t;
+		stack<Jso*> stk;
+		stk.push(s);
+		while (!stk.empty()){
+			s = stk.top();
+			stk.pop();
+			for (auto v : *(s->x.m)){
+				switch (v.second->t){
+					case JType::Str:
+						(*(s->x.m))[v.first] = new Jso(*(v.second->x.s));
+						// Assign to map a new string:string
+						break;
+					case JType::Num:
+						(*(s->x.m))[v.first] = new Jso(v.second->x.f);
+						// x.f is just a float, so no dereferencing needed.
+						break;
+					case JType::Obj:
+						t = new Jso(JType::Obj);
+						(*(s->x.m))[v.first] = t;
+						stk.push(t);
+						break;
+					case JType::Arr:
+						break;
+				}
+			}
+		}
+	}
 };
