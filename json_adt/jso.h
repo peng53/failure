019e79c9a1804968077e61104d0599ff4415f6b3
Perry Eng <eng.perry@gmail.com>
2018-11-12 07:32:23 -0600

    Moved Jso code to jso.cpp/h. Deleted some functions for JSON and added dereference op so that operations can be called on the Obj. Made Jso::jso_out which is an iterative print (most of the work is done in adder).
    The flaw in the initial version was that I was trying to end
    the loop with i=e (setting the itr to the end point)
    But the loop condition check i!=e is checked after ++i meaning
    i goes out of bounds and i==e is never true. another way I could
    have did it was make a loop bool?

---
+++ json_adt/jso.h
@@ -0,0 +1,46 @@
+#ifndef JSOnObj
+#define JSOnObj
+
+#include <string>
+#include <vector>
+#include <map>
+#include <stack>
+#include <iostream>
+
+using std::tie;
+using std::string;
+using std::vector;
+using std::map;
+using std::stack;
+using std::pair;
+using std::ostream;
+
+enum class JType { Obj, Arr, Str, Num};
+
+struct Jso {
+	JType t;
+	union {
+		string* s;
+		float f;
+		vector<Jso*>* a;
+		map<string,Jso*>* m;
+	} x;
+	explicit Jso(float v);
+	explicit Jso(const string& v);
+	explicit Jso(JType j);
+	~Jso();
+	void key_value(const string& k,Jso* v);
+	void key_value(const string& k,const string& v);
+	void key_value(const string& k,const float v);
+	void key_value(const string& k,JType vt);
+	void add_value(const float v);
+	void add_value(const string& v);
+	void add_value(const JType vt);
+	void set_value(float v);
+	void set_value(const string& v);
+	friend std::ostream& operator<<(std::ostream& out,const Jso& J);
+	Jso* key_value(const string& k);
+	void jso_out(ostream& out);
+};
+
+#endif
