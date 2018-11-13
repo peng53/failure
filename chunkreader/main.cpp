cbec45779164d1569678a252226a9d2ab631142c
Perry Eng <eng.perry@gmail.com>
2018-11-13 09:59:53 -0600

    Issue:     fstream::get stops at \n which causes problems.     The loop in main.cpp should correctly take in newlines,     but chunkreader may need serious changes.

--- chunkreader/main.cpp
+++ chunkreader/main.cpp
@@ -4,11 +4,20 @@
 using std::cout;
 
 int main(){
-	ChunkReader f("../test.json",24);
-	char l[45];
-	f.get(l,45);
-	cout << l << '\n';
-	f.get(l,45);
-	cout << l << '\n';
+	/*ChunkReader f("main.cpp",24);
+	for (size_t i=0;i<24;++i){
+		cout << f.at();
+		f.advance();
+	}*/
+	ifstream F ("main.cpp",std::ifstream::in);
+	char t[64];
+	size_t i;
+	for (i=0;(i<64 && F.good()); ++i){
+		F.get(t[i]);
+	}
+	for (size_t j=0;(j<i); ++j){
+		cout << t[j];
+	}
+	F.close();
 	return 0;
 }
