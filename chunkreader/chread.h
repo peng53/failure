32858fe1b019b0fe3682cb19c352bd84abc6efaf
Perry Eng <eng.perry@gmail.com>
2018-11-17 11:24:24 -0600

    chunkreader: got it working for files with linebreaks. but had to use += char instead of += string.
    json_adt: moved where the header is included

--- chunkreader/chread.h
+++ chunkreader/chread.h
@@ -12,23 +12,20 @@ using std::map;
 using std::ostream;
 
 class ChunkReader : ifstream {
-	size_t i, b;
-	const size_t S;
+	size_t I,E;
+	const size_t M;
 	char* ch;
 	public:
 	ChunkReader(const char* filename,const size_t csize);
 	~ChunkReader();
 	void feed();
-	string get_capture();
-	string& get_capture(string& s);
-	string& capture_until(string& s, char c);
-	string capture_until(char c);
-	string& closure(string& s);
-	char at();
-	void advance();
-	bool empty();
 	char until(char c);
-	friend ostream& operator<<(ostream& out,ChunkReader& chk);
+	string& capture_until(string& str,char c);
+	string capture_until(char c);
+	friend ostream& operator<<(ostream& out,ChunkReader& rhs);
+	string& closure(string& s);
+	string closure();
+	void advance();
 };
 
 #endif
--- json_adt/jso_f.h
+++ json_adt/jso_f.h
@@ -1,6 +1,6 @@
 #ifndef JSOn
 #define JSOn
-
+#include "jso.h"
 class JSON {
 	private:
 		Jso* o;
