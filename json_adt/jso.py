074960d94e33fdca0ed380757939ba3b81772746
Perry E. <eng.perry@gmail.com>
2018-11-22 07:22:10 -0600

    Replaced loops that may be repeated with functions. Use of next() isn't advised? as it is low level? I'll see about replacing it with my own function.

--- json_adt/jso.py
+++ json_adt/jso.py
@@ -1,6 +1,7 @@
 
 s = """
 { "hello world" : "test",
+  "some_num"    :  10.86,
 	"mouth" : [ 1, 2, 3 ],
 	"obj" : {
 		"first": 1,
@@ -64,6 +65,42 @@ j['mice'] = 24
 r['arr'] = []
 print r
 
+def ex_str_value(s,i):
+	s_len = len(s)
+	j = i
+	while j<s_len and s[j]!='"':
+		j += 1
+	# j = next(k for k in xrange(i,s_len) if s[k]=='"')
+	if j>=s_len:
+		return (None,j)
+	r_str = s[i:j]
+	return (r_str,j+1)
+
+def ex_num_value(s,i):
+	s_len = len(s)
+	j = next(k for k in xrange(i,s_len) if not s[k].isdigit())
+	if s[j]=='.':
+		j = next(k for k in xrange(j+1,s_len) if not s[k].isdigit())
+		r_num = float(s[i:j])
+	else:
+		r_num = int(s[i:j])
+	return (r_num,j)
+
+def skip_until(s,i,c):
+	s_len = len(s)
+	while i<s_len and s[i]!=c:
+		i += 1
+	if i>=s_len:
+		raise Exception
+	return i
+
+def next_open_close(s,i):
+	try:
+		j, c = next((j,c) for (j,c) in enumerate(s[i:]) if c in '{["1234567890')
+	except StopIteration:
+		print "No open symbols were found"
+	return (c,j)
+
 def object_handler(my_stack, stack_types, s, i):
 	s_len = len(s)
 	while my_stack:
@@ -77,19 +114,10 @@ def object_handler(my_stack, stack_types, s, i):
 					return (None,i+1)
 			elif s[i]=='"':
 				### getting property name ###
-				j = i+1
-				while j<s_len and s[j]!='"':
-					j += 1
-				if j>=s_len:
-					raise Exception
-				## Success ###
-				else:
-					prop_name = s[i:j]
-					i = j+1
-					while i<s_len and s[i]!=':':
-						i += 1
-					if i>=s_len:
-						raise Exception
+				prop_name, j = ex_str_value(s,i+1)
+				if prop_name:
+					i = j + 1
+					j = skip_until(s,i,':')
 				try:
 					j, c = next((j,c) for (j,c) in enumerate(s[i:]) if c in '{["1234567890')
 				except StopIteration:
@@ -97,10 +125,8 @@ def object_handler(my_stack, stack_types, s, i):
 				### getting object type ###
 				i += j+1
 				if c=='"': ### it was a string
-					j = i
-					while j<s_len and s[j]!='"':
-						j += 1
-					my_stack[-1][prop_name] = s[i:j]
+					str_value, j = ex_str_value(s,i)
+					my_stack[-1][prop_name] = str_value
 					i = j+1
 				elif c=='{': ### it was a dictionary/ key-value pairs
 					my_stack[-1][prop_name] = {}
@@ -113,16 +139,10 @@ def object_handler(my_stack, stack_types, s, i):
 					stack_types.append('l')
 					return (None,i)
 				elif c.isdigit(): ### it was a number.
-					j = i
-					while j<s_len and (s[j].isdigit()):
-						j += 1
-					if (j>=s_len):
-						raise Exception
-					if s[j]=='.':
-						j += 1
-						while j<s_len and (s[j].isdigit()):
-							j += 1						
-					my_stack[-1][prop_name] = float(s[i-1:j-1])
+					j = i-1
+					num_value, j = ex_num_value(s,j)
+					my_stack[-1][prop_name] = num_value
+					i = j
 				else:
 					raise Exception
 			else:
@@ -131,5 +151,4 @@ my_stack = [{}]
 stack_types = ['d']
 print object_handler(my_stack,stack_types,s,1)
 print my_stack
-#print j.data
-#print p.get_value("obj")
+print stack_types
