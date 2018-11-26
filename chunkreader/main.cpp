6d238b3d4ed30f6800c82306d59911e772e9467b
Perry E. <eng.perry@gmail.com>
2018-11-26 07:41:07 -0600

    Merge on other side.

--- chunkreader/main.cpp
+++ chunkreader/main.cpp
@@ -1,12 +1,15 @@
 #include "chread.h"
+#include "../json_adt/jso_f.h"
 #include <iostream>
 
 using std::cout;
 
 int main(){
-	ChunkReader F("closure.txt",80);
+	ChunkReader F("../test_json.json",80);
+	JSON lv;
 	string str;
-	//F.until('{');
+	F.until('{');
+	F.until('"');
 	F.closure(str);
 	cout << str;
 	//cout << F;
