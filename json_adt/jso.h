28cb9aff113023c45826e5ccbae2d049399037b9
Perry E. <eng.perry@gmail.com>
2018-12-01 16:47:10 -0600

    Bug: depends on input, not closing a brace may lead to seggy-fault despite efforts with exceptions.
    I'm thinking it might be the use of chread's get since advance
    increments dumbly leading to indices out of bounds but
    attempts to change this by returning a '\0' leads to infinite looping
    (by next_symplex?) despite putting a case for '\0'.
    
    Also moved printing to jso.cpp and removed old printing.
    Even though jso_out worked, it was a mess to read.

--- json_adt/jso.h
+++ json_adt/jso.h
@@ -4,15 +4,15 @@
 #include <string>
 #include <vector>
 #include <map>
-#include <stack>
+//#include <stack>
 #include <iostream>
 
-using std::tie;
+//using std::tie;
 using std::string;
 using std::vector;
 using std::map;
-using std::stack;
-using std::pair;
+//using std::stack;
+//using std::pair;
 using std::ostream;
 
 enum class JType { Obj, Arr, Str, Num};
@@ -39,10 +39,9 @@ struct Jso {
 	void add_value(Jso* v);
 	void set_value(float v);
 	void set_value(const string& v);
-	friend std::ostream& operator<<(std::ostream& out,const Jso& J);
+	friend ostream& operator<<(ostream& out,const Jso& J);
 	Jso* key_value(const string& k);
-	void jso_out(ostream& out);
+	void rprint(ostream& out, const string& label);
 };
-void all_out(Jso j,ostream& out);
 void indent_it(size_t i,ostream& out);
 #endif
