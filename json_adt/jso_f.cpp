76e7df90642f2023c00e56f9cc7869654e8d1733
Perry E <eng.perry@gmail.com>
2018-12-01 10:59:02 -0600

    Ported jso.py stuff to json_adt (cpp). Not complete, need to take pause to design exceptions.

--- json_adt/jso_f.cpp
+++ json_adt/jso_f.cpp
@@ -1,5 +1,6 @@
 #include "jso_f.h"
-#include "../chunkreader/chread.h"
+
+using std::cout;
 
 void dispose(Jso* j, stack<Jso*>& more){
 	switch (j->t){
@@ -85,23 +86,134 @@ JSON::JSON(const JSON& rhs): JSON(){
 Jso* JSON::operator*(){
 	return o;
 }
-int main(){
-	JSON lv;
-	Jso* j = (*lv);
-	j->key_value("str","test");
-	j->key_value("num",27.2f);
-	j->key_value("arr",JType::Arr);
-	j->key_value("obj",JType::Obj);
-	j = (*lv)->key_value("arr");
-	j->add_value(5);
-	j->add_value(2);
-	j->add_value("horse");
-	j = (*lv)->key_value("obj");
-	j->key_value("horse","table");
-	j->key_value("mouse",3.14f);
-	//cout << **lv << "\n------------\n";
-	(*lv)->jso_out(cout);
-	cout << "\n----------\n";
-	//all_out(**lv,cout);
-	return 0;
+
+char next_symplex(ChunkReader& chr){
+	char c;
+	while (!chr.empty()){
+		c = chr.get();
+		if (c>='0' && c<='9'){
+			return '0';
+		}
+		switch (c){
+			case '{':
+			case '[':
+			case '"':
+			case ']':
+			case '}':
+				return c;
+				break;
+			default:
+				chr.advance();
+				break;
+		}
+	}
+	return '\0';
+}
+float get_a_number(ChunkReader& chr){
+	char c;
+	string s;
+	while (!chr.empty()){
+		c = chr.get();
+		if (c>='0' && c<='9'){
+			s += c;
+			chr.advance();
+		} else {
+			break;
+		}
+	}
+	if (c=='.'){
+		s += '.';
+		chr.advance();
+		while (!chr.empty()){
+			c = chr.get();
+			if (c>='0' && c<='9'){
+				s += c;
+				chr.advance();
+			} else {
+				break;
+			}
+		}
+	}
+	return std::stof(s);
+}
+JType mk_key_value(ChunkReader& chr, string& s){
+	// Given a ChunkReader at '"' char, modify propname, and return a JType
+	s.clear();
+	chr.advance();
+	chr.capture_until(s,'"');
+	if (chr.get()!='"'){
+		throw 1; // put an specific exception here.
+	}
+	if (s.length()==0){
+		throw 2; // put an specific exception here. (relating to 0len)
+	}
+	if (chr.until(':')!=':'){
+		throw 3; // put an specific exception here. (relating to missing :)
+	}
+	chr.advance();
+	switch (next_symplex(chr)){
+		case '"':
+			chr.advance();
+			return JType::Str;
+			break;
+		case '{':
+			chr.advance();
+			return JType::Obj;
+			break;
+		case '[':
+			chr.advance();
+			return JType::Arr;
+			break;
+		case '0':
+			return JType::Num;
+			break;
+		default:
+			throw 4; // exception: unexpected char.
+			break;
+	}
+	return JType::Str;
+}
+
+void object_handler(stack<Jso*>& stk, ChunkReader& chr){
+	string s;
+	char c;
+	Jso* j;
+	while (!chr.empty() && !stk.empty()){
+		c = next_symplex(chr);
+		if (c=='}'){
+			chr.advance();
+			stk.pop();
+			return;
+		} else if (c=='"'){
+			switch (mk_key_value(chr,s)){
+				case JType::Num:
+					stk.top()->key_value(s,get_a_number(chr));
+					break;
+				case JType::Str:
+					stk.top()->key_value(s,chr.capture_until('"'));
+					if (chr.get()!='"'){
+						throw 1;
+					}
+					// NEED TO THROW IF " isn't found!
+					break;
+				case JType::Obj:
+					j = new Jso(JType::Obj);
+					stk.top()->key_value(s,j);
+					stk.emplace(j);
+					return;
+					break;
+				case JType::Arr:
+					j = new Jso(JType::Arr);
+					stk.top()->key_value(s,j);
+					stk.emplace(j);
+					return;
+					break;
+			}
+		} else {
+			throw 1;
+		}
+	}
+}
+void array_handler(stack<Jso*>& stk, ChunkReader& chr){
+	
 }
--- json_adt/jso_f.h
+++ json_adt/jso_f.h
@@ -1,6 +1,7 @@
 #ifndef JSOn
 #define JSOn
 #include "jso.h"
+#include "../chunkreader/chread.h"
 class JSON {
 	private:
 		Jso* o;
@@ -10,5 +11,7 @@ class JSON {
 		~JSON();
 		Jso* operator*();
 };
-
+char next_symplex(ChunkReader& chr);
+void object_handler(stack<Jso*>& stk, ChunkReader& chr);
+void array_handler(stack<Jso*>& stk, ChunkReader& chr);
 #endif
