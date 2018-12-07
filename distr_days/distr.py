7c788cc7c64fb124e1d20720a35a5a49c171cabe
Perry Eng <eng.perry@gmail.com>
2018-12-07 14:46:31 -0600

    Made function to suggest days.. but it's a bit wonky since it just guesses, e.g ATM it can choose weekends. Ideally in a month without any days off, it should choose the middle of the month.. BUT! if two days are possible, the days should cut the month to close fourths. Dunno whether this is possible in code since I want a similar outcome with 3 days but.. of course its only a suggestion. The choice isn't set in stone.

--- distr_days/distr.py
+++ distr_days/distr.py
@@ -2,20 +2,21 @@
 import datetime
 import calendar
 import heapq
+import bisect
 
 input_year = 2019 # make it an argv.
 g = datetime.datetime(year=2018,month=12,day=5)
 working_days = [0]*12 # count per month.
-days_off = [[] for m in range(12)] # days already off.
-days_off[0].append(1) # january 1st.
-days_off[0].append(21)
-days_off[1].append(18) # febuary 18th.
-days_off[4].append(27) # may 27th.
-days_off[6].append(4) # you see the pattern?
-days_off[8].append(2)
-days_off[10].append(11)
-days_off[10].append(28)
-days_off[11].append(25)
+days_off = [set() for m in range(12)] # days already off.
+days_off[0].add(1) # january 1st.
+days_off[0].add(21)
+days_off[1].add(18) # febuary 18th.
+days_off[4].add(27) # may 27th.
+days_off[6].add(4) # you see the pattern?
+days_off[8].add(2)
+days_off[10].add(11)
+days_off[10].add(28)
+days_off[11].add(25)
 
 #print(g)
 #print(calendar.day_name[g.weekday()])
@@ -52,3 +53,27 @@ for n in range(free_days):
 print("-"*20)
 for month in range(0,12):
 	print("{} {}({}+{})".format(calendar.month_abbr[month+1],working_days[month],len(days_off[month]),to_add[month]))
+
+def suggest_days(month,taken_s,count,last_dom):
+	# find n such that 1<=n<=|cnt| and dist(n,x) is maximal for x in month.
+	# Or something close. Since these 'pts' lie on just an x axis, just
+	# using the max midpt(day) for each day would be sufficient.
+	taken_l = [0]+list(taken_s)+[last_dom]
+	# to get boundaries, we add the min and max, 0 and last+1
+	for n in range(count):
+		# get midpts between d1,d2; d2,d3 .. dx-1,dx.
+		mid = [(taken_l[i+1]-taken_l[i],(taken_l[i+1]+taken_l[i])//2) for i in range(len(taken_l)-1)]
+		d = (max(mid))[1]
+		bisect.insort(taken_l,d)
+	return taken_l[1:-1]
+
+#suggest_days(0,days_off[0],3)
+#print(suggest_days(0,[1,4,31],2))
+for month in range(0,12):
+	print(calendar.month_abbr[month+1])
+	if to_add[month]>0:
+		dom = calendar.monthrange(input_year,month+1)[1]
+		print(suggest_days(month,days_off[month],to_add[month],dom))
+	else:
+		print(list(days_off[month]))
+
