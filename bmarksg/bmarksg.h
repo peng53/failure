c5b504031b797124c0477c08b5e14917277d74f5
Perry Eng <eng.perry@gmail.com>
2018-12-21 11:16:00 -0600

    Made delete_group_helper which binds and steps through. Moved prepared stmts to class definition. Made prepared stmts for transaction handling. Changed groups.rowid to gid. Removed rel.rowid as it isn't used/significant. It can still be used, just not in the statement.

--- bmarksg/bmarksg.h
+++ bmarksg/bmarksg.h
@@ -9,7 +9,9 @@ using std::string;
 class DB_Connection {
 	private:
 		sqlite3 *db;
-		sqlite3_stmt *new_group, *self_rel, *root_group, *child_group;
+		sqlite3_stmt *new_grp, *self_rel, *root_grp, *child_grp,
+			*del_grp, *del_grp_data, *del_grp_rel, *get_child_grp,
+			*begin_trans, *end_trans;
 	public:
 		DB_Connection();
 		~DB_Connection();
@@ -17,7 +19,9 @@ class DB_Connection {
 		void prepare_stmts();
 		void finalize_stmts();
 		int create_group(const string& name,const int parent_gid=0);
+		bool delete_group_helper(const int gid);
 		bool delete_group(const int gid);
 		void print_groups();
+		void trans_act(const char B_E);
 };
 #endif
