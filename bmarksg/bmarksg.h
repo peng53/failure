38045bd9dfe0ca83e56fd48d9dad146f8f86e5f1
Perry E. <eng.perry@gmail.com>
2019-01-05 12:17:22 -0600

    bmarksg joins in the program. Still need to analyze output so going to write copy function for bmarksg, otherwise the data is stuck on :memory:.

--- bmarksg/bmarksg.h
+++ bmarksg/bmarksg.h
@@ -12,13 +12,14 @@ class DB_Connection {
 		sqlite3_stmt *new_grp, *self_rel, *root_grp, *child_grp,
 			*del_grp, *del_grp_data, *del_grp_rel, *get_child_grp,
 			*begin_trans, *end_trans,
-			*set_grp_name, *childNparent, *ins_data;
-	public:
-		DB_Connection();
-		~DB_Connection();
+			*set_grp_name, *childNparent, *ins_data,
+			*del_data;
 		void create_tables();
 		void prepare_stmts();
 		void finalize_stmts();
+	public:
+		DB_Connection();
+		~DB_Connection();
 		bool child_link(const int root,const int child);
 		bool unlink(const int gid);
 		int create_group(const string& name,const int parent_gid=0);
@@ -29,5 +30,6 @@ class DB_Connection {
 		bool rename_group(const int gid,const string& s);
 		bool change_parent(const int gid,const int new_par=0);
 		bool add_data(const string& key,const string& value,const string& mtime,const int gid=0);
+		bool remove_data(const int rowid);
 };
 #endif
--- json_adt/main.cpp
+++ json_adt/main.cpp
@@ -1,12 +1,24 @@
 #include "parser.h"
 #include <iostream>
 #include <sstream>
+#include <stack>
+#include "../bmarksg/bmarksg.h"
 
 using std::cout;
 using std::cerr;
 using std::istringstream;
 using std::filebuf;
 using std::ios_base;
+using std::stack;
+using std::pair;
+
+struct JsoNameGid {
+	Jso* j;
+	const int gid;
+	string* name;
+	JsoNameGid(Jso* obj, const int n, string* label):
+		j(obj), gid(n), name(label) {}
+};
 
 int main(int argc, char** argv){
 	streambuf* f;
@@ -51,6 +63,38 @@ int main(int argc, char** argv){
 	cout << jsonTree;
 	if (my_fb.is_open()){
 		my_fb.close();
+	}
+	DB_Connection my_db;
+	stack<JsoNameGid> stk;
+	// Create a db group to hold what's going to be inserted.
+	stk.emplace((*jsonTree),my_db.create_group("group #1"),(*jsonTree)->key_value("title")->x.s);
+	Jso* j;
+	int pid, gid;
+	string* s;
+	while (!stk.empty()){
+		j = stk.top().j;
+		pid = stk.top().gid;
+		s = stk.top().name;
+		stk.pop();
+		// If top item was a 'x-moz-place-container', it should have an array named children.
+		if ((*(j->key_value("type")->x.s))=="text/x-moz-place-container"){
+			// So we create a group under the current parent.
+			gid = my_db.create_group(*s,pid);
+			if (gid==0){
+				cerr << "Could not create a group.\n";
+				return 1;
+			}
+			// And since its a group, we queue( stack) up its children.
+			for (const auto& k : *(j->key_value("children")->x.a) ){
+				stk.emplace(k,gid,k->key_value("title")->x.s);
+			}
+			cout << "Made a group: " << *s << '\n';
+		} else {
+			// It must be a link then.
+			// So we add the link.
+			my_db.add_data(*s,*(j->key_value("uri"))->x.s,"",gid);
+			cout << "Added data for: " << *s << '\n';
+		}
 	}
 	return 0;
 }
