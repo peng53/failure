acbb44b81f72767bc62d27d533e89ab59dfaffb3
Perry Eng <eng.perry@gmail.com>
2019-01-16 11:54:54 -0600

    Changed jso functions to be similarly named and removed ones that aren't used or can be troublesome. Still cleaning up.

--- json_adt/jso.h
+++ json_adt/jso.h
@@ -26,22 +26,19 @@ struct Jso {
 	explicit Jso(JType j);
 	explicit Jso(map<string,Jso*>* VAL);
 	~Jso();
-	void key_value(const string& k,Jso* v);
-	void key_value(const string& k,const string& v);
-	void key_value(const string& k,const double v);
-	void key_value(const string& k,JType vt);
-	void add_value(const double v);
-	void add_value(const string& v);
-	void add_value(const JType vt);
-	void add_value(Jso* v);
-	void set_value(double v);
-	void set_value(const string& v);
+	void Append(const string& k,Jso* v);
+
+	void Append(Jso* v);
+
 	friend ostream& operator<<(ostream& out,const Jso& J);
 	Jso* key_value(const string& k);
 	void rprint(ostream& out, const string& label);
 	explicit operator const string&();
 	Jso* operator[](const string& k);
 	static Jso JSO_NULL, JSO_TRUE, JSO_FALSE;
+	void Get(string **outptr);
+	void Get(double **outptr);
+	void Get(map<string,Jso*> **outptr);
+	void Get(vector<Jso*> **outptr);
 };
-void indent_it(size_t i,ostream& out);
 #endif
