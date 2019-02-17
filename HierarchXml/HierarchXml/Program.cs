a7660e1fedeaf328e4ad55c9d43292685469fc3b
Perry E <eng.perry@gmail.com>
2019-02-17 09:04:44 -0600

    Cleanup, using usings and consistent newlines before {

--- HierarchXml/HierarchXml/Program.cs
+++ HierarchXml/HierarchXml/Program.cs
@@ -9,7 +9,8 @@ namespace HierarchXml
 	{
 		public static string Abridged (this string value, int length)
 		{
-			if (value == null) {
+			if (value == null)
+			{
 				return null;
 			}
 			return value.Substring (0, Math.Min (value.Length, length));
@@ -20,30 +21,34 @@ namespace HierarchXml
 	{
 		public static void Main (string [] args)
 		{
-			var dbConn = new Mono.Data.Sqlite.SqliteConnection ("Data Source=/mnt/ramdisk/test.db");
-			dbConn.Open ();
-			const string dbConnReadSql = "SELECT rowid, name FROM groups";
-			var dbConnRead = dbConn.CreateCommand ();
-			dbConnRead.CommandText = dbConnReadSql;
+			using (var dbConn = new Mono.Data.Sqlite.SqliteConnection ("Data Source=/mnt/ramdisk/test.db"))
+			{
+				dbConn.Open ();
+				const string dbConnReadSql = "SELECT rowid, name FROM groups";
+				var dbConnRead = dbConn.CreateCommand ();
+				dbConnRead.CommandText = dbConnReadSql;
 
-			const string for_each_group = "SELECT key,value FROM data WHERE gid=@GID";
-			var dbConnEachGroup = dbConn.CreateCommand ();
-			dbConnEachGroup.CommandText = for_each_group;
+				using (IDataReader reader = dbConnRead.ExecuteReader ()) 
+                {
 
-			IDataReader reader = dbConnRead.ExecuteReader ();
-			while (reader.Read ()) {
-				Console.WriteLine ($"{reader.GetInt32 (0)} {reader.GetString (1)}");
-				dbConnEachGroup.Parameters.AddWithValue ("@GID", reader.GetInt32 (0));
-				using (IDataReader data_reader = dbConnEachGroup.ExecuteReader ()){
-					while (data_reader.Read ()) {
-						Console.WriteLine ($"{string.Format("{0,-24}",data_reader.GetString (0).Abridged(24))} - {data_reader.GetString (1).Abridged(48)}");
+					const string for_each_group = "SELECT key,value FROM data WHERE gid=@GID";
+					var dbConnEachGroup = dbConn.CreateCommand ();
+					dbConnEachGroup.CommandText = for_each_group;
+
+					while (reader.Read ()) 
+                    {
+						Console.WriteLine ($"{reader.GetInt32 (0)} {reader.GetString (1)}");
+						dbConnEachGroup.Parameters.AddWithValue ("@GID", reader.GetInt32 (0));
+						using (IDataReader data_reader = dbConnEachGroup.ExecuteReader ()) 
+                        {
+							while (data_reader.Read ()) 
+                            {
+								Console.WriteLine ($"{string.Format ("{0,-24}", data_reader.GetString (0).Abridged (24))} - {data_reader.GetString (1).Abridged (48)}");
+							}
+						}
 					}
 				}
 			}
-
-			reader.Dispose ();
-			dbConnRead.Dispose ();
-			dbConn.Clone ();
 		}
 	}
 }
