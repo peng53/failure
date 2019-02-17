6d518782193ae366d96185869c6f98e6273db3da
Perry E <eng.perry@gmail.com>
2019-02-17 08:32:23 -0600

    Preliminary work in C#, this reads a test.db from json_adt and prints the groups & data to stdout. This of course depends on the structure presented in bmarksg && tcl/hierarch_clo. Project was made in monodevelop but I wasn't sure which files to add so I just added the important one.

---
+++ HierarchXml/HierarchXml/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace HierarchXml
+{
+
+	static class StringExtensions
+	{
+		public static string Abridged (this string value, int length)
+		{
+			if (value == null) {
+				return null;
+			}
+			return value.Substring (0, Math.Min (value.Length, length));
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string [] args)
+		{
+			var dbConn = new Mono.Data.Sqlite.SqliteConnection ("Data Source=/mnt/ramdisk/test.db");
+			dbConn.Open ();
+			const string dbConnReadSql = "SELECT rowid, name FROM groups";
+			var dbConnRead = dbConn.CreateCommand ();
+			dbConnRead.CommandText = dbConnReadSql;
+
+			const string for_each_group = "SELECT key,value FROM data WHERE gid=@GID";
+			var dbConnEachGroup = dbConn.CreateCommand ();
+			dbConnEachGroup.CommandText = for_each_group;
+
+			IDataReader reader = dbConnRead.ExecuteReader ();
+			while (reader.Read ()) {
+				Console.WriteLine ($"{reader.GetInt32 (0)} {reader.GetString (1)}");
+				dbConnEachGroup.Parameters.AddWithValue ("@GID", reader.GetInt32 (0));
+				using (IDataReader data_reader = dbConnEachGroup.ExecuteReader ()){
+					while (data_reader.Read ()) {
+						Console.WriteLine ($"{string.Format("{0,-24}",data_reader.GetString (0).Abridged(24))} - {data_reader.GetString (1).Abridged(48)}");
+					}
+				}
+			}
+
+			reader.Dispose ();
+			dbConnRead.Dispose ();
+			dbConn.Clone ();
+		}
+	}
+}
