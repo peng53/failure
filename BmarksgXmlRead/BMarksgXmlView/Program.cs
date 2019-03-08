f9e823ce5a17ca40efbcb9f4755d743110a391c2
Perry E <eng.perry@gmail.com>
2019-03-08 16:29:12 -0600

    Added the gtk_gui so there's actually something to see..
    Change from treeview to nodeview reason being that although
    there is a hierarchy, its really just important only for the
    groups (which is reflected in the sql code).
    
    If I would include a group filter bar, having the entire group
    collapse would make it redundent.
    
    So in doing this change I need to (or rather should):
    	make some kind of way to sort by hierarchy. maybe some way
    		of keeping depth.
    
    	make a grouping? to keep all subgroups of X adjacent.
    
    	show/store the names of the groups but keep the group
    		column as the id. A possible solution would be storing
    		the group as a 'path', like 'g1/g2/g3' and that would
    		certainly help with sorting,grouping, and naming (less so
    		naming). the downside of it being fairly long.
    			what do I need to do? Add a prop to linknode called
    			'group' which is ref obj containing the XElement
    			and a string 'path' as members?

--- BmarksgXmlRead/BMarksgXmlView/Program.cs
+++ BmarksgXmlRead/BMarksgXmlView/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using Gtk;
 using System.Xml.Linq;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace BMarksgXmlView
 {
@@ -14,34 +12,10 @@ namespace BMarksgXmlView
             MainWindow win = new MainWindow();
 
             var document = XDocument.Load("../../../../in_out/test.xml");
-            Queue<XElement> xes = new Queue<XElement>();
-
             foreach (XElement node in document.Elements("Bookmarks").Elements("Group"))
             {
-                xes.Enqueue(node);
-                win.AddGroup(node.Attribute("NAME").Value, node.Attribute("Id").Value);
+                win.AddGroup(node);
             }
-            while (xes.Count > 0)
-            {
-                var group = xes.Peek();
-                xes.Dequeue();
-                Console.WriteLine($"group : {group.Attribute("NAME")}");
-                foreach (var link in group.Elements())
-                {
-                    if (link.Name == "Group")
-                    {
-                        xes.Enqueue(link);
-                        win.AddGroup(link.Attribute("NAME").Value, link.Attribute("Id").Value, link.Parent.Attribute("Id").Value);
-                    }
-                    else
-                    {
-                        win.AddLink(link.Attribute("NAME").Value, link.Attribute("URL").Value, link.Parent.Attribute("Id").Value);
-                        Console.WriteLine($"\t{link}");
-                    }
-
-                }
-            }
-
             win.Show();
             Application.Run();
 
