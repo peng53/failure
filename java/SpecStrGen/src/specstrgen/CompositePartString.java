7b1e1e960584693b13190b1959cf49102b658311
Perry Eng <eng.perry@gmail.com>
2019-04-03 09:55:35 -0500

    Changed all flags to use argumenter. Made basic loop in Main to detect which flag to use, but not all flags are implemented, e.g. the ones that don't take args. Loop is still dumb ATM meaning it expects correct # of arguments, need to implement a check. Switch statement is pretty ugly and un-expandable? Want to make a Hashmap<Character,Function> though don't know how.

--- java/SpecStrGen/src/specstrgen/CompositePartString.java
+++ java/SpecStrGen/src/specstrgen/CompositePartString.java
@@ -4,45 +4,51 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class CompositePartString {
-	public CompositePartString(){
-		literals = new ArrayList<>();
-		parts = new ArrayList<>();
-	}
-	public void addPart(Part part){
-		parts.add(part);
-	}
-	public Part getPart(int n){
-		if (n<0 || parts.size()<n){
-			return null;
-		}
-		return parts.get(n);
-	}
-	
-	public void addStr(String str){
-		literals.add(new StringBuilder(str));
-	}
-	public boolean hasStr(int n){
-		return (n<literals.size() && n>=0);
 
+    public CompositePartString() {
+	literals = new ArrayList<>();
+	parts = new ArrayList<>();
+    }
+
+    public void addPart(Part part) {
+	parts.add(part);
+    }
+
+    public Part getPart(int n) {
+	if (n < 0 || parts.size() < n) {
+	    return null;
 	}
-	public String getStr(int n){
-		if (hasStr(n)){
-			return literals.get(n).toString();
-		}
-		return null;
+	return parts.get(n);
+    }
+
+    public void addStr(String str) {
+	literals.add(new StringBuilder(str));
+    }
+
+    public boolean hasStr(int n) {
+	return (n < literals.size() && n >= 0);
+
+    }
+
+    public String getStr(int n) {
+	if (hasStr(n)) {
+	    return literals.get(n).toString();
 	}
-	public int literalsCount(){
-		return literals.size();
+	return null;
+    }
+
+    public int literalsCount() {
+	return literals.size();
+    }
+
+    public String toString() {
+	StringBuilder sb = new StringBuilder();
+	for (Part p : parts) {
+	    p.out(sb);
 	}
-	
-	public String toString(){
-		StringBuilder sb = new StringBuilder();
-        for (Part p : parts){
-            p.out(sb);
-        }
-        return sb.toString();
-	}
-	
-	List<StringBuilder> literals;
-	List<Part> parts;
+	return sb.toString();
+    }
+
+    List<StringBuilder> literals;
+    List<Part> parts;
 }
