06d4ea535d4fc7247aa4b311289d5c5b9f1bd8af
Perry E <eng.perry@gmail.com>
2019-04-12 16:49:20 -0500

    Created container of trays the 'collective', which just manages creation of trays.

--- py3/bt_sets.py
+++ py3/bt_sets.py
@@ -1,5 +1,6 @@
 #!/bin/env python3
 from typing import List
+from copy import deepcopy
 
 class Tray:
 	f_duplicate_found = []
@@ -8,12 +9,19 @@ class Tray:
 		self.number = number
 		self.codes = set()
 	
-	def add(self, code: int):
-		if code in self.codes:
-			for f in Tray.f_duplicate_found:
-				f(self.location, self.number, code)
-		else:
-			self.codes.add(code)
+	def add(self, *codes: List[int]):
+		for code in codes:
+			if code in self.codes:
+				for f in Tray.f_duplicate_found:
+					f(self.location, self.number, code)
+			else:
+				self.codes.add(code)
+	
+	def distinctFrom(self, otherTray):
+		return self.codes - otherTray.codes
+	
+	def __repr__(self):
+		return "#{} @ {}".format(self.number, self.location)
 	
 	@staticmethod
 	def AddEvent_DuplicateFound(f):
@@ -31,15 +39,50 @@ class Tray_Err:
 		self.err_trays[location][number].add(code)
 		
 
+class Collective:
+	def __init__(self, location: str):
+		self.location = location
+		self.trays = {}
+	
+	def addTray(self, number):
+		if number in self.trays:
+			pass
+		else:
+			self.trays[number] = Tray(self.location, number)
+		return self.trays[number]
+	
+	def tray(self, number):
+		if number in self.trays:
+			return self.trays[number]
+		else:
+			raise InvalidAccessErr
+	
+	def trackCode(self, code):
+		# Finds the tray(s) that contains this code.
+		out = set()
+		for tray in self.trays.values():
+			if code in tray.codes:
+				out.add(tray.number)
+		return out
+	
+	def __len__(self):
+		return len(self.trays)
+	
+	def __repr__(self):
+		return "Collective of trays @ {}".format(self.location)
+
 def main(argv : List[str] = []):
-	a = Tray("bed body and booty", 306)
+	bbb = Collective("bed body and booty")
+	bbb.addTray(306)
 	Tray.AddEvent_DuplicateFound(lambda l,n,c: print("DUP CODE %d AT %s #%d" %(c,l,n)))
 	duplicate_barn = []
 	Tray.AddEvent_DuplicateFound(lambda l,n,c: duplicate_barn.append((l,n,c)))
 	te = Tray_Err()
 	Tray.AddEvent_DuplicateFound(te.logError)
-	a.add(10)
-	a.add(3)
-	a.add(10)
+	bbb.tray(306).add(10,3,10,3,555)
+	
+	bbb.addTray(305).add(555, 201, 807)
+	print(bbb.trays)
 	print(duplicate_barn)
-	print(te.err_trays)
+	print(bbb.trackCode(10))
+	print(bbb.trackCode(555))
