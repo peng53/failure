8b697dc008b0a0166451ed3035f318f143094f8a
Perry E <eng.perry@gmail.com>
2019-06-02 07:52:25 -0500

    Project add, atm just GUI and very few bindings. Working on highlighting text stored in something

---
+++ gtk#/ResumAid/Keyword.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ResumAid
+{
+    [Gtk.TreeNode(ListOnly = true)]
+    public class Keyword : Gtk.TreeNode
+    {
+        public Keyword(string keyword)
+        {
+            this.keyword = keyword;
+        }
+
+        [Gtk.TreeNodeValue(Column = 0)]
+        string keyword;
+    }
+}
---
+++ gtk#/ResumAid/MainWindow.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Gtk;
+using System.Reflection;
+using System.Collections.Generic;
+
+public partial class MainWindow : Gtk.Window
+{
+    ResumAid.AddKeywordDialog addKeywordDialog;
+    NodeStore documentsNodes;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Action<string> printAction = s => Console.WriteLine(s);
+        addKeywordDialog = new ResumAid.AddKeywordDialog(AddAKeyword);
+        documentsNodes = new NodeStore(typeof(ResumAid.Keyword));
+        documentsNV.NodeStore = documentsNodes;
+        typeof(NodeView).GetField("store", BindingFlags.Instance | BindingFlags.NonPublic).SetValue(documentsNV, documentsNodes);
+        SetDocument(new ResumAid.RichDoc {
+            Text = "Sample Text\nPoops",
+            Highlights = new List<ResumAid.Highlight> {
+                new ResumAid.Highlight { Start = 3, Length = 3 },
+                new ResumAid.Highlight { Start = 9, Length = 5 }
+            }
+            });
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void AddKeywordToDocument(object sender, EventArgs e)
+    {
+        addKeywordDialog.Use();
+    }
+
+    protected void AddAKeyword(string keyword)
+    {
+        // ATM only adds to visual pane and not DB
+        if (keywordsTV.Buffer.Text.Length > 0)
+        {
+            keywordsTV.Buffer.Text += ", ";
+        }
+        keywordsTV.Buffer.Text += keyword;
+    }
+    protected void SetDocument(ResumAid.RichDoc richDoc)
+    {
+        documentText.Buffer.Text = richDoc.Text;
+        var tag = new TextTag("poop");
+        tag.Background = "red";
+        documentText.Buffer.TagTable.Add(tag);
+        foreach (var hl in richDoc.Highlights)
+        {
+            documentText.Buffer.ApplyTag("poop", documentText.Buffer.GetIterAtOffset(hl.Start), documentText.Buffer.GetIterAtOffset(hl.Start+hl.Length));
+        }
+
+    }
+
+}
