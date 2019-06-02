f940c2ac37c4f8906810344aced765043789e04b
Perry E. <eng.perry@gmail.com>
2019-06-02 07:52:25 -0500

    Project add, atm just GUI and very few bindings. Working on highlighting text stored in something

---
+++ gtk#/ResumAid/AddKeywordDialog.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace ResumAid
+{
+    public partial class AddKeywordDialog : Gtk.Dialog
+    {
+        private Action<string> addKeyword;
+
+        public AddKeywordDialog(Action<string> action)
+        {   
+            this.Build();
+            this.addKeyword = action;
+            this.Hide();
+        }
+        public void Use()
+        {
+            Show();
+            keywordEntry.GrabFocus();
+        }
+
+        protected void ReturnKeyword(object sender, EventArgs e)
+        {
+            if (keywordEntry.Text.Length > 0)
+            {
+                addKeyword(keywordEntry.Text);
+            }
+            ClearAndHide(sender, e);
+        }
+
+        protected void ClearAndHide(object sender, EventArgs e)
+        {
+            Hide();
+            keywordEntry.Text = "";
+        }
+    }
+}
---
+++ gtk#/ResumAid/RichDoc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ResumAid
+{
+    public class RichDoc
+    {
+        public RichDoc()
+        {
+        }
+        public string Text { get; set; }
+        public List<Highlight> Highlights { get; set; }
+        public List<int> Keywords { get; set; }
+    }
+
+    public class Highlight
+    {
+        public int Start { get; set; }
+        public int Length { get; set; }
+    }
+}
