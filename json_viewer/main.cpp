964926b4fdecf98d2af0017929a5836ce07751e0
Perry E <eng.perry@gmail.com>
2019-12-21 15:26:12 -0600

    Change in thought process (good thing I haven't really done anything yet!) Instead of view displaying anything via ncurses, view will just carry the infomation of the view. (the structure backing the window). In this way, app will hold many views and display 1 of them at anytime using a window it controls.
    Changes in the commit being removal of display properties from View. User will
    query the state property of a View and call getItem dependent on that. getItem
    will take a function that takes a const char* and call appropriately.
    
    Special care is needed with various JTypes.
    
    This project is going exceedingly slow mostly due to work on another repo, but
    there are some ideas on paper.

--- json_viewer/main.cpp
+++ json_viewer/main.cpp
@@ -1,9 +1,19 @@
 #include "app.h"
+#include <iostream>
+
+using std::cout;
+
+void tostdout(const char* outstring){
+	cout << outstring << '\n';
+}
+
 
 int main(int argc, char** argv){
 	App t;
 	View v;
 	Jso* j = JSON::Num(10);
 	v.setViewItem(j);
+	v.setItemsPerPage(10);
+	v.getItem(tostdout);
 	return 0;
 }
