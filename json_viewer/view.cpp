e9582280025b3ae2b88e1dda920b191911374c22
Perry Eng <eng.perry@gmail.com>
2019-12-25 17:36:10 -0600

    Changed setViewItem to reset item, page, & state as these properties are of the (last) viewItem.
    Moved string representation to seperate function. Now all getItem does
    is call string repr if when and with the Jso* needed to be represented.
    
    Might want to change repr from [array] to [array][size]

--- json_viewer/view.cpp
+++ json_viewer/view.cpp
@@ -2,6 +2,7 @@
 #include <string>
 
 using std::to_string;
+using std::string;
 
 View::View(): page(0), item(0){
 
@@ -11,27 +12,72 @@ View::~View(){
 
 }
 
-void View::setViewItem(Jso* item){
-	displayedItem = item;
+void View::setViewItem(Jso* viewItem){
+	displayedItem = viewItem;
+	item = 0;
+	page = 0;
+	state = PageState::MORE;
 }
 
 void View::setItemsPerPage(unsigned count){
 	itemsPerPage = count;
 }
 
+string JsoStringRep(Jso *j){
+	switch (j->t){
+		case JType::Num:
+			return to_string(j->x.f);
+		case JType::Str:
+			return string(*(j->x.s));
+		case JType::Arr:
+			return "[array]";
+		case JType::Obj:
+			return "{obj}";
+		case JType::Null:
+			return "(null)";
+		case JType::True:
+			return "(true)";
+		case JType::False:
+			return "(false)";
+		default:
+			return "SHOULD NOT BE SEEN JSOSTRINGREP";
+	}
+}
+
 void View::getItem(void (*f) (string& s)){
+	if (state != PageState::MORE){
+		return;
+	}
 	string out;
 	if (item < itemsPerPage){
 		switch (displayedItem->t){
 			case JType::Num:
-				out = to_string(displayedItem->x.f);
+				out = JsoStringRep(displayedItem);
+				state = PageState::DONE;
 				break;
 			case JType::Str:
-				out = *(displayedItem->x.s);
+				out = JsoStringRep(displayedItem);
+				state = PageState::DONE;
+				break;
+			case JType::Arr:
+				if (item < itemsPerPage){
+					size_t index = item + itemsPerPage*page;
+					if (index >= displayedItem->x.a->size()){
+						state = PageState::DONE;
+					} else {
+						out = JsoStringRep((*displayedItem->x.a)[index]);
+						++item;
+					}
+				}
 				break;
 			default:
 				break;
 		}
 		f(out);
 	}
+}
+
+void View::reloadPage(){
+	state = PageState::MORE;
+	item = 0;
 }
