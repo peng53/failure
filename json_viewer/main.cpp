f3fdabefc108148b367bf565f07db55c33287e71
Perry Eng <eng.perry@gmail.com>
2020-01-01 10:07:38 -0600

    Replaced use of array of ptrs to JSON, taking care of cleanup. Made better demo of paginating (showing the unorder-ness of uo-map)
    Need to distinguish between 'end-point' values and 'nest' values
    (str/num/null/true/false) vs (obj,arr) in string_repr

--- json_viewer/main.cpp
+++ json_viewer/main.cpp
@@ -10,43 +10,23 @@ using std::unordered_map;
 
 int main(int argc, char** argv){
 	View v;
-	Jso* jf = JSON::Num(10);
-	Jso* js = JSON::Str("hello world");
-	Jso* ja = JSON::Arr();
-	Jso* jm = JSON::Map();
-
-	ja->Append(jf);
-	ja->Append(js);
-	jm->Append("arr", ja);
-	jm->Append("str", js);
-	jm->Append("num", jf);
-
+	JSON master;
+	
 	const size_t numbersCnt = 26+1;
-	Jso *numbers[numbersCnt];
 	for (size_t i=0; i<numbersCnt; ++i){
-		numbers[i] = JSON::Num(i);
-		jm->Append(to_string(i), numbers[i]);
+		(*master)->Append(to_string(i), JSON::Num(i));
 	}
 
-	v.setViewItem(JSON::Single(JType::Null));
-	v.setItemsPerPage(10);
-	v.setViewItem(jm);
-	cout << "---Page 1--\n";
-	while (v.state == PageState::MORE){
-		v.getItem([] (string& s){ cout << s << '\n';});
-	}
-	v.nextPage();
-	cout << "---Page 2--\n";
-	while (v.state == PageState::MORE){
-		v.getItem([] (string& s){ cout << s << '\n';});
-	}
-	delete jf;
-	delete js;
-	delete ja;
-	delete jm;
-	for (size_t i=0; i<numbersCnt; ++i){
-		delete numbers[i];
-	}
-
+	v.setViewItem(*master);
+	v.setItemsPerPage(4);
+	
+	unsigned pageNum = 0;
+	do {
+		cout << "---Page " << pageNum++ << " ---\n";
+		while (v.state == PageState::MORE){
+			v.getItem([] (string& s){ cout << s << '\n';});
+		}
+	} while (v.nextPage());
+	
 	return 0;
 }
