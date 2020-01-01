ed9695d76dff3d46c7f4ae0385940d9b468ec02d
Perry Eng <eng.perry@gmail.com>
2020-01-01 10:31:25 -0600

    Fixed pagination of array (item needs to be set to 0) Fixed bug where the f of getItem is called even when there is nothing to output. Exported printing of view in main to allPagesFromViewToStdOut. (this may be a long function name, but it isn't supposed to be a function to be used)

--- json_viewer/main.cpp
+++ json_viewer/main.cpp
@@ -7,6 +7,15 @@ using std::to_string;
 using std::cout;
 using std::unordered_map;
 
+void allPagesFromViewToStdOut(View &v){
+	unsigned pageNum = 0;
+	do {
+		cout << "---Page " << pageNum++ << " ---\n";
+		while (v.state == PageState::MORE){
+			v.getItem([] (string& s){ cout << s << '\n';});
+		}
+	} while (v.nextPage());
+}
 
 int main(int argc, char** argv){
 	View v;
@@ -18,17 +27,22 @@ int main(int argc, char** argv){
 	}
 	(*master)->Append("mini-map", JSON::Map());
 	(*master)->Append("mini-arr", JSON::Arr());
+	Jso* myarr = (*master)->key_value("mini-arr");
+	myarr->Append(JSON::Single(JType::Null));
+	myarr->Append(JSON::Single(JType::True));
+	myarr->Append(JSON::Single(JType::False));
+	myarr->Append(JSON::Num(24));
+	myarr->Append(JSON::Str("twenty-four"));
 
 	v.setViewItem(*master);
 	v.setItemsPerPage(4);
-	
-	unsigned pageNum = 0;
-	do {
-		cout << "---Page " << pageNum++ << " ---\n";
-		while (v.state == PageState::MORE){
-			v.getItem([] (string& s){ cout << s << '\n';});
-		}
-	} while (v.nextPage());
-	
+
+	cout << "Print out of master JSON\n";
+	allPagesFromViewToStdOut(v);
+	cout << "--------------\n";
+	v.setViewItem(myarr);
+	cout << "Print out of mini-arr\n";
+	allPagesFromViewToStdOut(v);
+	cout << "--------------\n";
 	return 0;
 }
