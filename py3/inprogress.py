a38c0e396f2b35bf3106094b071ab7f682d4f19a
Perry Eng <eng.perry@gmail.com>
2020-02-02 13:32:30 -0600

    There's more!

--- py3/inprogress.py
+++ py3/inprogress.py
@@ -10,18 +10,31 @@ class BinaryStateIP(InProgress):
 	def __init__(self, states: str = ['/\r','\\\r']):
 		self.state = False
 		self.states = states
-	
+
 	def out(self):
 		if self.state is True:
 			print(self.states[int(self.state)], end='')
 		else:
 			print(self.states[int(self.state)], end='')
 		self.state = not self.state
-		
+
+class NStateIP(BinaryStateIP):
+	def __init__(self, states: str = ['/\r','\\\r']):
+		self.state = 0
+		self.states = states
+		self.mstate = len(states)
+	
+	def out(self):
+		print(self.states[self.state], end='')
+		self.state += 1
+		if self.state == self.mstate:
+			self.state = 0
 
 from time import sleep
 
-d = BinaryStateIP()
+SpinningLine = NStateIP(['-\r', '\\\r', '|\r', '/\r'])
+Pac = NStateIP(['<ooooo\r', ' <oooo\r', '  <ooo\r', '   <oo\r', '    <o\r', '     <\r'])
+Xss = NStateIP(['x+x\r', '+x+\r'])
 for i in range(35):
-	d.out()
+	Xss.out()
 	sleep(1)
