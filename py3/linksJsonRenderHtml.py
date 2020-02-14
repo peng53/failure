abf85d7c982af897bf9772d0fd33b0c462417cf2
Perry Eng <eng.perry@gmail.com>
2020-02-14 09:10:45 -0600

    Change to less script-like / more expandable

--- py3/linksJsonRenderHtml.py
+++ py3/linksJsonRenderHtml.py
@@ -1,13 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
 import json
 
-env = Environment(loader = FileSystemLoader('templates'))
-tmpFile = "linksTableView.html"
-template = env.get_template(tmpFile)
 
-with open('linksrender.json', 'r') as f:
+def renderAsTable(data, out):
+	env = Environment(loader = FileSystemLoader('templates'))
+	tmpFile = "linksTableView.html"
+	template = env.get_template(tmpFile)
+	output = template.render(links=data['links'], groups=data['groups'])
+	with open(out, 'w') as o:
+		o.write(output)
+
+dataf = 'linksrender.json'
+out = 'linksJsonOut.html'
+
+with open(dataf, 'r') as f:
 	data = json.load(f)
 
-output = template.render(links=data['links'], groups=data['groups'])
-with open('linksJsonOut.html', 'w') as o:
-	o.write(output)
+renderAsTable(data, out)
