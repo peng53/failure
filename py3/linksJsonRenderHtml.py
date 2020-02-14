a4717e101154255b6db2e8f13932caf840e7be2a
Perry Eng <eng.perry@gmail.com>
2020-02-14 09:31:17 -0600

    Initial add linksGroupView.html
    Made alternate output, instead of a tableview, a grouped view- which
    required some processing beforehand.

--- py3/linksJsonRenderHtml.py
+++ py3/linksJsonRenderHtml.py
@@ -1,4 +1,5 @@
 from jinja2 import Environment, FileSystemLoader
+from collections import namedtuple
 import json
 
 
@@ -10,10 +11,28 @@ def renderAsTable(data, out):
 	with open(out, 'w') as o:
 		o.write(output)
 
+Link = namedtuple('Link', 'name,url')
+
+def renderAsGrouped(data, out):
+	env = Environment(loader = FileSystemLoader('templates'))
+	tmpFile = "linksGroupView.html"
+	template = env.get_template(tmpFile)
+
+	groupedData = { i:[] for i,e in enumerate(data['groups'])}
+	groupsCount = len(groupedData)
+	for l in data['links']:
+		if l['group']<groupsCount:
+			groupedData[l['group']].append(Link(l['name'],l['url']))
+	
+	output = template.render(groups=groupedData, groupNames = data['groups'])
+	with open(out, 'w') as o:
+		o.write(output)
+
 dataf = 'linksrender.json'
 out = 'linksJsonOut.html'
 
 with open(dataf, 'r') as f:
 	data = json.load(f)
 
-renderAsTable(data, out)
+#renderAsTable(data, out)
+renderAsGrouped(data,out)
