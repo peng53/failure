40ce0030a33aad8cbede1deff5090f5b90c2e73a
P. Eng <eng.perry@gmail.com>
2020-05-25 08:39:16 -0500

    Moved queue implementation to separate file and fixed some problems with it, namely the node becoming undefined after certain number of dequeues and enqueues. The approach now exhausts most possible states of the queue via a 'length'.

---
+++ html/queue.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function QNode(val){
+	this.v = val;
+	this.next = undefined;
+}
+
+function Queue(){
+	this.head = undefined;
+	this.tail = undefined;
+	this.len = 0;
+	this.put = function(val){
+		let n = new QNode(val);
+		if (this.len == 0){
+			this.head = n;
+		} else if (this.len = 1){
+			this.head.next = n;
+			this.tail = n;
+		} else {
+			this.tail.next = n;
+		}
+		++this.len;
+	};
+	this.get = function(){
+		let n;
+		if (this.len == 0){
+			return;
+		} else if (this.len == 1){
+			n = this.head;
+			this.head = undefined;
+		} else {
+			n = this.head;
+			this.head = this.head.next;
+		}
+		--this.len;
+		return n.v;
+	};
+}
