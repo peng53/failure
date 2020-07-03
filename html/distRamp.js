aa8e675b0871a15cb5966b6d70c04593735e87fe
P. Eng <eng.perry@gmail.com>
2020-07-03 13:02:55 -0500

    Change from es5 class to es6- this kills support to older versions of IE but,  this make coding a lot more easier as the file is easier to read. If need be, there's always a transcoder somewhere..
    ATM connectNodes doesn't work because there was an attempt to
    create a normalized drawing (nodes with negative coordinates)

--- html/distRamp.js
+++ html/distRamp.js
@@ -1,57 +1,60 @@
 'use strict';
 
-function RampCols (cols){
-	this.cur = 0
-	this.cols = new Array(cols);
-	for (let i=0;i<cols;++i){
-		this.cols[i] = [];
-	}
-}
-RampCols.prototype.ins = function(v){
-	if (this.cur==this.cols.length || 
-		this.cur!=0 && this.cols[this.cur].length+1 == this.cols[this.cur-1].length){
-		// if not first column AND new count will equal previous column's current count
-		this.cur = 0;
-	}
-	const i = this.cur;
-	const j = this.cols[this.cur].length;
-	this.cols[this.cur].push(v);
-	++this.cur;
-	return [i,j];
-}
-
-RampCols.prototype.renderAsTable = function(){
-	const ret = document.createElement('table');
-	let row, cell;
-	for (let r=0;r<this.cols.length;++r){
-		row = ret.insertRow(-1);
-		for (let c= 0, l=this.cols[r].length;c<l;++c){
-			cell = row.insertCell(-1);
-			cell.appendChild(document.createTextNode(this.cols[r][c]));
+class RampCols {
+	constructor(cols){
+		this.cur = 0
+		this.cols = new Array(cols);
+		for (let i=0;i<cols;++i){
+			this.cols[i] = [];
 		}
 	}
-	return ret;
+	ins(v){
+		if (this.cur==this.cols.length || 
+			this.cur!=0 && this.cols[this.cur].length+1 == this.cols[this.cur-1].length){
+			// if not first column AND new count will equal previous column's current count
+			this.cur = 0;
+		}
+		const i = this.cur;
+		const j = this.cols[this.cur].length;
+		this.cols[this.cur].push(v);
+		++this.cur;
+		return [i,j];
+	}
+	renderAsTable(){
+		const ret = document.createElement('table');
+		let row, cell;
+		for (let r=0;r<this.cols.length;++r){
+			row = ret.insertRow(-1);
+			for (let c= 0, l=this.cols[r].length;c<l;++c){
+				cell = row.insertCell(-1);
+				cell.appendChild(document.createTextNode(this.cols[r][c]));
+			}
+		}
+		return ret;
+	}
 }
 
-function RampDist(rows){
-	this.rows = new Array(rows);
-	this.reset();
-}
-RampDist.prototype.get = function(){
-	if (this.cur==this.rows.length || 
-		this.cur!=0 && this.rows[this.cur]+1 == this.rows[this.cur-1]){
-		// if not first column AND new count will equal previous column's current count
-		this.cur = 0;
+class RampDist {
+	constructor(rows){
+		this.rows = new Array(rows);
+		this.reset();
 	}
-	const i = this.cur;
-	const j = this.rows[this.cur];
-	++this.rows[this.cur];
-	++this.cur;
-	return [i,j];
-}
-RampDist.prototype.reset =function(){
-	this.cur = 0;
-	for (let i=0; i<this.rows.length; ++i){
-		this.rows[i] = 0;
+	get(){
+		if (this.cur==this.rows.length || 
+			this.cur!=0 && this.rows[this.cur]+1 == this.rows[this.cur-1]){
+			// if not first column AND new count will equal previous column's current count
+			this.cur = 0;
+		}
+		const i = this.cur;
+		const j = this.rows[this.cur];
+		++this.rows[this.cur];
+		++this.cur;
+		return [i,j];
+	}
+	reset(){
+		this.cur = 0;
+		for (let i=0; i<this.rows.length; ++i){
+			this.rows[i] = 0;
+		}
 	}
 }
