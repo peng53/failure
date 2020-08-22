c5ed5d2b0ecb15773c8852930546366ca522e67d
Perry Eng <eng.perry@gmail.com>
2020-08-22 15:13:54 -0500

    Made function to write the time lines to the page

--- html/timesheet.js
+++ html/timesheet.js
@@ -44,3 +44,52 @@ function niceTime(td, sep){
 	out += td.getMinutes();
 	return out;
 }
+function addTimeRow(table, data){
+	const row = table.querySelector('tbody').insertRow(-1);
+	row.insertCell(-1).textContent = data.code;
+	row.insertCell(-1).textContent = data.start;
+	row.insertCell(-1).textContent = data.end;
+	row.insertCell(-1).textContent = data.total;
+}
+
+function addDataToTables(tables, data, rowlimit, f){
+	let i=0;
+	for (let t=0; t<tables.length; ++t){
+		for (;i<data.length && i<rowlimit; ++i){
+			f(tables[t], data[i]);
+		}
+		rowlimit *= 2;
+	}
+}
+
+function testingFunc(){
+	const testIn =`\
+2:00 PM 8/22/2020
+123
+2:05 PM 8/22/2020
+
+2:07 PM 8/22/2020
+134
+2:10 PM 8/22/2020
+
+2:10 PM 8/22/2020
+145
+2:15 PM 8/22/2020
+
+2:20 PM 8/22/2020
+156
+2:40 PM 8/22/2020
+
+3:00 PM 8/22/2020
+167
+3:30 PM 8/22/2020
+
+4:00 PM 8/22/2020
+178
+5:00 PM 8/22/2020
+`;
+	const parsedData = formatNotes(testIn);
+	addDataToTables(rows.querySelectorAll('table'), parsedData, 10, addTimeRow);
+	console.table(parsedData);
+}
+testingFunc();
