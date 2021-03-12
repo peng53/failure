32007c8d954153085b081e6555856e1f240d303c
Perry Eng <eng.perry@gmail.com>
2021-03-12 15:14:56 -0600

    Creation of hint nodes complete. Less complex that I thought or perhaps acquired knowledge. Now, an actual problem: 	each tile can have an event listener removing the need to 	calculate which tile is clicked but a tile right now does not 	know what it is on the adj table.
    	one say you can use the class denoting the color, but once again
    	although the color would change, the hints would not.
    
    	as a result, I need to somehow either store the tiles or their
    	position (former in the NCrossSvg and latter in the tile itself)

--- html/ncrossSvg.js
+++ html/ncrossSvg.js
@@ -1,12 +1,13 @@
 import { NCrossBoard } from './ncross.js';
 
-function generateColorStyle(p,states){
+function generateColorStyle(p,states,fontsize){
 	let s = p.appendChild(document.createElement('style'));
 	for (let i=0;i<states.length;++i){
 		console.log(`.state${i} { fill: ${states[i]};}`);
 		//s.sheet.addRule(`.state${i} { fill : ${states[i]};}`,0);
-		s.textContent += `.state${i} { fill: ${states[i]};}`;
+		s.textContent += `.state${i} { fill: ${states[i]}; color: ${states[i]};}`;
 	}
+	s.textContent += `text { font-size: ${fontsize}%}`;
 }
 
 export class NCrossSvg {
@@ -26,12 +27,18 @@ export class NCrossSvg {
 	}
 	init(){
 		// need to remove tiles if already there
-		generateColorStyle(this.svg,this.colorStates);
+		generateColorStyle(this.svg,this.colorStates,this.metrics.hintReserve);
 		console.log(this.board);
 		for (let r=0;r<this.board.height;++r){
 			for (let c=0;c<this.board.width;++c){
 				this.createTile(r,c);
 			}
+		}
+		for (let r=0;r<this.board.height;++r){
+			this.createRHint(r);
+		}
+		for (let c=0;c<this.board.width;++c){
+			this.createCHint(c);
 		}
 	}
 	createTile(r,c){
@@ -42,5 +49,29 @@ export class NCrossSvg {
 		tile.setAttributeNS(null, 'width', this.metrics.twidth);
 		tile.setAttributeNS(null, 'height', this.metrics.theight);
 		tile.classList.add(`state${this.board.A[r][c]}`);
+	}
+	createRHint(r){
+		let text;
+		let twidth = this.metrics.hintReserve/this.board.rowHints[r].length;
+		for (let i=0;i<this.board.rowHints[r].length;++i){
+			text = this.svg.appendChild(
+				document.createElementNS("http://www.w3.org/2000/svg", 'text'));
+			text.appendChild(document.createTextNode(this.board.rowHints[r][i].cnt));
+			text.setAttribute('x', twidth*i);
+			text.setAttribute('y', 1.25*this.metrics.hintReserve+r*this.metrics.theight);
+			text.classList.add(`state${this.board.rowHints[r][i].state}`);
+		}
+	}
+	createCHint(c){
+		let text;
+		let theight = this.metrics.hintReserve/(this.board.colHints[c].length+1);
+		for (let i=0;i<this.board.colHints[c].length;++i){
+			text = this.svg.appendChild(
+				document.createElementNS("http://www.w3.org/2000/svg", 'text'));
+			text.appendChild(document.createTextNode(this.board.colHints[c][i].cnt));
+			text.setAttribute('y', theight*(i+1));
+			text.setAttribute('x', 1.25*this.metrics.hintReserve+c*this.metrics.twidth);
+			text.classList.add(`state${this.board.colHints[c][i].state}`);
+		}
 	}
 }
