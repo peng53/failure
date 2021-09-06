ba4dd7cf371f59c526c994029f356a9aeaf49066
Perry Eng <eng.perry@gmail.com>
2021-09-05 22:43:21 -0500

    Made functions to create a sqlite table,  read rows from said table, and then insert them. Work in tk and sqlite really reminds me how nice it was to work in TCL/TK doing to the same thing Also practice in javascript still helping out in python

--- py3/showprog.py
+++ py3/showprog.py
@@ -2,20 +2,41 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
 import sqlite3
+import os
 
-def sqlitestuff(filename):
+def getShowsFromSqlite(filename):
 	con = sqlite3.connect(filename)
-	return con
+	cur = con.cursor()
+	shows = []
+	colsToGet = ['title','last_watched']
+	for row in cur.execute(f'select {",".join(colsToGet)} from shows'):
+		shows.append({colsToGet[i]:row[i] for i in range(len(colsToGet))})
+	con.close()
+	return shows
+
+def testGenNewSqlite(filename):
+	if os.path.exists(filename):
+		return
+	con = sqlite3.connect(filename)
+	cur = con.cursor()
+	cur.execute("create table shows (title text, last_watched text)")
+	cur.execute("insert into shows (title,last_watched) values ('Bleak','e10')")
+	cur.execute("insert into shows (title,last_watched) values ('Bland','e23')")
+	con.commit()
+	con.close()
 
 def loadExistingFile():
 	filename = filedialog.askopenfilename()
-	f = filename
+	f = getShowsFromSqlite(filename)
 	# open file as sqlite db
 	# verify contents..
 	# return connection
 	return f
 
-
+def newDatabaseFile():
+	filename = filedialog.asksaveasfilename()
+	print(filename)
+	testGenNewSqlite(filename)
 
 class Application(tk.Frame):
 	def __init__(self, master=None):
@@ -27,17 +48,6 @@ class Application(tk.Frame):
 
 		dumb = lambda: 1
 		filemenu = tk.Menu(master)
-		filemenu.add_command(label='New', command=dumb)
-		filemenu.add_command(label='Open', command=loadExistingFile)
-		filemenu.add_command(label='Close', command=dumb)
-		filemenu.add_separator()
-		filemenu.add_command(label='Exit', command=root.destroy)
-		self.menubar.add_cascade(label='File',menu=filemenu)
-
-		trackF = lambda: self.tracknew()
-		editmenu = tk.Menu(master)
-		editmenu.add_command(label='Track New', command=trackF)
-		self.menubar.add_cascade(label='Edit',menu=editmenu)
 
 		self.showlist = ttk.Treeview(master, columns=('#1','#2'), show='headings')
 		self.showlist.heading('#1',text='Title')
@@ -49,6 +59,25 @@ class Application(tk.Frame):
 		self.showlistScroll.grid(row=0, column=1, sticky='ns')
 
 		self.showlist.insert(parent='', index='end', values=('1','2'))
+
+		def loadNIns():
+			filename = filedialog.askopenfilename()
+			shows = getShowsFromSqlite(filename)
+			for show in shows:
+				self.showlist.insert(parent='', index='end', values=(show['title'],show['last_watched']))
+	
+		filemenu.add_command(label='New', command=newDatabaseFile)
+		filemenu.add_command(label='Open', command=loadNIns)
+		filemenu.add_command(label='Close', command=dumb)
+		filemenu.add_separator()
+		filemenu.add_command(label='Exit', command=root.destroy)
+		self.menubar.add_cascade(label='File',menu=filemenu)
+
+		trackF = lambda: self.tracknew()
+		editmenu = tk.Menu(master)
+		editmenu.add_command(label='Track New', command=trackF)
+		self.menubar.add_cascade(label='Edit',menu=editmenu)
+		
 
 	def tracknew(self):
 		print('tracknew')
