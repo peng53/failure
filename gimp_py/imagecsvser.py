82fd126a498eadcc7dcc46bccae752da12ef41b6
Perry Eng <eng.perry@gmail.com>
2022-05-01 17:47:09 -0500

    Fix: text editing relies on iteration of what's available not what's possible. Added: use of csv to get rows

--- gimp_py/imagecsvser.py
+++ gimp_py/imagecsvser.py
@@ -1,4 +1,5 @@
 import os
+import csv
 from gimp import pdb
 
 def saveFlatDpiPngFn(outdir, filenamefmt, dpi):
@@ -20,6 +21,13 @@ def setLayerText(textLayer, text):
 	"""
 	pdb.gimp_text_layer_set_text(textLayer,text)
 
+def copiesFromCsv(csvfile):
+	"""
+	Return list of dictionaries of key values from open csv file
+	"""
+	reader = csv.DictReader(csvfile)
+	return [{field:row[field] for field in reader.fieldnames} for row in reader]
+
 def render(image, copies, layerMap, savefn):
 	"""
 	copies is list of dictionaries with text
@@ -29,7 +37,7 @@ def render(image, copies, layerMap, savefn):
 	dlayers = {l.name:l for l in image.layers}
 	toEdits = {k:image.layers[layerMap[k]] if layerMap[k]!=None else dlayers[k] for k in layerMap}
 	for i,copy in enumerate(copies):
-		for key in toEdits:
+		for key in copy:
 			setLayerText(toEdits[key], copy[key])
 		result = pdb.gimp_image_duplicate(image)
 		savefn(result, i)
@@ -40,15 +48,16 @@ def example():
 	load image of which 0th and 2nd layer are text layers (examine image.layers for exact order)
 	create savefn that saves to r:/ with name 'seq-{0}-image.png and 200 dpi
 	(not sure if 200 dpi is working)
-	create list of pictures to render, an image for each item of which contains data
-	create dictionary of layer to key mapping
+	create list copies of pictures to render, an image for each item of which contains data
+	the csv file has a header row that correspond to layers in layMap
+	create dictionary layMap with layer to key mapping
 	calls render function building the images
 	and frees image
 	"""
 	image = pdb.gimp_file_load('r:/textlayers.xcf','textlayers.xcf')
 	savefn = saveFlatDpiPngFn('r:/', 'seq-{0}-image.png', 200)
-	copies = [{'first':'first card','second':'first tagline'}
-	, {'first':'second card','second':'second tagline'}]
+	with open('r:/test.csv') as f:
+		copies = copiesFromCsv(f)
 	layMap = {'first':0, 'second':2}
 	render(image, copies, layMap, savefn)
 	pdb.gimp_image_delete(image)
